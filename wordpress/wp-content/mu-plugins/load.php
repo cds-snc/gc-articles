7b0933fc3cfbf29ef0e348ebd47925f21c82f52a
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2021-09-02 13:08:52 -0400

    Add mu-plugins meta info

--- wordpress/wp-content/mu-plugins/load.php
+++
@@ -1,3 +0,0 @@
-<?php // mu-plugins/load.php
-require WPMU_PLUGIN_DIR.'/cds-base/index.php';
-require WPMU_PLUGIN_DIR.'/two-factor/two-factor.php';
---
+++ wordpress/wp-content/mu-plugins/load_muplugins.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * Plugin Name: Must-use plugins
+ * Description: Include must-use plugins
+ *
+ * @package cds-snc-base
+ */
+
+require WPMU_PLUGIN_DIR.'/cds-base/index.php';
+require WPMU_PLUGIN_DIR.'/two-factor/two-factor.php';

b507a51abfa77664c1da8f628063d96e6b2e6eb1
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2021-09-14 10:55:00 -0400

    Bump version for release (#75)

--- wordpress/wp-content/mu-plugins/cds-base/index.php
+++ wordpress/wp-content/mu-plugins/cds-base/index.php
@@ -8,7 +8,7 @@ require __DIR__ . '/vendor/autoload.php';
  * Plugin Name: CDS-SNC Base
  * Plugin URI: https://github.com/cds-snc/platform-mvp
  * Description: Custom Block setup and other overrides
- * Version: 1.0.6
+ * Version: 1.0.7
  * Author: Tim Arney
  *
  * @package cds-snc-base
@@ -21,7 +21,7 @@ if (!defined('BASE_PLUGIN_NAME')) {
 }
 
 if (!defined('BASE_PLUGIN_NAME_VERSION')) {
-    define('BASE_PLUGIN_NAME_VERSION', '1.0.6');
+    define('BASE_PLUGIN_NAME_VERSION', '1.0.7');
 }
 
 require_once __DIR__ . '/admin-cleaner/index.php';

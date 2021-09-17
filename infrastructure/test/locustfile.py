56ba47a1d890143aeec62c5d05ebc8018adcae20
Pat Heard <patrick.heard@cds-snc.ca>
2021-09-17 11:25:59 -0400

    feat: updated Locust performance tests (#114)
    Two flows:
    1. No login, primarily handled by CloudFront (20x more likely to run).
    2. Login, no caching.

--- infrastructure/test/locustfile.py
+++ infrastructure/test/locustfile.py
@@ -1,13 +1,21 @@
-from locust import HttpUser, task
+import os
+from locust import HttpUser, task, between
+
+USER = os.getenv("USER")
+PASSWORD = os.getenv("PASSWORD")
 
 class AdminUser(HttpUser):
-    @task
-    def run_flow(self):
-        # No, login - all handled by CloudFront
-        self.client.get(f"/")
-        self.client.get(f"/hello-world")
-        self.client.get(f"/?s=hello")
+    wait_time = between(1, 3)
 
-        # To test login flow, create a `locust` user in WordPress
-        response = self.client.post("/wp-login.php", {"log":"locust", "pwd":"locust", "testcookie": "1"})
-        response = self.client.get("/wp-admin/profile.php")
+    @task(20)
+    def no_login(self):
+        "No login - all handled by CloudFront"
+        self.client.get(f"/")
+        self.client.get(f"/avis-de-confidentialite")
+        self.client.get(f"/confirmation-of-subscription")
+
+    @task
+    def login(self):
+        "Login flow - not cached"
+        response = self.client.post("/login/", {"log":USER, "pwd":PASSWORD, "testcookie": "1", "redirect_to": "https://ircc.digital.canada.ca/wp-admin/admin.php?page=cds_notify_send"})
+        self.client.get("/wp-admin/edit.php", cookies=response.cookies)

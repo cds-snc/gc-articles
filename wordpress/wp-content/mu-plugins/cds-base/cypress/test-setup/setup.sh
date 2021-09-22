1ee6527e885e8301cd231f702b995f2a7934f44b
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2021-09-22 15:53:06 -0400

    Configure the cds-default theme in wp-env (#160)

--- wordpress/wp-content/mu-plugins/cds-base/cypress/test-setup/setup.sh
+++ wordpress/wp-content/mu-plugins/cds-base/cypress/test-setup/setup.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 wp-env run tests-cli wp option delete list_values
 wp-env run tests-cli wp option set list_values --format=json < ./cypress/fixtures/notify-list-data.json
+wp-env run tests-cli wp theme activate cds-default
+wp-env run cli wp theme activate cds-default

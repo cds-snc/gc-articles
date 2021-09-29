7fae1dd2086804f7b3d98379eee231fc1a1ed62e
Tim Arney <tim@line37.com>
2021-09-29 09:03:04 -0400

    add GC Article roles

--- wordpress/wp-content/mu-plugins/cds-base/classes/Modules/Cleanup/Roles.php
+++ wordpress/wp-content/mu-plugins/cds-base/classes/Modules/Cleanup/Roles.php
@@ -20,14 +20,17 @@ class Roles
             }
         }
 
-        Utils::checkOptionCallback('cds_base_activated', '1.0', function () {
+        Utils::checkOptionCallback('cds_base_activated', '1.0.4', function () {
             if (is_blog_installed()) {
                 remove_role('administrator');
                 remove_role('editor');
                 remove_role('author');
                 remove_role('contributor');
                 remove_role('subscriber');
-                $this->cleanupRoles('ircc');
+                remove_role('gceditor');
+                remove_role('gcadmin');
+                $this->cleanupRoles('gceditor');
+                $this->cleanupRoles('gcadmin');
             }
         });
     }
@@ -158,10 +161,58 @@ class Roles
                 'read' => 1,
                 'level_0' => 1,
             ],
-            'ircc' => [
+            'gceditor' => [
+                'edit_posts' => 1,
+                'edit_others_posts' => 1,
+                'edit_published_posts' => 1,
+                'publish_posts' => 1,
+                'edit_pages' => 1,
                 'read' => 1,
+                'level_7' => 1,
+                'level_6' => 1,
+                'level_5' => 1,
+                'level_4' => 1,
+                'level_3' => 1,
+                'level_2' => 1,
+                'level_1' => 1,
                 'level_0' => 1,
                 'delete_posts' => 1,
+                'delete_others_posts' => 1,
+                'delete_published_posts' => 1,
+                'delete_private_posts' => 1,
+                'edit_private_posts' => 1,
+                'read_private_posts' => 1,
+                'delete_private_pages' => 1,
+                'edit_private_pages' => 1,
+                'read_private_pages' => 1,
+            ],
+            'gcadmin' => [
+                'list_users' => 1,
+                'delete_users' => 1,
+                'create_users' => 1,
+                'edit_posts' => 1,
+                'edit_others_posts' => 1,
+                'edit_published_posts' => 1,
+                'publish_posts' => 1,
+                'edit_pages' => 1,
+                'read' => 1,
+                'level_7' => 1,
+                'level_6' => 1,
+                'level_5' => 1,
+                'level_4' => 1,
+                'level_3' => 1,
+                'level_2' => 1,
+                'level_1' => 1,
+                'level_0' => 1,
+                'delete_posts' => 1,
+                'delete_others_posts' => 1,
+                'delete_published_posts' => 1,
+                'delete_private_posts' => 1,
+                'edit_private_posts' => 1,
+                'read_private_posts' => 1,
+                'delete_private_pages' => 1,
+                'edit_private_pages' => 1,
+                'read_private_pages' => 1,
             ],
             'display_name' => [
                 'administrator' => 'Administrator',
@@ -169,7 +220,8 @@ class Roles
                 'author' => 'Author',
                 'contributor' => 'Contributor',
                 'subscriber' => 'Subscriber',
-                'ircc' => 'IRCC',
+                'gceditor' => 'GC Editor',
+                'gcadmin' => 'GC Admin'
             ],
         ];
 

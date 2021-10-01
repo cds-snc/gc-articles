78a2e9b5f97d9d5f973d86d8cd8abbc1a00448e6
Paul Craig <paul_craig_16@hotmail.com>
2021-10-01 14:14:44 -0400

    Increase font sizes for the Admin dashboard (#78)
    * Increase font sizes for the Admin dashboard
    
    To make the site perform better on a tablet, we want to increase the
    font size for desktop and tablet screen sizes.
    
    This PR bumps the font sizes from:
    
    - 13 px
    - 14 px
    - 16.9 px (subheadings)
    - 23 px (main headings)
    
    to
    
    - 14.5 px
    - 16 px
    - 20.8 px (subheadings)
    - 30 px (main headings)
    
    For more information, read this issue comment
    - https://github.com/cds-snc/gc-articles/issues/62#issuecomment-930222404
    
    * Increase text size on the login screen
    
    * Add cypress checks for increased font size
    
    Check on the login page and on the dashboard.

--- cypress/integration/login.spec.js
+++ cypress/integration/login.spec.js
@@ -10,6 +10,7 @@ describe('Login', () => {
     cy.screenshot();
     cy.get("#login h1 a").should("have.text", "Canadian Digital Service");
     cy.get('#login h1 a').should('have.css', 'background-image', 'url("'+host+'/wp-content/plugins/cds-base/images/site-login-logo.svg")')
+    cy.get('.login form label').should('have.css', 'font-size', '16px')
   });
 
   it('Login redirect to dashboard', () => {
@@ -19,5 +20,12 @@ describe('Login', () => {
     cy.url().should('eq', `${host}/wp-admin/index.php`)
   });
 
+  it('Can view styled dashboard', () => {
+    cy.loginUser();
+
+    cy.get("body").should("have.css", "font-size", "14.5px"); // body font-size increased to 14.5
+    cy.get("#wpadminbar").should("have.css", "height", "36px"); // admin bar height increased to 36
+    cy.get("h1").should("have.css", "font-size", "30px"); // h1 font-size increased to 30
+  });
 
 });

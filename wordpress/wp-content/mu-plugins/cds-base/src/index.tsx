517e9c037c4acd41412d582d8055320eca706c7a
Tim Arney <tim@line37.com>
2021-10-05 13:30:50 +0000

    format

--- wordpress/wp-content/mu-plugins/cds-base/src/index.tsx
+++ wordpress/wp-content/mu-plugins/cds-base/src/index.tsx
@@ -1,47 +1,47 @@
-import * as React from 'react';
+import * as React from "react";
 
-import '../classes/Modules/Blocks/src/expander';
-import '../classes/Modules/Blocks/src/alert';
-import {render} from '@wordpress/element';
-import {LoginsPanel} from '../classes/Modules/TrackLogins/src/LoginsPanel';
-import {NotifyPanel} from '../classes/Modules/Notify/src/NotifyPanel';
-import {List} from '../classes/Modules/Notify/src/Types';
+import "../classes/Modules/Blocks/src/expander";
+import "../classes/Modules/Blocks/src/alert";
+import { render } from "@wordpress/element";
+import { LoginsPanel } from "../classes/Modules/TrackLogins/src/LoginsPanel";
+import { NotifyPanel } from "../classes/Modules/Notify/src/NotifyPanel";
+import { List } from "../classes/Modules/Notify/src/Types";
 
 declare global {
-    interface Window {
-        CDS: {
-            Notify?: {
-                renderPanel: ({
-                                  sendTemplateLink,
-                              }: {
-                    sendTemplateLink: boolean;
-                }) => void;
-            };
-            renderLoginsPanel?: () => void;
-        };
-        CDS_VARS: {
-            rest_url?: string;
-            rest_nonce?: string;
-            notify_list_ids?: List[];
-        };
-    }
+  interface Window {
+    CDS: {
+      Notify?: {
+        renderPanel: ({
+          sendTemplateLink,
+        }: {
+          sendTemplateLink: boolean;
+        }) => void;
+      };
+      renderLoginsPanel?: () => void;
+    };
+    CDS_VARS: {
+      rest_url?: string;
+      rest_nonce?: string;
+      notify_list_ids?: List[];
+    };
+  }
 }
 
 export const renderLoginsPanel = () => {
-    render(<LoginsPanel/>, document.getElementById('logins-panel'));
+  render(<LoginsPanel />, document.getElementById("logins-panel"));
 };
 
 export const renderNotifyPanel = ({
-                                      sendTemplateLink,
-                                  }: {
-    sendTemplateLink: boolean;
+  sendTemplateLink,
+}: {
+  sendTemplateLink: boolean;
 }) => {
-    render(
-        <NotifyPanel sendTemplateLink={sendTemplateLink}/>,
-        document.getElementById('notify-panel')
-    );
+  render(
+    <NotifyPanel sendTemplateLink={sendTemplateLink} />,
+    document.getElementById("notify-panel")
+  );
 };
 
 window.CDS = window.CDS || {};
-window.CDS.Notify = {renderPanel: renderNotifyPanel};
+window.CDS.Notify = { renderPanel: renderNotifyPanel };
 window.CDS.renderLoginsPanel = renderLoginsPanel;

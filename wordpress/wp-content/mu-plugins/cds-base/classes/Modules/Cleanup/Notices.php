2a687b8b76e19f1e94d790e8611af2010a6679d7
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2021-10-05 15:10:11 -0400

    remove empty add hook

--- wordpress/wp-content/mu-plugins/cds-base/classes/Modules/Cleanup/Notices.php
+++ wordpress/wp-content/mu-plugins/cds-base/classes/Modules/Cleanup/Notices.php
@@ -8,13 +8,6 @@ class Notices
 {
     public function __construct()
     {
-        add_action('in_admin_header', static function (): void {
-            // if we want to add a custom notice later
-            /*
-            add_action('admin_notices', function () {
-                echo 'My notice';
-            });
-            */
-        }, 1000);
+
     }
 }

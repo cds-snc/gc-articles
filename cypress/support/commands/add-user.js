b1e42dc9e9384e3044dfc237f8c17cb41ccb869d
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2021-10-05 16:46:14 -0400

    use new login() command

--- cypress/support/commands/add-user.js
+++ cypress/support/commands/add-user.js
@@ -2,7 +2,7 @@
 
 Cypress.Commands.add('addUser', (userName, password, role, autoLogin = true) => {
   if (autoLogin) {
-    cy.loginUser();
+    cy.login();
   }
   
   cy.exec(`wp-env run tests-cli "wp user create ${userName} ${userName}@example.com --role=${role} --user_pass=${password}"`, { failOnNonZeroExit: false });

4176e448bb1c5ad24e4ee8541f6635da23b85ea7
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2021-10-05 13:51:59 -0400

    remove commented out code

--- wordpress/wp-content/mu-plugins/cds-base/classes/Modules/Cleanup/Notices.php
+++ wordpress/wp-content/mu-plugins/cds-base/classes/Modules/Cleanup/Notices.php
@@ -9,13 +9,6 @@ class Notices
     public function __construct()
     {
         add_action('in_admin_header', static function (): void {
-            /**
-             * Note: commenting these out for now. This ended up having unintended side effect of disabling all
-             * admin_notices, and should be more narrowly focussed on hiding specific notices.
-             */
-            // remove_all_actions('admin_notices');
-            // remove_all_actions('all_admin_notices');
-
             // if we want to add a custom notice later
             /*
             add_action('admin_notices', function () {

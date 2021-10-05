73bc7034947c5f9dd7db87654e4c6bbe73d22299
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2021-10-05 17:15:06 -0400

    remove autologin

--- cypress/support/commands/add-user.js
+++ cypress/support/commands/add-user.js
@@ -1,10 +1,6 @@
 
 
-Cypress.Commands.add('addUser', (userName, password, role, autoLogin = true) => {
-  if (autoLogin) {
-    cy.login();
-  }
-  
+Cypress.Commands.add('addUser', (userName, password, role) => {
   cy.exec(`wp-env run tests-cli "wp user create ${userName} ${userName}@example.com --role=${role} --user_pass=${password}"`, { failOnNonZeroExit: false });
 });
 

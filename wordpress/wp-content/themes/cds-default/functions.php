c79df6b2457221dcba591dfeac9f126e8aafc7c6
Paul Craig <paul_craig_16@hotmail.com>
2021-10-06 10:11:31 -0400

    version bump 1.4.0 (#100)

--- wordpress/wp-content/themes/cds-default/functions.php
+++ wordpress/wp-content/themes/cds-default/functions.php
@@ -17,7 +17,7 @@ require_once __DIR__ . '/inc/template-filters.php';
 
 if (!defined('_S_VERSION')) {
     // Replace the version number of the theme on each release.
-    define('_S_VERSION', '1.3.4');
+    define('_S_VERSION', '1.4.0');
 }
 
 if (!defined('THEME_NAMESPACE')) {

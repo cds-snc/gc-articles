e65937e1f84c68a54a14e1964597214ca2d0125d
Paul Craig <paul@pcraig3.ca>
2021-10-07 11:59:29 -0400

    Add cypress test for meta description
    The previous "await" wasn't actually returning properly, so I
    restructured the test.

--- cypress/integration/util.js
+++ cypress/integration/util.js
@@ -1,13 +1,7 @@
-export const addArticle = async (text) => {
+export const addArticle = (text) => {
     cy.visit("/wp-admin/post-new.php");
     cy.setPostContent(text);
     cy.wait(1000);
     cy.get("body").type('{cmd}s');
     cy.wait(1000);
-
-    return new Promise((resolve, reject) => {
-        cy.get(".editor-post-preview").invoke('attr', 'href').then((href) => {
-            resolve(href);
-        });
-    })
 }

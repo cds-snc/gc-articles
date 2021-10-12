1c244bdef3a6af20c659b6f67f06d236c2f5a9cf
Tim Arney <tim@line37.com>
2021-10-12 10:45:31 -0400

    fix format

--- wordpress/wp-content/mu-plugins/cds-base/classes/Modules/Blocks/Blocks.php
+++ wordpress/wp-content/mu-plugins/cds-base/classes/Modules/Blocks/Blocks.php
@@ -70,7 +70,7 @@ class Blocks
 
         wp_enqueue_style(
             'cds-base-editor-fonts',
-            '<link href="https://fonts.googleapis.com/css2?family=Lato:wght@700&family=Noto+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet">',
+            'https://fonts.googleapis.com/css2?family=Lato:wght@700&family=Noto+Sans:ital,wght@0,400;0,700;1,400;1,700&display=swap',
             [],
             $this->version,
         );

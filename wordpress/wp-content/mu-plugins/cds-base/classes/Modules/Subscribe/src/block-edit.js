7fb51e7613cc2a94f4f4f431dce7f90a33fa2056
Tim Arney <tim@line37.com>
2021-10-13 00:20:22 +0000

    wrap disabled

--- wordpress/wp-content/mu-plugins/cds-base/classes/Modules/Subscribe/src/block-edit.js
+++ wordpress/wp-content/mu-plugins/cds-base/classes/Modules/Subscribe/src/block-edit.js
@@ -1,13 +1,16 @@
 
 import { useBlockProps } from '@wordpress/block-editor';
 import ServerSideRender from '@wordpress/server-side-render';
+import { Disabled } from '@wordpress/components';
 import { name } from '../block.json';
 
 const Edit = ({ attributes }) => {
 	const blockProps = useBlockProps();
 	return (
 		<div {...blockProps}>
-			<ServerSideRender block={name} attributes={attributes} />
+			<Disabled>
+				<ServerSideRender block={name} attributes={attributes} />
+			</Disabled>
 		</div>
 	);
 };

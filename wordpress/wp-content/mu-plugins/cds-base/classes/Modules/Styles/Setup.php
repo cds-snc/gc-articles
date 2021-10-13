60df5fe305fce169c144297776db6415fa774686
Paul Craig <paul@pcraig3.ca>
2021-10-13 09:19:25 -0400

    Update public site with WET styles that weren't getting applied
    The main elements that weren't getting styles were the tables and
    the lists.
    
    Added yet another CSS file since I don't want to touch our generated
    "styles.css" file.

--- wordpress/wp-content/mu-plugins/cds-base/classes/Modules/Styles/Setup.php
+++ wordpress/wp-content/mu-plugins/cds-base/classes/Modules/Styles/Setup.php
@@ -8,11 +8,12 @@ class Setup
 {
     public function __construct()
     {
-        add_action('wp_enqueue_scripts', [$this, 'enqueue']);
+        add_action('wp_enqueue_scripts', [$this, 'enqueue'], 99);
     }
 
     public function enqueue()
     {
         wp_enqueue_style('cds-base-css', plugin_dir_url(__FILE__) . 'template/css/styles.css', null, "1.0.0");
+        wp_enqueue_style('cds-base-css-fixes', plugin_dir_url(__FILE__) . 'template/css/fixes.css', null, "1.0.0");
     }
 }

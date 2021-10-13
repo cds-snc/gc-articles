05374d01749eed33f305fd27c47d1f44117c78e0
Tim Arney <tim@line37.com>
2021-10-13 13:27:42 -0400

    remove placeholder for contact form

--- wordpress/wp-content/mu-plugins/cds-base/classes/Modules/Contact/src/block-edit.js
+++ wordpress/wp-content/mu-plugins/cds-base/classes/Modules/Contact/src/block-edit.js
@@ -1,31 +1,18 @@
 import { __ } from "@wordpress/i18n";
 import ServerSideRender from '@wordpress/server-side-render';
 import { PanelBody, Disabled, TextControl } from '@wordpress/components';
-import { InspectorControls, useBlockProps } from '@wordpress/block-editor';
+import { useBlockProps } from '@wordpress/block-editor';
 import { name } from '../block.json';
 
 const Edit = ({ attributes, setAttributes }) => {
 	const { placeholderValue } = attributes;
 	const blockProps = useBlockProps();
 	return (
-		<>
-			<InspectorControls>
-				<PanelBody title={__('Contact form settings', "cds-snc")}>
-					<TextControl
-						label={__("Placeholder text", "cds-snc")}
-						value={placeholderValue}
-						onChange={(value) => setAttributes({
-							placeholderValue: value,
-						})}
-					/>
-				</PanelBody>
-			</InspectorControls>
-			<div {...blockProps}>
-				<Disabled>
-					<ServerSideRender block={name} attributes={attributes} />
-				</Disabled>
-			</div>
-		</>
+		<div {...blockProps}>
+			<Disabled>
+				<ServerSideRender block={name} attributes={attributes} />
+			</Disabled>
+		</div>
 	);
 };
 

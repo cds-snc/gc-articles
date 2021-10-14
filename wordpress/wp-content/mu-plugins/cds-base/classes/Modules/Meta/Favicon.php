e554a10d8ef2c6b9c422f59ac0ec992d172cbfab
Paul Craig <paul@pcraig3.ca>
2021-10-14 11:16:24 -0400

    Redirect to our favicon
    WordPress has a specific action for the root "/favicon.ico" request
    which serves up a default wordpress logo.
    
    Added a function that returns our favicon and then exits, so that
    the wordpress redirect doesn't run.
    
    Sources:
    - https://developer.wordpress.org/reference/hooks/do_faviconico/#source
    - https://core.trac.wordpress.org/browser/tags/5.8/src/wp-includes/functions.php#L1692

--- wordpress/wp-content/mu-plugins/cds-base/classes/Modules/Meta/Favicon.php
+++ wordpress/wp-content/mu-plugins/cds-base/classes/Modules/Meta/Favicon.php
@@ -6,14 +6,24 @@ namespace CDS\Modules\Meta;
 
 class Favicon
 {
+    public string $favicon_url;
+
     public function __construct()
     {
+        $this->favicon_url = plugin_dir_url(__FILE__) . 'img/favicon.ico';
+
         add_action('wp_head', [$this, 'addFavicon']);
+        add_action('do_faviconico', [$this, 'removeDefaultFavicon']);
     }
 
     public function addFavicon()
     {
-        $favicon_url = plugin_dir_url(__FILE__) . 'img/favicon.ico';
-        printf("<link href='%s' rel='icon' type='image/x-icon' />", $favicon_url);
+        printf("<link href='%s' rel='icon' type='image/x-icon' />", $this->favicon_url);
+    }
+
+    public function removeDefaultFavicon()
+    {
+        wp_redirect($this->favicon_url);
+        exit;
     }
 }

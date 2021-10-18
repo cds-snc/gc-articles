f587786bed27b4b25ab19cec747a080bd762fca8
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2021-10-18 13:44:47 -0400

    add a couple assertions

--- wordpress/wp-content/mu-plugins/cds-base/classes/Modules/EncryptedOption/tests/EncryptedOptionTest.php
+++ wordpress/wp-content/mu-plugins/cds-base/classes/Modules/EncryptedOption/tests/EncryptedOptionTest.php
@@ -8,3 +8,15 @@ test('Encrypt/Decrypt string', function() {
     $this->assertNotSame('foo', $encrypted);
     $this->assertSame('foo', $encryptedOption->decryptString($encrypted));
 });
+
+test('GenerateKey', function() {
+    $encryptedOption = new EncryptedOption(str_repeat('a', 32));
+
+    // key should be prefixed by 'base64:'
+    $key = $encryptedOption->generateKey();
+    $this->assertStringContainsString('base64:', $key);
+
+    // key part should be 44 chars
+    $keyparts = explode(':', $key);
+    $this->assertTrue(strlen($keyparts[1]) === 44);
+});

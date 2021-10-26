43e3e2380acc71c06e797e7906b8e4ed9d7c67a6
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2021-10-26 14:24:06 -0400

    Place s3-uploads in mu-plugins

--- wordpress/wp-content/mu-plugins/load_muplugins.php
+++ wordpress/wp-content/mu-plugins/load_muplugins.php
@@ -12,3 +12,4 @@ require WPMU_PLUGIN_DIR . '/cds-base/index.php';
 require WPMU_PLUGIN_DIR . '/two-factor/two-factor.php';
 require WPMU_PLUGIN_DIR . '/wps-hide-login/wps-hide-login.php';
 require WPMU_PLUGIN_DIR . '/wp-bootstrap-blocks/wp-bootstrap-blocks.php';
+require WPMU_PLUGIN_DIR . '/s3-uploads/s3-uploads.php';

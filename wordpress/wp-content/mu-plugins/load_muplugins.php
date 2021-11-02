952a5e99872355dffab2c4c14b4c3797ca76561a
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2021-11-02 10:39:36 -0400

    don't load loginlockdown in wp-env

--- wordpress/wp-content/mu-plugins/load_muplugins.php
+++ wordpress/wp-content/mu-plugins/load_muplugins.php
@@ -7,6 +7,8 @@
  * @package cds-snc-base
  */
 
+use CDS\Utils;
+
 require_once(__DIR__ . '/../../vendor/autoload.php');
 
 require WPMU_PLUGIN_DIR . '/wp-native-php-sessions/pantheon-sessions.php';
@@ -15,4 +17,7 @@ require WPMU_PLUGIN_DIR . '/two-factor/two-factor.php';
 require WPMU_PLUGIN_DIR . '/wps-hide-login/wps-hide-login.php';
 require WPMU_PLUGIN_DIR . '/wp-bootstrap-blocks/wp-bootstrap-blocks.php';
 require WPMU_PLUGIN_DIR . '/s3-uploads/s3-uploads.php';
-require WPMU_PLUGIN_DIR . '/login-lockdown/loginlockdown.php';
+
+if (!Utils::isWpEnv()) {
+    require WPMU_PLUGIN_DIR . '/login-lockdown/loginlockdown.php';
+}

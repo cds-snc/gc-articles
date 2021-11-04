22be78a5110c780ea7e7c6c9c7a2cb184f0bc87c
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2021-11-04 16:53:03 -0400

    add comment

--- wordpress/wp-content/mu-plugins/cds-base/classes/Modules/Cleanup/Wpml.php
+++ wordpress/wp-content/mu-plugins/cds-base/classes/Modules/Cleanup/Wpml.php
@@ -10,6 +10,11 @@ class Wpml
 {
     public static function setup()
     {
+        /*
+         * We need to set this option to an empty array. This is supposed to be an array of directories that WPML will
+         * scan looking for language switchers, but it has problems with s3: prefixed URLs. We don't use these
+         * language switchers anyway, so just kill it.
+         */
         add_action( 'setup_theme', function() {
             Utils::addOrUpdateOption('wpml_language_switcher_template_objects', []);
         }, 10, 1 );

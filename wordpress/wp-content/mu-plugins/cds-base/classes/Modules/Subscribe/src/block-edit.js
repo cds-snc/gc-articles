6bfb21500c8e7465efe51cbf4b725592335884dc
Tim Arney <tim@line37.com>
2021-11-04 09:45:48 -0400

    add select for list Ids

--- wordpress/wp-content/mu-plugins/cds-base/classes/Modules/Subscribe/src/block-edit.js
+++ wordpress/wp-content/mu-plugins/cds-base/classes/Modules/Subscribe/src/block-edit.js
@@ -1,12 +1,20 @@
 import { __ } from "@wordpress/i18n";
 import ServerSideRender from '@wordpress/server-side-render';
-import { PanelBody, Disabled, TextControl } from '@wordpress/components';
+import { PanelBody, Disabled, TextControl, SelectControl } from '@wordpress/components';
 import { InspectorControls, useBlockProps } from '@wordpress/block-editor';
 import { name } from '../block.json';
 
 const Edit = ({ attributes, setAttributes }) => {
+
 	const { placeholderValue, listId } = attributes;
 	const blockProps = useBlockProps();
+
+	let listValues = CDS_VARS.notify_list_ids || [];
+
+	listValues = listValues.map((item) => {
+		return { value: item?.id, label: item?.label }
+	});
+
 	return (
 		<>
 			<InspectorControls>
@@ -18,15 +26,13 @@ const Edit = ({ attributes, setAttributes }) => {
 							placeholderValue: value,
 						})}
 					/>
-
-					<TextControl
+					<SelectControl
 						label={__("List ID", "cds-snc")}
 						value={listId}
 						onChange={(value) => setAttributes({
 							listId: value,
 						})}
-					/>
-
+						options={listValues} />
 				</PanelBody>
 			</InspectorControls>
 			<div {...blockProps}>

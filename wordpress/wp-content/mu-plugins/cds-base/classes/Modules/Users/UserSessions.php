c695ca6a00aff3d81e90dc05d308be1b4391db9a
Paul Craig <paul@pcraig3.ca>
2021-11-09 13:51:57 -0500

    Shorten session time to 48 hours
    Add a session-time filter that always returns 48 hours.
    
    Wordpress' default is 14 days if you click "remember me", and 2 days
    if you don't click it.
    
    Asked Bryan about what we should do and he says:
    
    > Just hide it, we will accept a session time out of 48 hours and the associated risk and I will reflect that in the Security report.
    
    Sources:
    - https://pc021.info/change-session-expire-time-in-wordpress/
    - https://github.com/WordPress/WordPress/blob/780b0399f74916c25e8d65393877a40af3c21db0/wp-includes/pluggable.php#L889

--- wordpress/wp-content/mu-plugins/cds-base/classes/Modules/Users/UserSessions.php
+++ wordpress/wp-content/mu-plugins/cds-base/classes/Modules/Users/UserSessions.php
@@ -37,12 +37,23 @@ class UserSessions
 
     private function addHooks(): void
     {
+        // shorten session time to 48 hours
+        add_filter('auth_cookie_expiration', [$this, 'expireSessions'], 99, 3);
+
         // ~Heavily~ inspired by the "Remember Me Not" plugin: https://wordpress.org/plugins/remembermenot/
 
         // Remove the "Remember me" option from the login form
         add_action('login_form', [$this, 'removeRememberMe'], 99);
         // Reset any attempt to set the "Remember me" option
         add_action('login_head', [$this, 'resetRememberMeOption'], 99);
+    }
+
+    public function expireSessions(int $seconds, int $user_id, bool $remember): int
+    {
+        // Wordpress default is 14 or 2 days
+        $expiration = (60 * 60) * 48; // 48 hours
+
+        return $expiration;
     }
 
     public function resetRememberMeOption(): void
@@ -59,7 +70,7 @@ class UserSessions
     }
 
     public function processLoginForm($content): string
-    {   
+    {
         $content = preg_replace(
             '/<p class="forgetmenot">(.*)<\/p>/', // Remove the "remember me" checkbox
             '<style>#wp-submit {float: left;}</style>', // add CSS rules to left-align the button

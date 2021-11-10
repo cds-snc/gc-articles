74d6648062664f1f6b9173799d8ec4f77664a818
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2021-11-10 09:27:06 -0500

    removed list_manager_service_id from settings

--- cypress/integration/list-manager.settings.spec.js
+++ cypress/integration/list-manager.settings.spec.js
@@ -13,7 +13,6 @@ describe('List Manager Settings', () => {
     cy.get('h1').should('have.text', 'List Manager Settings');
 
     cy.get('.api-key').should('be.empty');
-    cy.get('#list_manager_service_id').should('be.empty');
   });
 
   it('Can save settings', () => {
@@ -21,7 +20,6 @@ describe('List Manager Settings', () => {
 
     // @todo update test
     // cy.get('#list_manager_notify_services').type('listmanagerserviceslist');
-    cy.get('#list_manager_service_id').type('serviceidforlistmanager');
     cy.get('#submit').click();
 
     cy.get('#setting-error-settings_updated').should('contain.text', 'Settings saved');
@@ -31,6 +29,5 @@ describe('List Manager Settings', () => {
     cy.visit('/wp-admin/admin.php?page=cds_list_manager_settings');
     
     cy.get('#list_manager_notify_services').should('be.empty');
-    cy.get('#list_manager_service_id').should('be.empty');
   })
 });

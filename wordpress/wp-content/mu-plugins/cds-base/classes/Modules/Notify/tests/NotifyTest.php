25d957bc3b342fc78b4fa9f3281a1eba7d977548
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2021-11-10 14:09:30 -0500

    Renamed method

--- wordpress/wp-content/mu-plugins/cds-base/classes/Modules/Notify/tests/NotifyTest.php
+++ wordpress/wp-content/mu-plugins/cds-base/classes/Modules/Notify/tests/NotifyTest.php
@@ -11,7 +11,7 @@ use InvalidArgumentException;
 test('parseServiceIds', function () {
     $sender = new NotifyTemplateSender(new FormHelpers(), new Notices());
     $serviceIdEnv = "serviceID1~mvp-e609f6b0-0390-45b0-aaae-f4cc92c713e4-1deede83-37a9-4b47-ade1-64a9094d00f7,serviceID2~mvp-e609f6b0-0390-45b0-aaae-f4cc92c713e4-1deede83-37a8-4b47-ade1-64a9094d00f7,serviceID3~mvp-e609f6b0-0390-45b0-aaae-f4cc92c713e4-1deede83-37a9-4b46-ade1-64a9094d00f7";
-    $serviceIds = Utils::parseServiceIdsFromEnv($serviceIdEnv);
+    $serviceIds = Utils::parseServicesStringToArray($serviceIdEnv);
     expect($serviceIds)->toEqual(["serviceID1" => [
         'service_id' => 'e609f6b0-0390-45b0-aaae-f4cc92c713e4',
         'api_key' => 'mvp-e609f6b0-0390-45b0-aaae-f4cc92c713e4-1deede83-37a9-4b47-ade1-64a9094d00f7',
@@ -30,12 +30,12 @@ test('parseServiceIds', function () {
 test('parseServiceIdsBadInput', function () {
     $sender = new NotifyTemplateSender(new FormHelpers(), new Notices());
     $serviceIdEnv = "serviceID1";
-    Utils::parseServiceIdsFromEnv($serviceIdEnv);
+    Utils::parseServicesStringToArray($serviceIdEnv);
 })->throws(InvalidArgumentException::class);
 
 test('parseServiceIdsNoInput', function () {
     $sender = new NotifyTemplateSender(new FormHelpers(), new Notices());
-    Utils::parseServiceIdsFromEnv(null);
+    Utils::parseServicesStringToArray(null);
 })->throws(InvalidArgumentException::class);
 
 test('parseJsonOptions', function () {

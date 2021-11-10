09a08af01a5254f2b119f84c247b8b39b95f36f5
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2021-11-10 14:08:59 -0500

    Merge ListManagerServicesString and add Tests

--- wordpress/wp-content/mu-plugins/cds-base/classes/Modules/Notify/Utils.php
+++ wordpress/wp-content/mu-plugins/cds-base/classes/Modules/Notify/Utils.php
@@ -9,7 +9,7 @@ use InvalidArgumentException;
 
 class Utils
 {
-    public static function parseServiceIdsFromEnv($serviceIdData): array
+    public static function parseServicesStringToArray($serviceIdData): array
     {
         if (!$serviceIdData) {
             throw new InvalidArgumentException('No service data');
@@ -22,11 +22,15 @@ class Utils
             for ($i = 0; $i < count($arr); $i++) {
                 $key_value = explode('~', $arr [$i]);
 
-                $service_ids[$key_value[0]] = [
-                    'service_id' => self::extractServiceIdFromApiKey($key_value[1]),
-                    'api_key' => $key_value[1],
-                    'name' => $key_value[0]
-                ];
+                if(array_key_exists(1, $key_value)) {
+                    $service_ids[$key_value[0]] = [
+                        'service_id' => self::extractServiceIdFromApiKey($key_value[1]),
+                        'api_key'    => $key_value[1],
+                        'name'       => $key_value[0]
+                    ];
+                } else {
+                    $service_ids[$key_value[0]] = null;
+                }
             }
 
             return $service_ids;
@@ -38,5 +42,59 @@ class Utils
     public static function extractServiceIdFromApiKey($apiKey): string
     {
         return substr($apiKey, -73, 36);
+    }
+
+    public static function convertServicesArrayToString($services)
+    {
+        $str = "";
+
+        foreach ($services as $key => $value) {
+            if (is_array($value)) {
+                if ($value['name'] !== "" && $value['service_id'] !== "" && $value['api_key'] !== "") {
+
+                    $str .= $value['name'].'~'.$value['api_key'].",";
+                }
+            }
+        }
+
+        return trim($str, ',');
+    }
+
+    public static function mergeListManagerServicesString(string $incoming, string $existing): string
+    {
+        // If existing is empty no reason to merge just return incoming
+        if(!$existing) {
+            return $incoming;
+        }
+
+        // Likewise if incoming is empty no reason to merge just return existing
+        if(!$incoming) {
+            return $existing;
+        }
+
+        $incomingArray = self::parseServicesStringToArray($incoming);
+        $existingArray = self::parseServicesStringToArray($existing);
+
+        if(count($incomingArray)) {
+            foreach($existingArray as $key => $details) {
+                if(array_key_exists($key, $incomingArray)) {
+                    if ($incomingArray[$key] == null || $incomingArray[$key]['service_id'] == '') {
+                        // no change
+                        $incomingArray[$key] = $details;
+                    } else {
+                        // update
+                        $existingArray[$key] = $incomingArray[$key];
+                    }
+                } else {
+                    // delete
+                    unset($existingArray[$key]);
+                }
+            }
+
+            $merged = array_merge($incomingArray, $existingArray);
+            return self::convertServicesArrayToString($merged);
+        }
+
+        return $existing;
     }
 }

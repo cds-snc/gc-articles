bd3890ee5f1fbffb546169b7198ee00fc1d3f5eb
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2021-11-15 15:23:46 -0500

    Catch when the notify services option is empty (new site)

--- wordpress/wp-content/mu-plugins/cds-base/classes/Modules/Notify/SendTemplateDashboardPanel.php
+++ wordpress/wp-content/mu-plugins/cds-base/classes/Modules/Notify/SendTemplateDashboardPanel.php
@@ -26,10 +26,11 @@ class SendTemplateDashboardPanel
     {
         $sender = new NotifyTemplateSender();
         $serviceIdData = get_option('LIST_MANAGER_NOTIFY_SERVICES');
-        $services = Utils::deserializeServiceIds($serviceIdData);
-
         $serviceIds = [];
+        
         try {
+            $services = Utils::deserializeServiceIds($serviceIdData);
+
             foreach ($services as $key => $value) {
                 array_push($serviceIds, $value['service_id']);
             }

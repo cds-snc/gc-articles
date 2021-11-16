873ddf2364ef1b8a9528fc63529e4c48e7dd7c5b
Tim Arney <tim@line37.com>
2021-11-16 13:51:28 -0500

    remove action for show user

--- wordpress/wp-content/mu-plugins/cds-base/classes/Modules/Notify/ListManagerUserProfile.php
+++ wordpress/wp-content/mu-plugins/cds-base/classes/Modules/Notify/ListManagerUserProfile.php
@@ -15,7 +15,6 @@ class ListManagerUserProfile
     {
         $instance = new self();
 
-        add_action('show_user_profile', [$instance, 'displayListManagerMeta']);
         add_action('edit_user_profile', [$instance, 'displayListManagerMeta']);
 
         add_action('personal_options_update', [$instance, 'updateListManagerMeta']);

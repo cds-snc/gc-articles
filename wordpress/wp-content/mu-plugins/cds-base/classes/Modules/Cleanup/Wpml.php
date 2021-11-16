672887661cfab1f99bb41e43758b280b1a6e2715
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2021-11-16 12:38:07 -0500

    slight refactor

--- wordpress/wp-content/mu-plugins/cds-base/classes/Modules/Cleanup/Wpml.php
+++ wordpress/wp-content/mu-plugins/cds-base/classes/Modules/Cleanup/Wpml.php
@@ -10,7 +10,7 @@ class Wpml
 {
     public static function setup()
     {
-        /*
+        /**
          * We need to set this option to an empty array. This is supposed to be an array of directories that WPML will
          * scan looking for language switchers, but it has problems with s3: prefixed URLs. We don't use these
          * language switchers anyway, so just kill it.
@@ -19,12 +19,13 @@ class Wpml
             Utils::addOrUpdateOption('wpml_language_switcher_template_objects', []);
         }, 10, 1);
 
+        /**
+         * Disable WPML Translation Editor
+         */
         if ($settings = get_option('icl_sitepress_settings')) {
-            if (isset($settings['translation-management'])) {
-                if (!isset($settings['translation-management']['post_translation_editor_native'])) {
-                    $settings['translation-management']['post_translation_editor_native'] = true;
-                    update_option('icl_sitepress_settings', $settings);
-                }
+            if (!array_key_exists('post_translation_editor_native', $settings['translation-management'] ?? [])) {
+                $settings['translation-management']['post_translation_editor_native'] = true;
+                update_option('icl_sitepress_settings', $settings);
             }
         }
     }

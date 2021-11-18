2cb67404083d2cea83080f579389bd0e8058acfd
Tim Arney <tim@line37.com>
2021-11-18 15:13:06 -0500

    version bump 2.1.1

--- wordpress/wp-content/mu-plugins/cds-base/index.php
+++ wordpress/wp-content/mu-plugins/cds-base/index.php
@@ -4,7 +4,7 @@
  * Plugin Name: CDS-SNC Base
  * Plugin URI: https://github.com/cds-snc/platform-mvp
  * Description: Custom Block setup and other overrides
- * Version: 2.1.0
+ * Version: 2.1.1
  * Author: Tim Arney
  *
  * @package cds-snc-base
@@ -24,7 +24,7 @@ if (!defined('BASE_PLUGIN_NAME')) {
 }
 
 if (!defined('BASE_PLUGIN_NAME_VERSION')) {
-    define('BASE_PLUGIN_NAME_VERSION', '2.1.0');
+    define('BASE_PLUGIN_NAME_VERSION', '2.1.1');
 }
 
 if (is_multisite()) {

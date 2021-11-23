0a0f37692a1b8614aaf71d7cd7b3ee845f8c443e
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2021-11-23 16:50:59 -0500

    Move wp-native-php-sessions out of mu-plugins

--- wordpress/wp-content/mu-plugins/load_muplugins.php
+++ wordpress/wp-content/mu-plugins/load_muplugins.php
@@ -11,7 +11,6 @@ use CDS\Utils;
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 
-require WPMU_PLUGIN_DIR . '/wp-native-php-sessions/pantheon-sessions.php';
 require WPMU_PLUGIN_DIR . '/disable-user-login/disable-user-login.php';
 
 

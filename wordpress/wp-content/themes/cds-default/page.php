e1e2c9ae65339fdd7f875ae5c70c41334dbd31bd
Paul Craig <paul_craig_16@hotmail.com>
2021-11-24 11:43:59 -0500

    Put in a basic side menu (#284)

--- wordpress/wp-content/themes/cds-default/page.php
+++ wordpress/wp-content/themes/cds-default/page.php
@@ -15,15 +15,71 @@
 
 declare(strict_types=1);
 
+function wpse33151_getSubpages()
+{
+    global $post;
+    $parent_page_is_current_page = '';
+
+    $parents = get_post_ancestors($post->post_id);
+    krsort($parents);
+    $parents = array_merge(array(), $parents);
+
+    if (is_home() || is_single()) {
+        $id = get_option('page_for_posts');
+        $parent = get_post_ancestors($id);
+        $id = $parent[0];
+    } elseif ($parents) {
+        $id = $parents[0];
+    } else {
+        $id = $post->ID;
+        $parent_page_is_current_page = "current_page_item";
+    }
+
+    $children = wp_list_pages('title_li=&child_of=' . $id . '&echo=0');
+
+    $out = null;
+
+    if ($children) {
+        $out = '<div id="subpages" class="nav--about__desktop">';
+        $out .= '<div class="nav--about__desktop__title ' . $parent_page_is_current_page . '">' . get_the_title($id) . '</div>';
+        $out .= '<nav class="nav--about" aria-label="Table of contents: ' . get_the_title($id) . '">';
+        $out .= '<ul>';
+        $out .= $children;
+        $out .= '</ul>';
+        $out .= '</nav>';
+        $out .= '</div>';
+        $out .= '<details class="nav--about__mobile">';
+        $out .= '<summary><div>' . get_the_title($id) . '</div></summary>';
+        $out .= '<nav class="nav--about" aria-label="Table of contents: ' . get_the_title($id) . '">';
+        $out .= '<ul>';
+        $out .= $children;
+        $out .= '</ul>';
+        $out .= '</nav>';
+        $out .= '</details>';
+    }
+
+    return $out;
+}
+
+$side_nav = wpse33151_getSubpages();
+$has_side_nav = $side_nav ? true : false;
+
 get_header();
 ?>
 
     <main property="mainContentOfPage" class="page container" resource="#wb-main" typeof="WebPageElement">
 
+        <?php if ($has_side_nav) { ?>
+        <div class="wp-block-columns">
+            <div class="wp-block-column page__side-nav">   
+                <?php echo($side_nav); ?>
+            </div>
+            <div class="wp-block-column page__content">   
+        <?php } ?>
+
         <?php
         while (have_posts()) {
             the_post();
-
             get_template_part('template-parts/content', 'page');
 
             // If comments are open or we have at least one comment, load up the comment template.
@@ -32,6 +88,10 @@ get_header();
             }
         } // End of the loop.
         ?>
+        <?php if ($has_side_nav) { ?>
+            </div>
+            </div><!--end of .wp-block-columns -->
+        <?php } ?>
 
     </main><!-- #main -->
 

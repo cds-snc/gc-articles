a239c7e978d9169a98e762a24c01c3b3cd0a8deb
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2021-11-24 10:41:37 -0500

    Remove FlashMessage and wp php sessions plugin

--- wordpress/wp-content/mu-plugins/load_muplugins.php
+++ wordpress/wp-content/mu-plugins/load_muplugins.php
@@ -7,13 +7,6 @@
  * @package cds-snc-base
  */
 
-use CDS\Utils;
-
+// leaving this here so this loads really early
 require_once __DIR__ . '/../../vendor/autoload.php';
 
-
-/* @TODO: need to not enable in test env
-if (!Utils::isWpEnv()) {
-    require WPMU_PLUGIN_DIR . '/login-lockdown/loginlockdown.php';
-}
-*/

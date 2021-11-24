691a70aa8914a63f8ebf6076844ab8cfb42643fc
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2021-11-24 15:24:30 -0500

    make sure nobody's logged in first

--- cypress/support/commands/login-user.js
+++ cypress/support/commands/login-user.js
@@ -1,8 +1,9 @@
 Cypress.Commands.add('login', (username = 'admin', password = 'password') => {
+  cy.clearCookies();
   cy.visit('/login');
   // somehow we need to wait for some time before entering the credentials
   // eslint-disable-next-line cypress/no-unnecessary-waiting
-  cy.wait(600);
+  cy.wait(500);
   cy.get('#user_login').clear().type(username);
   cy.get('#user_pass').clear().type(password);
   cy.get('#wp-submit').click({ force: true });

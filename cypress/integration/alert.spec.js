bf3ffee313cac5d7713b34a12eb3ac84a1fe32c2
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2021-11-25 13:39:56 -0500

    add a bunch of screenshots

--- cypress/integration/alert.spec.js
+++ cypress/integration/alert.spec.js
@@ -5,13 +5,15 @@ const NEW_TAB_REL_DEFAULT_VALUE = 'noreferrer noopener';
 describe('Alert Block', () => {
     beforeEach(() => {
         cy.login();
+        cy.screenshot();
         cy.createNewPost();
+        cy.screenshot();
     });
 
     it('Alert block should be initialized with default attributes', () => {
         cy.insertAlertBlock();
-
+        cy.screenshot();
         cy.selectAlertBlock();
-
+        cy.screenshot();
     });
 });
--- cypress/support/commands/login-user.js
+++ cypress/support/commands/login-user.js
@@ -1,10 +1,12 @@
 Cypress.Commands.add('login', (username = 'admin', password = 'password') => {
   cy.visit('/login');
+  cy.screenshot();
   // somehow we need to wait for some time before entering the credentials
   // eslint-disable-next-line cypress/no-unnecessary-waiting
   cy.wait(500);
   cy.get('#user_login').clear().type(username);
   cy.get('#user_pass').clear().type(password);
   cy.get('#wp-submit').click({ force: true });
+  cy.screenshot();
 });
 

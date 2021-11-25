1a28742ff3c43d1c45629944f8d72fb026bc8e78
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2021-11-25 15:42:04 -0500

    visit home first

--- cypress/integration/alert.spec.js
+++ cypress/integration/alert.spec.js
@@ -4,16 +4,13 @@ const NEW_TAB_REL_DEFAULT_VALUE = 'noreferrer noopener';
 
 describe('Alert Block', () => {
     beforeEach(() => {
+        cy.visit('/');
         cy.login('admin', 'password');
-        cy.screenshot();
         cy.createNewPost();
-        cy.screenshot();
     });
 
     it('Alert block should be initialized with default attributes', () => {
         cy.insertAlertBlock();
-        cy.screenshot();
         cy.selectAlertBlock();
-        cy.screenshot();
     });
 });

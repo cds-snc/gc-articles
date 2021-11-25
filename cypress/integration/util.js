64c8e89e34cbf23b85f85c08a2d245954ddc6731
Paul Craig <paul@pcraig3.ca>
2021-11-25 10:10:38 -0500

    Add test to add 2 pages and check for a a side nav

--- cypress/integration/util.js
+++ cypress/integration/util.js
@@ -5,3 +5,13 @@ export const addArticle = (text) => {
     cy.get("body").type('{cmd}s');
     cy.wait(1000);
 }
+
+export const addPage = (text, title = "Title", childPage) => {
+    cy.visit("/wp-admin/post-new.php?post_type=page");
+    cy.setPostContent(text);
+    cy.wait(1000);
+    cy.get("textarea#post-title-0").type(title);
+    cy.wait(1000);
+    cy.get("body").type('{cmd}s');
+    cy.wait(1000);
+}

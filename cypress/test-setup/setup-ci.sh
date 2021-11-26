9dccc02e9e3ddd6c19a4e62c4f9e21e648e6149b
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2021-11-26 11:24:10 -0500

    don't activate gutenberg plugin

--- cypress/test-setup/setup-ci.sh
+++ cypress/test-setup/setup-ci.sh
@@ -13,7 +13,7 @@ wp-env clean
 wp-env run tests-cli wp option delete list_values
 wp-env run tests-cli wp option set list_values --format=json < ./cypress/fixtures/notify-list-data.json
 wp-env run tests-cli wp theme activate cds-default
-wp-env run tests-cli wp plugin activate sitepress-multilingual-cms cds-base two-factor gutenberg;
+wp-env run tests-cli wp plugin activate sitepress-multilingual-cms cds-base two-factor;
 wp-env run tests-cli wp plugin activate s3-uploads wps-hide-login disable-user-login;
 
 wp-env run tests-cli wp option update permalink_structure "/%postname%/";

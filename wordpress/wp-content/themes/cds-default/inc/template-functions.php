1655dba655d9a273157b51abadad23d682616f31
Tim Arney <tim@line37.com>
2021-11-29 10:25:53 -0500

    fix which varibables get used and where

--- wordpress/wp-content/themes/cds-default/inc/template-functions.php
+++ wordpress/wp-content/themes/cds-default/inc/template-functions.php
@@ -31,9 +31,9 @@ function cds_prev_next_links(): void
   <nav class="mrgn-tp-xl">
     <h2 class="wb-inv"> <?php _e('Document navigation', 'cds-snc'); ?> </h2>
     <ul class="pager">
-      <?php if ($prev_id) : ?>
+      <?php if ($next_id && $next_permalink) : ?>
       <li class="next">
-        <a id="<?php echo $prev_id ?>" href="<?php echo $next_permalink; ?>">
+        <a id="<?php echo $next_id ?>" href="<?php echo $next_permalink; ?>">
             <?php _e(
                 'Next blog post',
                 'cds-snc'
@@ -42,9 +42,9 @@ function cds_prev_next_links(): void
       </li>
       <?php endif; ?>
 
-      <?php if ($next_id) : ?>
+      <?php if ($prev_id && $prev_permalink) : ?>
       <li class="previous">
-        <a id="<?php echo $next_id ?>" href="<?php echo $prev_permalink; ?>"
+        <a id="<?php echo $prev_id ?>" href="<?php echo $prev_permalink; ?>"
            rel="prev">«&nbsp;<?php _e('Previous blog post', 'cds-snc'); ?></a>
       </li>
       <?php endif; ?>

90eaf92fce34197fd9a47dab83a97698ac17566c
Tim Arney <tim@line37.com>
2021-12-02 08:32:35 -0500

    add section id

--- wordpress/wp-content/themes/cds-default/index.php
+++ wordpress/wp-content/themes/cds-default/index.php
@@ -18,7 +18,7 @@ declare(strict_types=1);
 get_header();
 ?>
 
-    <main property="mainContentOfPage" class="index container" resource="#wb-main" typeof="WebPageElement">
+    <main id="primary" property="mainContentOfPage" class="index container" resource="#wb-main" typeof="WebPageElement">
 
         <?php
         if (have_posts()) {

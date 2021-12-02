dae9a56554a4da29b906671b5fe843b5cf1179ef
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2021-12-02 15:05:06 -0500

    Try static html file (#345)
    * copy in a default .htaccess to test volume mount
    
    * Try using a static .html file
    
    * fix

--- infrastructure/terragrunt/aws/load-balancer/load-balancer.tf
+++ infrastructure/terragrunt/aws/load-balancer/load-balancer.tf
@@ -36,7 +36,7 @@ resource "aws_lb_target_group" "wordpress" {
     interval            = 10
     port                = 443
     protocol            = "HTTPS"
-    path                = "/health-check"
+    path                = "/index.html"
     matcher             = "200-399"
     timeout             = 5
     healthy_threshold   = 2

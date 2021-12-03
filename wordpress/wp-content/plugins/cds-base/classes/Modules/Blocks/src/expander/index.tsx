7bceec180dbc11323aada6f54003d25b8cf752bc
Paul Craig <paul@pcraig3.ca>
2021-12-03 15:04:58 -0500

    Make "expander" element true to life
    Added better markup and CSS for the expander element, and then
    placed the inner "title" block with an inner block.
    
    Expanders can now house all the other blocks:
    - headings
    - lists
    - images
    
    Whatever you want.
    
    The only annoying thing is that they will open and close during
    editing, which I don't know how to stop.

--- wordpress/wp-content/plugins/cds-base/classes/Modules/Blocks/src/expander/index.tsx
+++ wordpress/wp-content/plugins/cds-base/classes/Modules/Blocks/src/expander/index.tsx
@@ -1,43 +1,32 @@
 import * as React from 'react';
 import { __ } from "@wordpress/i18n";
 import { registerBlockType } from "@wordpress/blocks";
-import { useBlockProps, RichText } from "@wordpress/block-editor";
+import { useBlockProps, RichText, InnerBlocks } from "@wordpress/block-editor";
 
 registerBlockType("cds-snc/expander", {
   title: __("Expander", "cds-snc"),
   icon: "megaphone",
   category: "layout",
   attributes: {
-    content: {
-      type: "string",
-      source: "html",
-      selector: "div",
-    },
     title: {
       type: "string",
       source: "html",
-      selector: "h3",
+      selector: "summary",
     },
   },
 
   edit({ attributes, setAttributes }) {
     return (
-      <>
-        <RichText
-          tagName="h3" // The tag here is the element output and editable in the admin
-          value={attributes.title} // Any existing content, either from the database or an attribute default
-          allowedFormats={[]} // Allow the content to be made bold or italic, but do not allow other formatting options
-          onChange={(title) => setAttributes({ title })} // Store updated content as a block attribute
-          placeholder={__("Title", "cds-snc")} // Display this text before any content has been added by the user
-        />
-        <RichText
-          tagName="div" // The tag here is the element output and editable in the admin
-          value={attributes.content} // Any existing content, either from the database or an attribute default
-          // allowedFormats={["core/bold", "core/italic", "core/list"]} // Allow the content to be made bold or italic, but do not allow other formatting options
-          onChange={(content) => setAttributes({ content })} // Store updated content as a block attribute
-          placeholder={__("Summary", "cds-snc")} // Display this text before any content has been added by the user
-        />
-      </>
+        <details>
+          <RichText
+            tagName="summary" // The tag here is the element output and editable in the admin
+            value={attributes.title} // Any existing content, either from the database or an attribute default
+            allowedFormats={[]}
+            onChange={(title) => setAttributes({ title })} // Store updated content as a block attribute
+            placeholder={__("Title", "cds-snc")} // Display this text before any content has been added by the user
+          />
+          <InnerBlocks />
+      </details>
     );
   },
 
@@ -47,13 +36,9 @@ registerBlockType("cds-snc/expander", {
     return (
       <details>
         <summary>
-          <h3>{attributes.title}</h3>
+          {attributes.title}
         </summary>
-        <RichText.Content
-          {...blockProps}
-          tagName="div"
-          value={attributes.content}
-        />
+        <InnerBlocks.Content />
       </details>
     );
   },

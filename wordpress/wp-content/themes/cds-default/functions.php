ab844bb669b92a07e72cc2b5e3a23ec5ed87a916
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2021-12-09 12:39:42 -0500

    version bump 2.7.0 (#385)

--- wordpress/wp-content/themes/cds-default/functions.php
+++ wordpress/wp-content/themes/cds-default/functions.php
@@ -17,7 +17,7 @@ require_once __DIR__ . '/inc/template-filters.php';
 // phpcs:disable
 if (!defined('_S_VERSION')) {
     // Replace the version number of the theme on each release.
-    define('_S_VERSION', '2.6.0');
+    define('_S_VERSION', '2.7.0');
 }
 
 if (!defined('THEME_NAMESPACE')) {

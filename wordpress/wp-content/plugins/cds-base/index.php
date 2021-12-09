36c4e267886db3c5b13df1c588c0557d8c361dd6
Tim Arney <timarney@users.noreply.github.com>
2021-12-09 14:08:51 -0500

    version bump 2.7.1 (#388)

--- wordpress/wp-content/plugins/cds-base/index.php
+++ wordpress/wp-content/plugins/cds-base/index.php
@@ -4,7 +4,7 @@
  * Plugin Name: CDS-SNC Base
  * Plugin URI: https://github.com/cds-snc/gc-articles
  * Description: Custom Block setup and other overrides
- * Version: 2.7.0
+ * Version: 2.7.1
  * Update URI: false
  * Author: CDS-SNC
  *
@@ -23,7 +23,7 @@ if (!defined('BASE_PLUGIN_NAME')) {
 }
 
 if (!defined('BASE_PLUGIN_NAME_VERSION')) {
-    define('BASE_PLUGIN_NAME_VERSION', '2.7.0');
+    define('BASE_PLUGIN_NAME_VERSION', '2.7.1');
 }
 
 if (is_multisite()) {

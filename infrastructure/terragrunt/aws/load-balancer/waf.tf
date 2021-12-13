f737b0169d040d96418aed575b507b1894beef15
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2021-12-13 16:58:08 -0500

    re-add rules we commented out for capacity reasons

--- infrastructure/terragrunt/aws/load-balancer/waf.tf
+++ infrastructure/terragrunt/aws/load-balancer/waf.tf
@@ -343,49 +343,49 @@ resource "aws_wafv2_web_acl" "wordpress_waf" {
     }
   }
 
-  # rule {
-  #   name     = "AWSManagedRulesAmazonIpReputationList"
-  #   priority = 7
+  rule {
+    name     = "AWSManagedRulesAmazonIpReputationList"
+    priority = 8
 
-  #   override_action {
-  #     none {}
-  #   }
+    override_action {
+      none {}
+    }
 
-  #   statement {
-  #     managed_rule_group_statement {
-  #       name        = "AWSManagedRulesAmazonIpReputationList"
-  #       vendor_name = "AWS"
-  #     }
-  #   }
+    statement {
+      managed_rule_group_statement {
+        name        = "AWSManagedRulesAmazonIpReputationList"
+        vendor_name = "AWS"
+      }
+    }
 
-  #   visibility_config {
-  #     cloudwatch_metrics_enabled = true
-  #     metric_name                = "AWSManagedRulesAmazonIpReputationList"
-  #     sampled_requests_enabled   = true
-  #   }
-  # }
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "AWSManagedRulesAmazonIpReputationList"
+      sampled_requests_enabled   = true
+    }
+  }
 
-  # rule {
-  #   name     = "WordpressRateLimit"
-  #   priority = 101
+  rule {
+    name     = "WordpressRateLimit"
+    priority = 101
 
-  #   action {
-  #     block {}
-  #   }
+    action {
+      block {}
+    }
 
-  #   statement {
-  #     rate_based_statement {
-  #       limit              = 10000
-  #       aggregate_key_type = "IP"
-  #     }
-  #   }
+    statement {
+      rate_based_statement {
+        limit              = 10000
+        aggregate_key_type = "IP"
+      }
+    }
 
-  #   visibility_config {
-  #     cloudwatch_metrics_enabled = true
-  #     metric_name                = "WordpressRateLimit"
-  #     sampled_requests_enabled   = true
-  #   }
-  # }
+    visibility_config {
+      cloudwatch_metrics_enabled = true
+      metric_name                = "WordpressRateLimit"
+      sampled_requests_enabled   = true
+    }
+  }
 
   visibility_config {
     cloudwatch_metrics_enabled = true

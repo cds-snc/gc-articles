418d104df0916ee658946e5f612267fd3d77cabc
Paul Craig <paul@pcraig3.ca>
2021-12-17 12:42:44 -0500

    Add "Collections" option to admin header bar
    If you are a non-superadmin and signed up to multiple sites, you
    previously didn't have a good way to orient yourself or get
    around on your site.
    
    Adding a "Collections" drop-down so that you can see your existing
    sites you are signed up for.
    
    Sources
    - https://github.com/WordPress/WordPress/blob/40cd53a5737346e8766a35ede063b6202978a378/wp-includes/class-wp-admin-bar.php#L643
    - See https://github.com/WordPress/WordPress/blob/f70fb51f99973274809caa80135d87f78b66b86a/wp-includes/admin-bar.php#L496

--- wordpress/wp-content/plugins/cds-base/classes/Modules/Cleanup/AdminBar.php
+++ wordpress/wp-content/plugins/cds-base/classes/Modules/Cleanup/AdminBar.php
@@ -9,8 +9,80 @@ class AdminBar
     public function __construct()
     {
         add_action('admin_bar_menu', [$this, 'removeFromAdminBar'], 2147483647);
-
         add_action('wp_before_admin_bar_render', [$this, 'removeFromAdminBarBefore'], 99);
+
+        add_action( 'admin_bar_menu', [$this, 'addCollections'], 21 );
+    }
+
+    public function addCollections($wp_admin_bar): void
+    {
+        // if less than 2 collections or a superadmin, skip this
+        if (count($wp_admin_bar->user->blogs) < 2 || is_super_admin()) {
+            return;
+        }
+
+        $root_id = 'my-collections';
+        $wp_admin_bar->add_node([
+            'id'    =>  $root_id,
+            'title' => '<div class="ab-item"><span class="ab-icon"></span>' . __(
+                'My Collections',
+                'cds-snc'
+            ) . '</div>'
+        ]);
+
+        $current_site_id = get_current_blog_id();
+
+        /* Inspiration for this loop comes from 'wp_admin_bar_my_sites_menu' function in core WP */
+        foreach ((array) $wp_admin_bar->user->blogs as $blog) {
+            switch_to_blog($blog->userblog_id);
+
+            if (has_site_icon()) {
+                $blavatar = sprintf(
+                    '<img class="blavatar" src="%s" srcset="%s 2x" alt="" width="16" height="16" />',
+                    esc_url(get_site_icon_url(16)),
+                    esc_url(get_site_icon_url(32))
+                );
+            } else {
+                $blavatar = '<div class="blavatar"></div>';
+            }
+
+            $blogname = $blog->blogname;
+
+            if (!$blogname) {
+                $blogname = preg_replace('#^(https?://)?(www.)?#', '', get_home_url());
+            }
+
+            $menu_id = 'site-' . $blog->userblog_id;
+            $is_current = $blog->userblog_id === $current_site_id ? __(' (current)', 'cds-snc') : '';
+
+            $wp_admin_bar->add_node(
+                array(
+                    'parent' => $root_id,
+                    'id'     => $menu_id,
+                    'title'  => $blavatar . $blogname . $is_current,
+                    'href'   => admin_url(),
+                )
+            );
+
+            $wp_admin_bar->add_node(
+                array(
+                    'parent' => $menu_id,
+                    'id'     => $menu_id . '-d',
+                    'title'  => __('Dashboard', 'cds-snc'),
+                    'href'   => admin_url(),
+                )
+            );
+
+            $wp_admin_bar->add_node(
+                array(
+                    'parent' => $menu_id,
+                    'id'     => $menu_id . '-v',
+                    'title'  => __('Visit', 'cds-snc'),
+                    'href'   => home_url('/'),
+                )
+            );
+        }
+        restore_current_blog();
     }
 
     public function removeFromAdminBarBefore(): void

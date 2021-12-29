32ec579c4d214ecc82bac2681797a69a4f18b60d
Paul Craig <paul_craig_16@hotmail.com>
2021-12-29 10:48:26 -0700

    Hide default maple leaf and show icon if a default icon is set (#431)
    Also stop the stupid transition effect on hover for the icon.

--- wordpress/wp-content/plugins/cds-base/classes/Modules/Cleanup/AdminStyles.php
+++ wordpress/wp-content/plugins/cds-base/classes/Modules/Cleanup/AdminStyles.php
@@ -9,6 +9,7 @@ class AdminStyles
     public function __construct()
     {
         add_action('admin_enqueue_scripts', [$this, 'enqueueStyles'], 99);
+        add_action('admin_head', [$this, 'gutenbergIconCSS'], 99);
     }
 
     public function enqueueStyles()
@@ -27,5 +28,32 @@ class AdminStyles
             [],
             BASE_PLUGIN_NAME_VERSION,
         );
+    }
+
+    public function gutenbergIconCSS()
+    {
+        ob_start();
+        ?>
+        <style>
+            .edit-post-fullscreen-mode-close.has-icon svg,
+            .edit-post-fullscreen-mode-close.has-icon img {
+                display:none;
+            }
+
+            .edit-post-fullscreen-mode-close.has-icon:before {
+                display: inline-block;
+            }
+        </style>
+        <?php
+
+        $show_maple_leaf_css = ob_get_clean();
+        ob_end_clean();
+
+        $hasIcon = get_site_icon_url() !== "";
+
+        if (!$hasIcon) {
+            // If there is an icon, show the default maple leaf and hide the WP logo
+            echo $show_maple_leaf_css;
+        }
     }
 }

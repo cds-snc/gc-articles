3f67bfe53063e01327d590cb33f918c4bc39b550
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2022-01-10 14:01:12 -0500

    handle language path prefix

--- wordpress/wp-content/plugins/cds-base/classes/Modules/Cache/Cache.php
+++ wordpress/wp-content/plugins/cds-base/classes/Modules/Cache/Cache.php
@@ -6,16 +6,40 @@ namespace CDS\Modules\Cache;
 
 class Cache
 {
+    const locale_fr = 'fr_FR';
+
     public static function register()
+    {
+        $instance = new self();
+
+        $instance->registerInvalidationPaths();
+    }
+
+    protected function registerInvalidationPaths()
     {
         add_filter('c3_invalidation_items', function ($items, $post) {
             global $blog_id;
-            $site = get_blog_details(array( 'blog_id' => $blog_id ));
+            $site       = get_blog_details(array('blog_id' => $blog_id));
             $sitePrefix = $site->path;
 
+            $localePrefix = $this->getLocalePrefixForPost($post);
+
             return array_merge($items, [
-                "{$sitePrefix}wp-json/wp/v2/{$post->post_type}s/?slug={$post->post_name}",
+                "{$sitePrefix}{$localePrefix}wp-json/wp/v2/{$post->post_type}s/?slug={$post->post_name}",
             ]);
         }, 10, 2);
+    }
+
+    protected function getLocalePrefixForPost($post): string
+    {
+        if($language_information = apply_filters('wpml_post_language_details', null, $post->ID)) {
+            $locale = $language_information['locale'];
+
+            if ($locale === self::locale_fr) {
+                return 'fr/';
+            }
+        }
+
+        return '';
     }
 }

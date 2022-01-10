d86ea94f294f73a1d12098f9f3c615387219500d
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2022-01-10 14:33:23 -0500

    Configure c3 to clear json routes (#446)
    * load master branch of plugin
    
    * Configure C3 filter to add wp-json paths
    
    * fix path
    
    * add site prefix, post type, and slug
    
    * fix formatting
    
    * no need to instantiate
    
    * handle language path prefix
    
    * slight refactor
    
    * formatting

---
+++ wordpress/wp-content/plugins/cds-base/classes/Modules/Cache/Cache.php
@@ -0,0 +1,51 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CDS\Modules\Cache;
+
+class Cache
+{
+    const LOCALE_FR = 'fr_FR';
+
+    public static function register()
+    {
+        $instance = new self();
+
+        $instance->registerInvalidationPaths();
+    }
+
+    protected function registerInvalidationPaths()
+    {
+        add_filter('c3_invalidation_items', function ($items, $post) {
+
+            $sitePrefix = $this->getSitePrefixForPost();
+            $localePrefix = $this->getLocalePrefixForPost($post);
+
+            return array_merge($items, [
+                "{$sitePrefix}{$localePrefix}wp-json/wp/v2/{$post->post_type}s/?slug={$post->post_name}",
+            ]);
+        }, 10, 2);
+    }
+
+    protected function getSitePrefixForPost(): string
+    {
+        global $blog_id;
+        $site = get_blog_details(array('blog_id' => $blog_id));
+
+        return $site->path;
+    }
+
+    protected function getLocalePrefixForPost($post): string
+    {
+        if ($language_information = apply_filters('wpml_post_language_details', null, $post->ID)) {
+            $locale = $language_information['locale'];
+
+            if ($locale === self::LOCALE_FR) {
+                return 'fr/';
+            }
+        }
+
+        return '';
+    }
+}

cfd30ebfcbd8521fb8a9e0d832b206fe77c0c77f
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2022-01-19 13:50:46 -0500

    increase wordpress memory limit

--- wordpress/wp-config.php
+++ wordpress/wp-config.php
@@ -154,6 +154,8 @@ define('OTGS_DISABLE_AUTO_UPDATES', true);
 define('WP_AUTO_UPDATE_CORE', false);
 
 define('JWT_AUTH_SECRET_KEY', getenv_docker('JWT_AUTH_SECRET_KEY', 'tQ;XnD#UmY2A*O,LIm(:NL|4c=R|3t~QD/3p{7CBKRz^eepfib9q-PHr7ZMZG$uz'));
+
+define( 'WP_MEMORY_LIMIT', '256M' );
 /* That's all, stop editing! Happy publishing. */
 
 /** Absolute path to the WordPress directory. */

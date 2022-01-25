2be211b8f4a133f0e871f5033d7f7b249bbd71ba
Tim Arney <tim@line37.com>
2022-01-25 09:48:35 -0500

    collection to sites

--- wordpress/wp-content/plugins/cds-base/classes/Modules/Cleanup/SitesToCollections.php
+++ wordpress/wp-content/plugins/cds-base/classes/Modules/Cleanup/SitesToCollections.php
@@ -11,14 +11,6 @@ class SitesToCollections
 
     public function __construct()
     {
-        if (is_admin()) {
-            // note we may run into performance issues here so limiting to admin interface only
-            add_filter('gettext', [$this, 'changeSitesLabel'], 10, 3);
-        }
-
-        // for non wp-admin replace only in Admin Bar
-        add_action('wp_before_admin_bar_render', [$this, 'changeSitesLabelAdminBar'], 99, 0);
-
         $this->adminEmailMessage = __('Admin Email <strong>must be</strong> a current Super Admin', 'cds-snc');
         $this->collectionMessage = __('or else a Collection won’t be created', 'cds-snc');
 
@@ -26,31 +18,6 @@ class SitesToCollections
         add_action('network_site_new_form', [$this, 'editNewCollectionForm']);
         // throw an error if a new user is attepted to be created when adding a New Collection
         add_action('pre_network_site_new_created_user', [$this, 'dieIfNewUser']);
-    }
-
-    public function changeSitesLabel($translation, $text, $domain): string
-    {
-        if ($domain !== "default") {
-            // only look for default wp translations
-            return $translation;
-        }
-
-        if (str_contains($translation, '###')) {
-            return $translation;
-        }
-
-        return str_ireplace('Site', "Collection", $text);
-    }
-
-    public function changeSitesLabelAdminBar(): void
-    {
-        if (is_super_admin()) {
-            return;
-        }
-
-        // remove "My Sites"
-        global $wp_admin_bar;
-        $wp_admin_bar->remove_menu('my-sites');
     }
 
     public function editNewCollectionForm(): void

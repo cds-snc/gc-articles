d7745a23ab59c469fd0556c2a3cbfeda5883cd7b
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2022-01-31 15:48:45 -0500

    try quoting

--- cypress/test-setup/setup-ci.sh
+++ cypress/test-setup/setup-ci.sh
@@ -11,7 +11,7 @@ echo "Didn't find $DB_BACKUP will save one for next time"
 
 wp-env clean
 wp-env run tests-cli wp option delete list_values
-# wp-env run tests-cli wp option add list_values --format=json < ./cypress/fixtures/notify-list-data.json
+wp-env run tests-cli "wp option set list_values --format=json < ./cypress/fixtures/notify-list-data.json"
 wp-env run tests-cli wp theme activate cds-default
 wp-env run tests-cli wp plugin activate sitepress-multilingual-cms cds-base two-factor;
 wp-env run tests-cli wp plugin activate s3-uploads wps-hide-login disable-user-login;

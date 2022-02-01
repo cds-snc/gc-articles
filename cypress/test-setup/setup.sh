be96fabc6d88dd01d5ec49b9b0b61604653ba989
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2022-02-01 14:05:25 -0500

    don't load sql

--- cypress/test-setup/setup.sh
+++ cypress/test-setup/setup.sh
@@ -2,12 +2,12 @@
 
 DB_BACKUP=test_run_dump.sql
 
-echo "Looking for $DB_BACKUP"
-if wp-env run tests-cli wp db import "$DB_BACKUP"; then
-    echo "Found $DB_BACKUP and imported it"
-    exit 0
-fi
-echo "Didn't find $DB_BACKUP will save one for next time"
+#echo "Looking for $DB_BACKUP"
+#if wp-env run tests-cli wp db import "$DB_BACKUP"; then
+#    echo "Found $DB_BACKUP and imported it"
+#    exit 0
+#fi
+#echo "Didn't find $DB_BACKUP will save one for next time"
 
 wp-env run tests-cli wp option delete list_values
 wp-env run tests-cli wp option set list_values --format=json < ./cypress/fixtures/notify-list-data.json

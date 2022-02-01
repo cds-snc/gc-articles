d4c2d7102d5b28441f545022c5cc91a0620999ef
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2022-02-01 10:40:09 -0500

    undo timeout

--- cypress/integration/alert.spec.js
+++ cypress/integration/alert.spec.js
@@ -4,13 +4,7 @@ const NEW_TAB_REL_DEFAULT_VALUE = 'noreferrer noopener';
 
 describe('Alert Block', () => {
     before(() => {
-        cy.exec('npm run wp-env:test:setup', {
-            timeout: 20000
-        }).then((result) => {
-            cy.log("huzzah");
-            cy.log(result.stdout);
-        })
-
+        cy.exec('npm run wp-env:test:setup')
     });
     
     beforeEach(() => {

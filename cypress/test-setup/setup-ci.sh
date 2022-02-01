518f67def58f4c9ca6cbd0db234af5bbdc2a65f5
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2022-02-01 10:40:31 -0500

    undo comment out

--- cypress/test-setup/setup-ci.sh
+++ cypress/test-setup/setup-ci.sh
@@ -13,10 +13,10 @@ wp-env clean
 wp-env run tests-cli wp option delete list_values
 wp-env run tests-cli wp option set list_values --format=json < ./cypress/fixtures/notify-list-data.json
 wp-env run tests-cli wp theme activate cds-default
-#wp-env run tests-cli wp plugin activate sitepress-multilingual-cms cds-base two-factor;
-#wp-env run tests-cli wp plugin activate s3-uploads wps-hide-login disable-user-login;
-#wp-env run tests-cli wp plugin activate wordpress-seo wordpress-seo-premium wp-rest-api-v2-menus;
-#wp-env run tests-cli wp plugin activate jwt-authentication-for-wp-rest-api;
+wp-env run tests-cli wp plugin activate sitepress-multilingual-cms cds-base two-factor;
+wp-env run tests-cli wp plugin activate s3-uploads wps-hide-login disable-user-login;
+wp-env run tests-cli wp plugin activate wordpress-seo wordpress-seo-premium wp-rest-api-v2-menus;
+wp-env run tests-cli wp plugin activate jwt-authentication-for-wp-rest-api;
 
 wp-env run tests-cli wp option update permalink_structure "/%postname%/";
 wp-env run tests-cli "wp option add LIST_MANAGER_NOTIFY_SERVICES 'Les Articles GC Articles~gc-articles-fb26a6b5-57aa-4cc2-85fe-3053ed344fe8-30569ea9-362b-41c4-a811-842ccf3db3dc'"

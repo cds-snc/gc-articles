dbacdd483165c152f7c0de231e6f515aacd3ea93
Paul Craig <paul_craig_16@hotmail.com>
2022-02-03 11:55:35 -0500

    Add language switcher to Admin (#513)
    * Augment the GC Articles top menu link
    
    The top link itself now has the name of the site included, and toggles
    between the admin interface and the public interface.
    
    It also has a drop-down with 2 options, so all our bases are covered.
    
    * Add option to "Edit site" for superadmins
    
    Adding this option means we can totally remove the "site-name" default
    heading that was closely overlapping with this one.
    
    * Add language switcher to the top #wpadminbar
    
    Unlike the language switcher on the public facing site, this one
    changes the language in the wordpress interface.
    
    Getting this to work was fairly tricky to do because the
    admin bar is for links, not for putting random HTML into, but in
    the end it seems to have worked out okay.
    
    The other plugin created an extra script file and made it an Ajax
    request, but I didn't want to do that.
    
    * Use chevron instead of colon as separator in admin bar
    
    * Hide the Admin language switcher if not in the admin interface
    
    * Update default languages to English Canadian and French Canadian

--- wordpress/wp-content/plugins/cds-base/classes/Modules/Cleanup/AdminBar.php
+++ wordpress/wp-content/plugins/cds-base/classes/Modules/Cleanup/AdminBar.php
@@ -12,6 +12,117 @@ class AdminBar
         add_action('wp_before_admin_bar_render', [$this, 'removeFromAdminBarBefore'], 99);
 
         add_action('admin_bar_menu', [$this, 'addCollections'], 21);
+        add_action('admin_bar_menu', [$this, 'addAdminToggle'], 21);
+
+        add_action('admin_bar_menu', [$this, 'addLanguageSwitcher'], 21);
+        add_action('admin_post_cds_change_lang', [$this, 'handleLanguageSwitcherResponse']);
+    }
+
+    public function addAdminToggle($wp_admin_bar): void
+    {
+        $menu_id = 'cds-home';
+        // "is_admin" checks if viewing an admin page, it's not a role check
+        $home_url = is_admin() ? home_url('/') : admin_url();
+
+        $wp_admin_bar->add_node([
+            'id'    => $menu_id,
+            'title' => '<div class="ab-item"><span class="ab-icon"></span>' . __(
+                'GC Articles',
+                'cds-snc'
+            ) . ' › ' . get_bloginfo('name') . '</div>',
+            'href'  => $home_url,
+        ]);
+
+        // Add an option to visit the site.
+        $wp_admin_bar->add_node(
+            array(
+                'parent' => $menu_id,
+                'id'     => $menu_id . '-view-site',
+                'title'  => __('Visit Site'),
+                'href'   => home_url('/'),
+            )
+        );
+
+        // Add an option to visit the site's "settings" page for superadmins
+        if (is_multisite() && current_user_can('manage_sites')) {
+            $wp_admin_bar->add_node(
+                array(
+                    'parent' => $menu_id,
+                    'id'     => $menu_id . '-edit-site',
+                    'title'  => __('Edit Site'),
+                    'href'   => network_admin_url('site-info.php?id=' . get_current_blog_id()),
+                )
+            );
+        }
+
+        // Add an option to visit the dashboard.
+        $wp_admin_bar->add_node(
+            array(
+                'parent' => $menu_id,
+                'id'     => $menu_id . '-admin-dashboard',
+                'title'  => __('Admin dashboard'),
+                'href'   => admin_url(),
+            )
+        );
+    }
+
+    public function addLanguageSwitcher($wp_admin_bar): void
+    {
+        if (!is_admin()) {
+            return;
+        }
+
+        $user_locale = get_user_locale();
+        $translation_locale = str_contains($user_locale, "en") ? "fr_CA" : "en_CA";
+        // Not using i18n function for these because we don't want to translate them
+        $translation_locale_name = str_contains($user_locale, "en") ? "Français" : "English";
+
+        ob_start();
+        ?>
+        <form action="<?php echo esc_url(admin_url('admin-post.php')); ?>" method="post" id="cds_change_lang_form" >
+            <input type="hidden" name="action" value="cds_change_lang">
+            <?php wp_nonce_field('change_lang', "cds_change_lang_nonce"); ?>
+            <input name="locale" type="hidden" value="<?php echo $translation_locale; ?>" /> 
+            <input
+                type="submit"
+                name="submit"
+                id="submit-change-lang"
+                value="<?php echo $translation_locale_name; ?>"
+                lang="<?php echo $translation_locale; ?>"
+            />
+        </form>
+        <?php
+
+        $cds_change_lang_form = ob_get_contents();
+        ob_end_clean();
+
+        $wp_admin_bar->add_node([
+            'id'    => 'change-lang',
+            'parent' => 'top-secondary',
+            'title' => $cds_change_lang_form,
+            'href'  => '',
+        ]);
+    }
+
+    public function handleLanguageSwitcherResponse(): void
+    {
+        if (
+            isset($_POST['cds_change_lang_nonce']) &&
+            check_admin_referer('change_lang', 'cds_change_lang_nonce')
+        ) {
+            // sanitize the input
+            $lang = sanitize_key($_POST['locale']);
+            $user_id = get_current_user_id();
+            wp_update_user(['ID' => $user_id, 'locale' => $lang]);
+            wp_redirect(esc_url_raw($_POST['_wp_http_referer']));
+            die();
+        } else {
+            wp_die(
+                __('Invalid nonce', 'cds-snc'),
+                __('Error', 'cds-snc'),
+                ['response' => 403, 'back_link' => admin_url()]
+            );
+        }
     }
 
     public function addCollections($wp_admin_bar): void
@@ -93,6 +204,7 @@ class AdminBar
     public function removeFromAdminBar($wp_admin_bar): void
     {
         $wp_admin_bar->remove_node('new-content');
+        $wp_admin_bar->remove_node('site-name');
 
         if (is_super_admin()) {
             return;
@@ -101,7 +213,6 @@ class AdminBar
         $wp_admin_bar->remove_node('updates');
         $wp_admin_bar->remove_node('comments');
         $wp_admin_bar->remove_node('wp-logo');
-        $wp_admin_bar->remove_node('site-name');
         $wp_admin_bar->remove_node('customize');
 
         /* plugins */
@@ -114,15 +225,6 @@ class AdminBar
         $wp_admin_bar->add_node([
             'id'    => 'my-account',
             'title' => $newtext,
-        ]);
-
-        $wp_admin_bar->add_node([
-            'id'    => 'cds-home',
-            'title' => '<div class="ab-item"><span class="ab-icon"></span>' . __(
-                'GC Articles',
-                'cds-snc'
-            ) . '</div>',
-            'href'  => admin_url(),
         ]);
 
         $wp_admin_bar->remove_menu('my-sites');

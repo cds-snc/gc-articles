f853beb06df22db570c567c45c3f49b951bf2d47
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2022-02-03 13:03:00 -0500

    Cleanup static methods and use Class instance (#518)
    * Cleanup static methods and use class instance
    
    * move action registration
    
    * Change methods to public so they can be used in callbacks

--- wordpress/wp-content/plugins/cds-base/classes/Modules/Wpml/Wpml.php
+++ wordpress/wp-content/plugins/cds-base/classes/Modules/Wpml/Wpml.php
@@ -11,97 +11,96 @@ class Wpml
         Installer::register();
         Cleanup::register();
 
-        self::addTranslatedIDsToPages();
+        $instance = new self();
+
+        add_action('rest_api_init', [$instance, 'addTranslatedIDsToPages']);
     }
 
-    private static function getTranslatedPost($post_id, $lang)
+    public function getTranslatedPost($post_id, $lang)
     {
         return apply_filters('wpml_object_id', $post_id, 'post', false, $lang);
     }
 
-    private static function addTranslatedIDsToPages()
+    public function addTranslatedIDsToPages()
     {
-        add_action('rest_api_init', function () {
-            /**
-             * Add an 'slug_en' field field to the REST response for a page
-             * Returns a string, or 'null' if no translation provided
-             */
-            register_rest_field('page', 'slug_en', array(
-                'get_callback' => function ($post, $field_name, $request) {
-                    $translatedPostId = self::getTranslatedPost($post['id'], 'en');
-                    return is_null($translatedPostId) ? null : get_post_field('post_name', $translatedPostId);
-                },
-                'update_callback' => null,
-                'schema' => array(
-                    'description' => __('Post slug for English page.', 'cds-snc'),
-                    'type'        => 'string'
-                ),
-            ));
+        /**
+         * Add an 'slug_en' field field to the REST response for a page
+         * Returns a string, or 'null' if no translation provided
+         */
+        register_rest_field('page', 'slug_en', array(
+            'get_callback' => function ($post, $field_name, $request) {
+                $translatedPostId = $this->getTranslatedPost($post['id'], 'en');
+                return is_null($translatedPostId) ? null : get_post_field('post_name', $translatedPostId);
+            },
+            'update_callback' => null,
+            'schema' => array(
+                'description' => __('Post slug for English page.', 'cds-snc'),
+                'type'        => 'string'
+            ),
+        ));
 
-            /**
-             * Add an 'slug_fr' field field to the REST response for a page
-             * Returns an string, or 'null' if no translation provided
-             */
-            register_rest_field('page', 'slug_fr', array(
-                'get_callback' => function ($post, $field_name, $request) {
-                    $translatedPostId = self::getTranslatedPost($post['id'], 'fr');
-                    return is_null($translatedPostId) ? null : get_post_field('post_name', $translatedPostId);
-                },
-                'update_callback' => null,
-                'schema' => array(
-                    'description' => __('Post slug for French page.', 'cds-snc'),
-                    'type'        => 'string'
-                ),
-            ));
+        /**
+         * Add an 'slug_fr' field field to the REST response for a page
+         * Returns an string, or 'null' if no translation provided
+         */
+        register_rest_field('page', 'slug_fr', array(
+            'get_callback' => function ($post, $field_name, $request) {
+                $translatedPostId = $this->getTranslatedPost($post['id'], 'fr');
+                return is_null($translatedPostId) ? null : get_post_field('post_name', $translatedPostId);
+            },
+            'update_callback' => null,
+            'schema' => array(
+                'description' => __('Post slug for French page.', 'cds-snc'),
+                'type'        => 'string'
+            ),
+        ));
 
 
-            /**
-             * Add an 'id_en' field field to the REST response for a page
-             * Returns an integer id, or 'null' if no translation provided
-             */
-            register_rest_field('page', 'id_en', array(
-                'get_callback' => function ($post, $field_name, $request) {
-                    return self::getTranslatedPost($post['id'], 'en');
-                },
-                'update_callback' => null,
-                'schema' => array(
-                    'description' => __('ID for English page.', 'cds-snc'),
-                    'type'        => 'integer'
-                ),
-            ));
+        /**
+         * Add an 'id_en' field field to the REST response for a page
+         * Returns an integer id, or 'null' if no translation provided
+         */
+        register_rest_field('page', 'id_en', array(
+            'get_callback' => function ($post, $field_name, $request) {
+                return $this->getTranslatedPost($post['id'], 'en');
+            },
+            'update_callback' => null,
+            'schema' => array(
+                'description' => __('ID for English page.', 'cds-snc'),
+                'type'        => 'integer'
+            ),
+        ));
 
-            /**
-             * Add an 'id_fr' field to the REST response for a page
-             * Returns an integer id, or 'null' if no translation provided
-             */
-            register_rest_field('page', 'id_fr', array(
-                'get_callback' => function ($post, $field_name, $request) {
-                    return self::getTranslatedPost($post['id'], 'fr');
-                },
-                'update_callback' => null,
-                'schema' => array(
-                    'description' => __('ID for French page.', 'cds-snc'),
-                    'type'        => 'integer'
-                ),
-            ));
+        /**
+         * Add an 'id_fr' field to the REST response for a page
+         * Returns an integer id, or 'null' if no translation provided
+         */
+        register_rest_field('page', 'id_fr', array(
+            'get_callback' => function ($post, $field_name, $request) {
+                return $this->getTranslatedPost($post['id'], 'fr');
+            },
+            'update_callback' => null,
+            'schema' => array(
+                'description' => __('ID for French page.', 'cds-snc'),
+                'type'        => 'integer'
+            ),
+        ));
 
-            /**
-             * Adds a new endpoint to handle creating a linked page or post
-            */
+        /**
+         * Adds a new endpoint to handle creating a linked page or post
+        */
 
-            register_rest_route('cds-wpml/v1', '/translate', [
-                'methods'             => 'POST',
-                'callback'            => [new Wpml(), 'wpmlTranslatePost'],
-                'permission_callback' => function () {
-                    return current_user_can('delete_posts');
-                }
-            ]);
-        });
+        register_rest_route('cds-wpml/v1', '/translate', [
+            'methods'             => 'POST',
+            'callback'            => [$this, 'wpmlTranslatePost'],
+            'permission_callback' => function () {
+                return current_user_can('delete_posts');
+            }
+        ]);
     }
 
-    public static function wpmlTranslatePost()
+    public function wpmlTranslatePost()
     {
-
         try {
             $post_id = intval($_POST['post_id']);
             $post_type = "page";

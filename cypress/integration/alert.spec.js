3c031752246eb92c25edba2d70142cfe35fda52e
Paul Craig <paul@pcraig3.ca>
2022-02-16 16:39:34 -0500

    try new alert test

--- cypress/integration/alert.spec.js
+++ cypress/integration/alert.spec.js
@@ -1,3 +1,5 @@
+import { addArticle } from "./util";
+
 /// <reference types="Cypress" />
 
 const NEW_TAB_REL_DEFAULT_VALUE = 'noreferrer noopener';
@@ -7,13 +9,18 @@ describe.skip('Alert Block', () => {
         cy.testSetup();
     });
 
-    beforeEach(() => {
-        cy.login();
-        cy.createNewPost();
-    });
-
     it('Alert block should be initialized with default attributes', () => {
-        cy.insertAlertBlock();
-        cy.selectAlertBlock();
+        cy.addUser('gcadmin', 'secret', 'administrator');
+        cy.login('gcadmin', 'secret');
+
+        const text = "Hello from GC Admin";
+        addArticle(text)
+
+        cy.searchForBlock('Alert');
+        cy.get(
+            'button.editor-block-list-item-cds-snc-alert'
+        ).click({ force: true });
+        cy.selectBlockByName('cds-snc/alert', 0);
+
     });
 });

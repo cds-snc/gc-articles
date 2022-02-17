a2fdf4e7211574d8b9a620b9296280fb47f585ab
Paul Craig <paul@pcraig3.ca>
2022-02-17 10:31:25 -0500

    Revert changes to alert block
    I re-wrote the test to try and get it working, but it didn't help,
    so reverting to the original one.

--- cypress/integration/alert.spec.js
+++ cypress/integration/alert.spec.js
@@ -1,5 +1,3 @@
-import { addArticle } from "./util";
-
 /// <reference types="Cypress" />
 
 const NEW_TAB_REL_DEFAULT_VALUE = 'noreferrer noopener';
@@ -8,19 +6,15 @@ describe.skip('Alert Block', () => {
     before(() => {
         cy.testSetup();
     });
+    
+    beforeEach(() => {
+        cy.login();
+
+        cy.createNewPost();
+    });
 
     it('Alert block should be initialized with default attributes', () => {
-        cy.addUser('gcadmin', 'secret', 'administrator');
-        cy.login('gcadmin', 'secret');
-
-        const text = "Hello from GC Admin";
-        addArticle(text)
-
-        cy.searchForBlock('Alert');
-        cy.get(
-            'button.editor-block-list-item-cds-snc-alert'
-        ).click({ force: true });
-        cy.selectBlockByName('cds-snc/alert', 0);
-
+        cy.insertAlertBlock();
+        cy.selectAlertBlock();
     });
 });

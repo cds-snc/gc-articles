dab4c744c02f96c6a611c01c3a19cc6fef970965
Tim Arney <tim@line37.com>
2022-02-22 15:23:53 -0500

    add minor styling for form

--- list-manager/src/ListForm/ListForm.tsx
+++ list-manager/src/ListForm/ListForm.tsx
@@ -11,30 +11,33 @@ export const ListForm = ({ handler = "", formData = {} }: { handler: any, formDa
 
     return (
         <form onSubmit={handleSubmit(handler)}>
-            <div>
-                Name !
+            <div className="form-field">
+                <label htmlFor="name">Name</label>
                 <input defaultValue={getVal("name")} {...register("name", { required: true })} />
             </div>
-            <div>
-                Language
+            <div className="form-field">
+                <label htmlFor="language">Language</label>
                 <input defaultValue={getVal("language")} {...register("language", { required: true })} />
             </div>
-            <div>
-                Service Id
+            <div className="form-field">
+                <label htmlFor="service_id">Service Id</label>
                 <input defaultValue={getVal("service_id")} {...register("service_id", { required: true })} />
             </div>
 
-            <div>
-                Subscribe email template id
+            <div className="form-field">
+                <label htmlFor="subscribe_email_template_id">Subscribe email template id</label>
                 <input defaultValue={getVal("subscribe_email_template_id")} {...register("subscribe_email_template_id", { required: true })} />
             </div>
 
-            <div>
-                Subscribe redirect url
+            <div className="form-field">
+                <label htmlFor="subscribe_redirect_url">Subscribe redirect url</label>
                 <input defaultValue={getVal("subscribe_redirect_url")} {...register("subscribe_redirect_url", { required: true })} />
             </div>
 
-            <input type="submit" />
+            <div className="form-field">
+                <label htmlFor=""></label>
+                <input type="submit" />
+            </div>
         </form>
     );
 }

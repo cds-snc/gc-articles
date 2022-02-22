44e64fe8a54a0b2dd129191cb8c9625901e28ad7
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2022-02-22 10:00:48 -0500

    Use custom formatter and add notifications

--- cli/format.js
+++ cli/format.js
@@ -1,16 +1,20 @@
 import watch from "watch";
 import shell from 'shelljs';
+import notifier from 'node-notifier';
 
 watch.createMonitor('./wordpress', function (monitor) {
   monitor.files['*.php'];
 
   console.log("Watching...");
+  notifier.notify({ title: 'GC Articles', message: 'Watching...' });
 
   monitor.on("changed", function (f, curr, prev) {
     console.clear();
     console.log("File changed: " + f);
-    shell.exec(`./wordpress/vendor/bin/phpcbf -n --standard=PSR12 ${f}`)
-  })
+    const code = (shell.exec(`./wordpress/vendor/bin/phpcbf -n --standard=PSR12 ${f}`)).code;
 
-  // monitor.stop(); // Stop watching
+    if( code === 1) {
+      notifier.notify({ title: 'GC Articles', message: 'Formatting fixed: ' + f });
+    }
+  })
 })

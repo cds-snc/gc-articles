e9947aeef09afef43cb297ed8c45a3a579b82377
Tim Arney <tim@line37.com>
2022-02-22 14:43:08 -0500

    mark as requried

--- list-manager/src/ListForm/ListForm.tsx
+++ list-manager/src/ListForm/ListForm.tsx
@@ -13,25 +13,25 @@ export const ListForm = ({ handler = "", formData = {} }: { handler: any, formDa
         <form onSubmit={handleSubmit(handler)}>
             <div>
                 Name !
-                <input defaultValue={getVal("name")} {...register("name")} />
+                <input defaultValue={getVal("name")} {...register("name", { required: true })} />
             </div>
             <div>
                 Language
-                <input defaultValue={getVal("language")} {...register("language")} />
+                <input defaultValue={getVal("language")} {...register("language", { required: true })} />
             </div>
             <div>
                 Service Id
-                <input defaultValue={getVal("service_id")} {...register("service_id")} />
+                <input defaultValue={getVal("service_id")} {...register("service_id", { required: true })} />
             </div>
 
             <div>
                 Subscribe email template id
-                <input defaultValue={getVal("subscribe_email_template_id")} {...register("subscribe_email_template_id")} />
+                <input defaultValue={getVal("subscribe_email_template_id")} {...register("subscribe_email_template_id", { required: true })} />
             </div>
 
             <div>
                 Subscribe redirect url
-                <input defaultValue={getVal("subscribe_redirect_url")} {...register("subscribe_redirect_url")} />
+                <input defaultValue={getVal("subscribe_redirect_url")} {...register("subscribe_redirect_url", { required: true })} />
             </div>
 
             <input type="submit" />

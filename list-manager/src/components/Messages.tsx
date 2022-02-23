fd0f51f9dfc108b36375aefb7341640408ec43cc
Tim Arney <tim@line37.com>
2022-02-23 14:45:01 -0500

    add toast message

--- list-manager/src/components/Messages.tsx
+++ list-manager/src/components/Messages.tsx
@@ -1,10 +1,26 @@
 import React from 'react'
 import { useList } from "../store/ListContext";
+import Swal from "sweetalert2";
 
 type Message = {
     id: string,
+    type: string,
     message: string;
 };
+
+
+const Toast = Swal.mixin({
+    toast: true,
+    position: 'top-right',
+    iconColor: 'white',
+    customClass: {
+        popup: 'colored-toast'
+    },
+    showConfirmButton: false,
+    timer: 5000,
+    timerProgressBar: true
+})
+
 
 export const Messages = () => {
     const { state, dispatch } = useList();
@@ -12,10 +28,18 @@ export const Messages = () => {
     let messages: any = []
 
     if (state.messages && state.messages.length >= 1) {
-        messages = state.messages.map((item: Message) => {
-            return <div key={item?.id}>{item?.message}</div>
+
+        state.messages.map((item: Message) => {
+            if (item.type === "add" || item.type === "delete") {
+
+                Toast.fire({
+                    icon: "success",
+                    title: 'Success'
+                })
+
+            }
         })
     }
 
-    return messages;
+    return null;
 }

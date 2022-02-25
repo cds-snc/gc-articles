6e583dde3e0f3133acf2f9ab13a2dcd69f3d4872
Tim Arney <tim@line37.com>
2022-02-25 08:47:41 -0500

    remove non required

--- list-manager/src/components/ListForm.tsx
+++ list-manager/src/components/ListForm.tsx
@@ -39,45 +39,38 @@ export const ListForm = ({ handler = "", formData = {} }: { handler: any, formDa
                 </div>
 
                 <div className="focus-group">
-                    <label className="gc-label required" htmlFor="subscribe_email_template_id"><Asterisk />Subscribe email template id</label>
-                    {errors.subscribe_email_template_id && <p className="gc-error-message" role="alert">This field is required</p>}
-                    <input className="gc-input-text" {...register("subscribe_email_template_id", { required: true })} />
+                    <label className="gc-label required" htmlFor="subscribe_email_template_id">Subscribe email template id</label>
+                    <input className="gc-input-text" {...register("subscribe_email_template_id")} />
                 </div>
 
                 <div className="focus-group">
-                    <label className="gc-label required" htmlFor="unsubscribe_email_template_id"><Asterisk />Unsubscribe email template id</label>
-                    {errors.unsubscribe_email_template_id && <p className="gc-error-message" role="alert">This field is required</p>}
-                    <input className="gc-input-text" {...register("unsubscribe_email_template_id", { required: true })} />
+                    <label className="gc-label required" htmlFor="unsubscribe_email_template_id">Unsubscribe email template id</label>
+                    <input className="gc-input-text" {...register("unsubscribe_email_template_id")} />
                 </div>
 
                 <div className="focus-group">
-                    <label className="gc-label required" htmlFor="subscribe_phone_template_id"><Asterisk />Subscribe phone template id</label>
-                    {errors.subscribe_phone_template_id && <p className="gc-error-message" role="alert">This field is required</p>}
-                    <input className="gc-input-text" {...register("subscribe_phone_template_id", { required: true })} />
+                    <label className="gc-label required" htmlFor="subscribe_phone_template_id">Subscribe phone template id</label>
+                    <input className="gc-input-text" {...register("subscribe_phone_template_id")} />
                 </div>
 
                 <div className="focus-group">
-                    <label className="gc-label required" htmlFor="unsubscribe_phone_template_id"><Asterisk />Unsubscribe phone template id</label>
-                    {errors.unsubscribe_phone_template_id && <p className="gc-error-message" role="alert">This field is required</p>}
-                    <input className="gc-input-text" {...register("unsubscribe_phone_template_id", { required: true })} />
+                    <label className="gc-label required" htmlFor="unsubscribe_phone_template_id">Unsubscribe phone template id</label>
+                    <input className="gc-input-text" {...register("unsubscribe_phone_template_id")} />
                 </div>
 
                 <div className="focus-group">
-                    <label className="gc-label required" htmlFor="subscribe_redirect_url"><Asterisk />Subscribe redirect url</label>
-                    {errors.subscribe_redirect_url && <p className="gc-error-message" role="alert">This field is required</p>}
-                    <input className="gc-input-text" {...register("subscribe_redirect_url", { required: true })} />
+                    <label className="gc-label required" htmlFor="subscribe_redirect_url">Subscribe redirect url</label>
+                    <input className="gc-input-text" {...register("subscribe_redirect_url")} />
                 </div>
 
                 <div className="focus-group">
-                    <label className="gc-label required" htmlFor="unsubscribe_redirect_url"><Asterisk />Unsubscribe redirect url</label>
-                    {errors.unsubscribe_redirect_url && <p className="gc-error-message" role="alert">This field is required</p>}
-                    <input className="gc-input-text" {...register("unsubscribe_redirect_url", { required: true })} />
+                    <label className="gc-label required" htmlFor="unsubscribe_redirect_url">Unsubscribe redirect url</label>
+                    <input className="gc-input-text" {...register("unsubscribe_redirect_url")} />
                 </div>
 
                 <div className="focus-group">
-                    <label className="gc-label required" htmlFor="confirm_redirect_url"><Asterisk />Confirm redirect url</label>
-                    {errors.confirm_redirect_url && <p className="gc-error-message" role="alert">This field is required</p>}
-                    <input className="gc-input-text" {...register("confirm_redirect_url", { required: true })} />
+                    <label className="gc-label required" htmlFor="confirm_redirect_url">Confirm redirect url</label>
+                    <input className="gc-input-text" {...register("confirm_redirect_url")} />
                 </div>
 
                 <div className="focus-group">

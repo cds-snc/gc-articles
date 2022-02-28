1349638a0da06e5ac5a44e30ad9396d8cbce8676
Tim Arney <tim@line37.com>
2022-02-28 13:56:51 -0500

    form updates

--- wordpress/wp-content/plugins/cds-base/classes/Modules/ListManager/app/src/components/ListForm.tsx
+++ wordpress/wp-content/plugins/cds-base/classes/Modules/ListManager/app/src/components/ListForm.tsx
@@ -2,12 +2,16 @@ import * as React from 'react';
 import { useEffect } from "react";
 import { useForm } from "react-hook-form";
 import { List, FieldError } from "../types";
+import styled from 'styled-components';
+
+const textWidth = { width: "25em" }
+
 
 const Asterisk = () => {
     return (
         <>
             <span data-testid="asterisk" aria-hidden="true">* </span>
-            <i className="visually-hidden">Required Field</i>
+            <i style={{ display: "none" }} className="visually-hidden">Required Field</i>
         </>
     )
 }
@@ -28,70 +32,91 @@ export const ListForm = ({ handler, formData = {}, serverErrors = [] }: { handle
     }, [setError, serverErrors]);
 
     return (
-        <div id="gc-form-wrapper" className="gc-form-wrapper">
-            <form onSubmit={handleSubmit(handler)}>
-                <div className="focus-group">
-                    <label className="gc-label required" htmlFor="name"><Asterisk />Name</label>
-                    {errors.name && <p className="gc-error-message" role="alert">{errors.name?.message}</p>}
-                    <input className="gc-input-text" {...register("name", { required: true })} />
-                </div>
-                <div className="focus-group">
-                    <label className="gc-label required" htmlFor="language"><Asterisk />Language</label>
-                    {errors.language && <p className="gc-error-message" role="alert">{errors.language?.message}</p>}
-                    <input className="gc-input-text" {...register("language", { required: true })} />
-                </div>
-                <div className="focus-group">
-                    <label className="gc-label required" htmlFor="service_id"><Asterisk />Service Id</label>
-                    {errors.service_id && <p className="gc-error-message" role="alert">{errors.service_id?.message}</p>}
-                    <input className="gc-input-text" {...register("service_id", { required: true })} />
-                </div>
+        <form onSubmit={handleSubmit(handler)}>
+            <table id="form-table" className="form-table">
+                <tr>
+                    <th><label className="required" htmlFor="name"><Asterisk />Name</label></th>
+                    <td>
+                        {errors.name && <p className="gc-error-message" role="alert">{errors.name?.message}</p>}
+                        <input style={textWidth} type="text" {...register("name", { required: true })} />
+                    </td>
+                </tr>
+                <tr>
+                    <th><label className="required" htmlFor="language"><Asterisk />Language</label></th>
+                    <td>
+                        {errors.language && <p className="gc-error-message" role="alert">{errors.language?.message}</p>}
+                        <input style={textWidth} type="text" {...register("language", { required: true })} />
+                    </td>
+                </tr>
+                <tr>
+                    <th><label className="required" htmlFor="service_id"><Asterisk />Service Id</label></th>
+                    <td>
+                        {errors.service_id && <p className="gc-error-message" role="alert">{errors.service_id?.message}</p>}
+                        <input style={textWidth} type="text" {...register("service_id", { required: true })} />
+                    </td>
+                </tr>
 
-                <div className="focus-group">
-                    <label className="gc-label required" htmlFor="subscribe_email_template_id">Subscribe email template id</label>
-                    {errors.subscribe_email_template_id && <p className="gc-error-message" role="alert">{errors.subscribe_email_template_id?.message}</p>}
-                    <input className="gc-input-text" {...register("subscribe_email_template_id")} />
-                </div>
+                <tr>
+                    <th><label className="required" htmlFor="subscribe_email_template_id">Subscribe email template id</label></th>
+                    <td>
+                        {errors.subscribe_email_template_id && <p className="gc-error-message" role="alert">{errors.subscribe_email_template_id?.message}</p>}
+                        <input style={textWidth} type="text" {...register("subscribe_email_template_id")} />
+                    </td>
+                </tr>
 
-                <div className="focus-group">
-                    <label className="gc-label required" htmlFor="unsubscribe_email_template_id">Unsubscribe email template id</label>
-                    {errors.unsubscribe_email_template_id && <p className="gc-error-message" role="alert">{errors.unsubscribe_email_template_id?.message}</p>}
-                    <input className="gc-input-text" {...register("unsubscribe_email_template_id")} />
-                </div>
+                <tr>
+                    <th><label className="required" htmlFor="unsubscribe_email_template_id">Unsubscribe email template id</label></th>
+                    <td>
+                        {errors.unsubscribe_email_template_id && <p className="gc-error-message" role="alert">{errors.unsubscribe_email_template_id?.message}</p>}
+                        <input style={textWidth} type="text" {...register("unsubscribe_email_template_id")} />
+                    </td>
+                </tr>
 
-                <div className="focus-group">
-                    <label className="gc-label required" htmlFor="subscribe_phone_template_id">Subscribe phone template id</label>
-                    {errors.subscribe_phone_template_id && <p className="gc-error-message" role="alert">{errors.subscribe_phone_template_id?.message}</p>}
-                    <input className="gc-input-text" {...register("subscribe_phone_template_id")} />
-                </div>
+                <tr>
+                    <th><label className="required" htmlFor="subscribe_phone_template_id">Subscribe phone template id</label></th>
+                    <td>
+                        {errors.subscribe_phone_template_id && <p className="gc-error-message" role="alert">{errors.subscribe_phone_template_id?.message}</p>}
+                        <input style={textWidth} type="text" {...register("subscribe_phone_template_id")} />
+                    </td>
+                </tr>
 
-                <div className="focus-group">
-                    <label className="gc-label required" htmlFor="unsubscribe_phone_template_id">Unsubscribe phone template id</label>
-                    {errors.unsubscribe_phone_template_id && <p className="gc-error-message" role="alert">{errors.unsubscribe_phone_template_id?.message}</p>}
-                    <input className="gc-input-text" {...register("unsubscribe_phone_template_id")} />
-                </div>
+                <tr>
+                    <th><label className="required" htmlFor="unsubscribe_phone_template_id">Unsubscribe phone template id</label></th>
+                    <td>
+                        {errors.unsubscribe_phone_template_id && <p className="gc-error-message" role="alert">{errors.unsubscribe_phone_template_id?.message}</p>}
+                        <input style={textWidth} type="text" {...register("unsubscribe_phone_template_id")} />
+                    </td>
+                </tr>
 
-                <div className="focus-group">
-                    <label className="gc-label required" htmlFor="subscribe_redirect_url">Subscribe redirect url</label>
-                    {errors.subscribe_redirect_url && <p className="gc-error-message" role="alert">{errors.subscribe_redirect_url?.message}</p>}
-                    <input className="gc-input-text" {...register("subscribe_redirect_url")} />
-                </div>
+                <tr>
+                    <th><label className="gc-label required" htmlFor="subscribe_redirect_url">Subscribe redirect url</label></th>
+                    <td>
+                        {errors.subscribe_redirect_url && <p className="gc-error-message" role="alert">{errors.subscribe_redirect_url?.message}</p>}
+                        <input style={textWidth} type="text" {...register("subscribe_redirect_url")} />
+                    </td>
+                </tr>
 
-                <div className="focus-group">
-                    <label className="gc-label required" htmlFor="unsubscribe_redirect_url">Unsubscribe redirect url</label>
-                    {errors.unsubscribe_redirect_url && <p className="gc-error-message" role="alert">{errors.unsubscribe_redirect_url?.message}</p>}
-                    <input className="gc-input-text" {...register("unsubscribe_redirect_url")} />
-                </div>
+                <tr>
+                    <th><label className="required" htmlFor="unsubscribe_redirect_url">Unsubscribe redirect url</label></th>
+                    <td>
+                        {errors.unsubscribe_redirect_url && <p className="gc-error-message" role="alert">{errors.unsubscribe_redirect_url?.message}</p>}
+                        <input style={textWidth} type="text" {...register("unsubscribe_redirect_url")} />
+                    </td>
+                </tr>
 
-                <div className="focus-group">
-                    <label className="gc-label required" htmlFor="confirm_redirect_url">Confirm redirect url</label>
-                    {errors.confirm_redirect_url && <p className="gc-error-message" role="alert">{errors.confirm_redirect_url?.message}</p>}
-                    <input className="gc-input-text" {...register("confirm_redirect_url")} />
-                </div>
+                <tr>
+                    <th><label className="required" htmlFor="confirm_redirect_url">Confirm redirect url</label></th>
+                    <td>
+                        {errors.confirm_redirect_url && <p className="gc-error-message" role="alert">{errors.confirm_redirect_url?.message}</p>}
+                        <input style={textWidth} type="text" {...register("confirm_redirect_url")} />
+                    </td>
+                </tr>
 
-                <div className="focus-group">
-                    <input className="gc-button" type="submit" />
-                </div>
-            </form>
-        </div>
+                <tr>
+                    <td><input className="button button-primary" type="submit" /></td>
+                </tr>
+
+            </table>
+        </form>
     );
 }

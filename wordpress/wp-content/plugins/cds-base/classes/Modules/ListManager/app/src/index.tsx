ea03b1dd75ee9b56280c2edeefade96dd85f0e39
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2022-03-01 17:24:36 -0500

    signature must match

--- wordpress/wp-content/plugins/cds-base/classes/Modules/ListManager/app/src/index.tsx
+++ wordpress/wp-content/plugins/cds-base/classes/Modules/ListManager/app/src/index.tsx
@@ -2,13 +2,16 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { List } from '../../../Notify/src/Types';
 
 declare global {
   interface Window {
     CDS_LIST_MANAGER: { endpoint: string },
     renderListManager: (el: string) => void,
     CDS_VARS: {
-      rest_nonce: string
+      rest_url?: string;
+      rest_nonce?: string;
+      notify_list_ids?: List[];
     }
   }
 }

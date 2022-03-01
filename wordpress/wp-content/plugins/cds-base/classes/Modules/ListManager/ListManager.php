3a1d18576e6bc54227790c4f2e98805a3887fe38
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2022-03-01 16:11:33 -0500

    Add wp rest endpoint to proxy requests to list-manager and add auth header

--- wordpress/wp-content/plugins/cds-base/classes/Modules/ListManager/ListManager.php
+++ wordpress/wp-content/plugins/cds-base/classes/Modules/ListManager/ListManager.php
@@ -4,6 +4,11 @@ declare(strict_types=1);
 
 namespace CDS\Modules\ListManager;
 
+
+use WP_Error;
+use WP_REST_Request;
+use WP_REST_Response;
+
 class ListManager
 {
     public function __construct()
@@ -19,6 +24,7 @@ class ListManager
     public function addActions()
     {
         add_action('admin_enqueue_scripts', [$this, 'enqueue']);
+        add_action('rest_api_init', [$this, 'registerRestRoutes']);
     }
 
     public function enqueue()
@@ -44,6 +50,52 @@ class ListManager
             ]);
         } catch (\Exception $exception) {
             echo $exception->getMessage();
+        }
+    }
+
+    /**
+     * Route to proxy requests to list-manager
+     */
+    public function registerRestRoutes()
+    {
+        register_rest_route('list-manager', '/(?P<path>[a-z0-9\-_/]*)', [
+            'methods'             => \WP_REST_Server::ALLMETHODS,
+            'callback'            => [$this, 'proxyRequest'],
+            'permission_callback' => function () {
+                return current_user_can('list_manager_bulk_send');
+            }
+        ]);
+    }
+
+    public function proxyRequest(WP_REST_Request $request): WP_Error|WP_REST_Response
+    {
+        $token = getenv_docker('DEFAULT_LIST_MANAGER_API_KEY', '');
+        $base_url = getenv_docker('LIST_MANAGER_ENDPOINT', '');
+        $path = $request['path'];
+        $body = $request->get_body();
+        $url = $base_url .'/'. $path;
+
+        $args = [
+            'method' => $request->get_method(),
+            'headers' => [
+                'Authorization' => "$token",
+                'Content-Type' => 'application/json'
+            ],
+            'body' => $body,
+        ];
+
+        // Proxy request to list-manager
+        $proxy_response = wp_remote_request($url, $args);
+
+        // Retrieve information
+        $response_code = $proxy_response["response"]["code"];
+        $response_message = $proxy_response["response"]["message"];
+        $response_body = $proxy_response["body"];
+
+        if ($response_code < 400 ) {
+            return new WP_REST_Response(json_decode($response_body));
+        } else {
+            return new WP_Error($response_code, $response_message, $response_body);
         }
     }
 }

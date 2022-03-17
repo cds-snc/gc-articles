90bf244fed4d8952d7bfd72669b1604efe4a78ea
Tim Arney <tim@line37.com>
2022-03-17 12:12:37 -0400

    clean up service

--- wordpress/wp-content/plugins/cds-base/classes/Modules/ListManager/app/src/components/Service.tsx
+++ wordpress/wp-content/plugins/cds-base/classes/Modules/ListManager/app/src/components/Service.tsx
@@ -1,27 +1,8 @@
 import * as React from 'react';
 import { ListViewTable } from "./ListViewTable";
-import { useList } from "../store/ListContext";
 import { Messages } from "./Messages";
-import { ServiceData, Service as ServiceType } from "../types";
-import { useParams } from "react-router-dom";
-
-const getServiceName = (serviceData: ServiceData, serviceId: string | undefined): string => {
-    if (!serviceId) return "";
-
-    const currentService: ServiceType[] | undefined = serviceData?.filter((service: ServiceType) => {
-        return service['service_id'] === serviceId
-    });
-
-    if (currentService?.length) {
-        return currentService[0].name;
-    }
-
-    return "";
-}
 
 export const Service = () => {
-    const { state: { serviceData } } = useList();
-    const params = useParams();
     return (
         <div>
             <Messages />

e5e3abd53695547ed24d40fb2bc3f7ac2d11cc21
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2022-03-18 12:08:31 -0400

    add options to command

--- cypress/support/commands/test-setup.js
+++ cypress/support/commands/test-setup.js
@@ -13,7 +13,7 @@ Cypress.Commands.add('testSetup', ({ theme = 'cds-default' } = {}) => {
 
     cy.exec('npm run wp-env:clean', options);
     cy.exec('wp-env run tests-cli wp option delete list_values', options);
-    cy.exec('wp-env run tests-cli wp option add NOTIFY_API_KEY gc-articles-xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx-xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx')
+    cy.exec('wp-env run tests-cli wp option add NOTIFY_API_KEY gc-articles-xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx-xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx', options)
     cy.exec('wp-env run tests-cli wp option set list_values --format=json < ./cypress/fixtures/notify-list-data.json', options)
     cy.exec('wp-env run tests-cli wp plugin activate sitepress-multilingual-cms cds-base two-factor;', options)
     cy.exec('wp-env run tests-cli wp plugin activate s3-uploads disable-user-login;', options) // wps-hide-login

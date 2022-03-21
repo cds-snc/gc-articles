dcd5adc1ef97ac8f7b11e37476724cc40e2947c5
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2022-03-21 11:11:14 -0400

    no options

--- cypress/integration/notify.template.sender.spec.js
+++ cypress/integration/notify.template.sender.spec.js
@@ -5,7 +5,7 @@ const NEW_TAB_REL_DEFAULT_VALUE = 'noreferrer noopener';
 describe('Notify Template Sender', () => {
     before(() => {
         cy.testSetup();
-        cy.exec('wp-env run tests-cli wp set_encrypted_option NOTIFY_API_KEY gc-articles-xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx-xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx', options)
+        cy.exec('wp-env run tests-cli wp set_encrypted_option NOTIFY_API_KEY gc-articles-xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx-xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx')
     });
 
     beforeEach(() => {

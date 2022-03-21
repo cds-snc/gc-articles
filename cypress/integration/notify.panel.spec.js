f9fd08189ff8cc276503f55870b005ac38c88e0d
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2022-03-21 11:24:16 -0400

    add explanatory comment

--- cypress/integration/notify.panel.spec.js
+++ cypress/integration/notify.panel.spec.js
@@ -5,6 +5,8 @@ const NEW_TAB_REL_DEFAULT_VALUE = 'noreferrer noopener';
 describe('Notify Panel', () => {
     before(() => {
         cy.testSetup();
+
+        // Setting this only for test runs that need it
         cy.exec('wp-env run tests-cli wp set_encrypted_option NOTIFY_API_KEY gc-articles-xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx-xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx')
     });
 

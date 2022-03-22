ebbdd26972aecae6c04760a18dc1949782b3fa04
Paul Craig <paul@pcraig3.ca>
2022-03-22 10:06:15 -0400

    Hide "Canada.ca" menu + unassign custom menu if exists (#638)
    * Add site setting to hide the Canada.ca menu
    
    Same as we do for the search bar or the breadcrumbs, add a class
    and then hide it.
    
    Keeping the blue line for now. I think it adds scructure to the page.
    
    * Add a "warning" message if a custom menu exists
    
    * Add logic for setting and unsetting menus
    
    Essentially, we have 3 different states we can be in:
    
    - Canada.ca top menu
    - We have our own top menu that we set
    - We don't want a top menu
    
    To simplify things, we are going to assume
    
    - someone who clicks option (3) wants to get rid of any top menu they have assigned (whether 1 OR 2)
    - someone who assigns a top menu (option 2) wants to unset option (3)
    
    What we want to avoid:
    
    - someone builds their own menu but is not able to ever see it because they previously selected "hide canada.ca top menu" (in this case, it keeps getting programatically unassigned)
    - someone sets "hide canada.ca top menu" and then it still still shows their custom menu that they don't want
    
    * Use hook that happens later in the WP lifecycle
    
    Previously, using the "init" hook, the menu location hadn't been
    set yet, so the condition would fail.

--- wordpress/wp-content/plugins/cds-base/classes/Modules/Site/SettingsFunctions.php
+++ wordpress/wp-content/plugins/cds-base/classes/Modules/Site/SettingsFunctions.php
@@ -18,11 +18,19 @@ class SettingsFunctions
 
     public function addActions()
     {
+        add_action('admin_head', [$this, 'dequeuePrimaryMenu']);
+
         add_filter('body_class', [$this, 'addBodyClasses']);
     }
 
     public function addBodyClasses($classes)
     {
+        $showWetMenu = get_option('show_wet_menu');
+
+        if ($showWetMenu === 'off') {
+            $classes[] = 'hide-wet-menu';
+        }
+
         $showSearch = get_option('show_search');
 
         if ($showSearch === 'off') {
@@ -36,5 +44,28 @@ class SettingsFunctions
         }
 
         return $classes;
+    }
+
+    public function dequeuePrimaryMenu()
+    {
+        $showWetMenu = get_option('show_wet_menu');
+        $locations = get_nav_menu_locations();
+
+        if (
+            $showWetMenu === 'off' &&                   // if we _don't_ want to show the "wet menu"
+            array_key_exists('header', $locations) &&   // if there _is_ a 'header' location
+            is_int($locations['header'])                // if 'header' is assigned to an integer
+        ) {
+            $current_page_path = $_SERVER['REQUEST_URI'];
+
+            // if we are explicitly setting a menu, turn on the "show canada.ca menu" option
+            if (str_contains($current_page_path, "nav-menus.php")) {
+                update_option('show_wet_menu', 'on');
+            } else {
+                // if not, remove 'header' menu
+                $locations['header'] = null; // remove assigned menu
+                set_theme_mod('nav_menu_locations', $locations); // save the new "locations" array
+            }
+        }
     }
 }

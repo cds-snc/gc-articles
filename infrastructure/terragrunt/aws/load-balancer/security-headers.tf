e99e539ff198c254e24b06841e9d94c855caaddc
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2022-03-24 17:15:12 -0400

    Add cloudfront security headers config (#648)
    * add cloudfront security headers config
    
    * adjust some settings
    
    * comment out csp for now

---
+++ infrastructure/terragrunt/aws/load-balancer/security-headers.tf
@@ -0,0 +1,27 @@
+resource "aws_cloudfront_response_headers_policy" "security_headers_policy" {
+  name = "my-security-headers-policy"
+  security_headers_config {
+    content_type_options {
+      override = true
+    }
+    frame_options {
+      frame_option = "SAMEORIGIN"
+      override     = true
+    }
+    xss_protection {
+      mode_block = true
+      protection = true
+      override   = true
+    }
+    strict_transport_security {
+      access_control_max_age_sec = "31536000"
+      include_subdomains         = true
+      preload                    = true
+      override                   = true
+    }
+    # content_security_policy {
+    #   content_security_policy = "frame-ancestors 'none'; default-src 'none'; img-src 'self'; script-src 'self'; style-src 'self'; object-src 'none'"
+    #   override                = true
+    # }
+  }
+}

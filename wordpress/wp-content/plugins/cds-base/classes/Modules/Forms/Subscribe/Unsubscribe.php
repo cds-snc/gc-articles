2ef214e8b6c3eb13d0c8e172ec4c087856566579
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2022-03-25 11:24:35 -0400

    Proxy Confirm and Unsubscribe endpoints (#646)
    * Add confirm and unsubscribe endpoints
    
    * refactor uri patterns
    
    * handle error and default redirects

---
+++ wordpress/wp-content/plugins/cds-base/classes/Modules/Forms/Subscribe/Unsubscribe.php
@@ -0,0 +1,59 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CDS\Modules\Forms\Subscribe;
+
+use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Exception\ServerException;
+use WP_REST_Request;
+use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
+use Psr\Http\Message\UriInterface;
+
+class Unsubscribe
+{
+    protected string $redirect = '';
+
+    public function unsubscribe(WP_REST_Request $request)
+    {
+        $subscriptionId = $request['id'];
+
+        $client = new Client();
+
+        $endpoint = getenv('LIST_MANAGER_ENDPOINT');
+
+        try {
+            $client->request('GET', $endpoint . '/unsubscribe/' . $subscriptionId, [
+                'allow_redirects' => [
+                    'on_redirect' => function (
+                        RequestInterface $request,
+                        ResponseInterface $response,
+                        UriInterface $uri
+                    ) {
+                        $this->redirect = strval($uri);
+                    }
+                ]
+            ]);
+        } catch (ClientException $e) {
+            error_log($e->getMessage());
+            wp_redirect('/list-manager-error');
+            exit();
+        } catch (ServerException $e) {
+            error_log($e->getMessage());
+            wp_redirect('/list-manager-error');
+            exit();
+        }
+
+        // If a redirect was returned from the endpoint, handle it
+        if ($this->redirect) {
+            wp_redirect($this->redirect);
+            exit();
+        }
+
+        // Default redirect
+        wp_redirect('/unsubscribed-from-mailing-list-labonnement-supprime');
+        exit();
+    }
+}

80d716e2d3c267a0ad234034d0cc1474854f5bb3
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2022-04-01 14:38:57 -0400

    Remove wpemoji scripts/styles

--- wordpress/wp-content/themes/cds-default/functions.php
+++ wordpress/wp-content/themes/cds-default/functions.php
@@ -328,3 +328,10 @@ add_action('admin_head', 'fix_svg');
  */
 remove_action('wp_enqueue_scripts', 'wp_enqueue_global_styles');
 remove_action('wp_body_open', 'wp_global_styles_render_svg_filters');
+
+// REMOVE WP EMOJI
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('wp_print_styles', 'print_emoji_styles');
+
+remove_action('admin_print_scripts', 'print_emoji_detection_script');
+remove_action('admin_print_styles', 'print_emoji_styles');

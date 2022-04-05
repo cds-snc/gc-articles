69e057dfcd7917d313640d965e9edade58f3a70a
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2022-04-05 11:53:23 -0400

    Add API and Sign-in screen specific headers (#689)
    * Add API and Sign-in screen specific headers
    
    * add subsite sign-in
    
    * remove preceding slash

--- infrastructure/terragrunt/aws/load-balancer/cloudfront.tf
+++ infrastructure/terragrunt/aws/load-balancer/cloudfront.tf
@@ -65,7 +65,7 @@ resource "aws_cloudfront_distribution" "wordpress" {
   }
 
   ordered_cache_behavior {
-    path_pattern     = "/sign-in-se-connecter*"
+    path_pattern     = "*/sign-in-se-connecter*"
     allowed_methods  = ["GET", "HEAD", "OPTIONS", "DELETE", "PATCH", "POST", "PUT"]
     cached_methods   = ["GET", "HEAD", "OPTIONS"]
     target_origin_id = aws_lb.wordpress.name
@@ -83,6 +83,31 @@ resource "aws_cloudfront_distribution" "wordpress" {
     max_ttl                = 0
     compress               = true
     viewer_protocol_policy = "redirect-to-https"
+
+    response_headers_policy_id = aws_cloudfront_response_headers_policy.security_headers_policy_frontend.id
+  }
+
+  ordered_cache_behavior {
+    path_pattern     = "sign-in-se-connecter*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS", "DELETE", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = aws_lb.wordpress.name
+
+    forwarded_values {
+      query_string = true
+      headers      = ["Host", "Origin", "User-Agent"]
+      cookies {
+        forward = "all"
+      }
+    }
+
+    min_ttl                = 0
+    default_ttl            = 0
+    max_ttl                = 0
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+
+    response_headers_policy_id = aws_cloudfront_response_headers_policy.security_headers_policy_frontend.id
   }
 
   ordered_cache_behavior {
@@ -154,6 +179,29 @@ resource "aws_cloudfront_distribution" "wordpress" {
   }
 
   ordered_cache_behavior {
+    path_pattern     = "*/wp-json/*"
+    allowed_methods  = ["GET", "HEAD", "OPTIONS", "DELETE", "PATCH", "POST", "PUT"]
+    cached_methods   = ["GET", "HEAD", "OPTIONS"]
+    target_origin_id = aws_lb.wordpress.name
+
+    forwarded_values {
+      query_string = true
+      headers      = ["Host", "Origin", "User-Agent", "Authorization"]
+      cookies {
+        forward = "all"
+      }
+    }
+
+    min_ttl                = 0
+    default_ttl            = 0
+    max_ttl                = 0
+    compress               = true
+    viewer_protocol_policy = "redirect-to-https"
+
+    response_headers_policy_id = aws_cloudfront_response_headers_policy.security_headers_policy_api.id
+  }
+
+  ordered_cache_behavior {
     path_pattern     = "wp-json/*"
     allowed_methods  = ["GET", "HEAD", "OPTIONS", "DELETE", "PATCH", "POST", "PUT"]
     cached_methods   = ["GET", "HEAD", "OPTIONS"]
@@ -172,6 +220,8 @@ resource "aws_cloudfront_distribution" "wordpress" {
     max_ttl                = 0
     compress               = true
     viewer_protocol_policy = "redirect-to-https"
+
+    response_headers_policy_id = aws_cloudfront_response_headers_policy.security_headers_policy_api.id
   }
 
   price_class = "PriceClass_200"

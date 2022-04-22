4a84a731a20e4780600511234c7a6d066a57ae15
Tim Arney <tim@line37.com>
2022-04-22 10:30:43 -0400

    add gc-byline

--- wordpress/wp-content/themes/cds-default/inc/block-patterns/homepage/toc.php
+++ wordpress/wp-content/themes/cds-default/inc/block-patterns/homepage/toc.php
@@ -1,3 +1,6 @@
+<!-- wp:paragraph {"className":"gc-byline"} -->
+<p class="gc-byline"><strong>From <a href="http://canada.ca">[Institution name]</a></strong></p>
+<!-- /wp:paragraph -->
 <!-- wp:list {"className":"toc"} -->
 <ul class="toc"><li><a href="http://canada.ca">What this service offers</a></li><li><a href="http://canada.ca">Who is eligible</a></li><li><a href="http://canada.ca">What do you need before you start</a></li><li><a href="http://canada.ca">How to apply</a></li><li><a href="http://canada.ca">After you apply</a></li></ul>
 <!-- /wp:list -->

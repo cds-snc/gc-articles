8cef633be19ce336d2becdd3632b20878893fe1d
Tim Arney <tim@line37.com>
2022-04-27 12:51:13 -0400

    add route

--- wordpress/wp-content/plugins/cds-base/classes/Modules/ListManager/app/src/App.tsx
+++ wordpress/wp-content/plugins/cds-base/classes/Modules/ListManager/app/src/App.tsx
@@ -86,11 +86,3 @@ const App = ({ serviceData, user }: { serviceData: ServiceData, user: User }) =>
 }
 
 export default App;
-
-/*
-<Route path="/service/:serviceId/send" element={
-                <React.Suspense fallback={<Spinner />}>
-                  <SendTemplate />
-                </React.Suspense>
-              } />
-*/

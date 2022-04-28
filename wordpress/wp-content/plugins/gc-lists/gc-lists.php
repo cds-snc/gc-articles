caf890709d630a9f3b7c20f4e86337d5051ca2f5
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2022-04-28 11:42:19 -0400

    Initialize new plugin

---
+++ wordpress/wp-content/plugins/gc-lists/gc-lists.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Plugin Name:     Gc Lists
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     gc-lists
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Gc_Lists
+ */
+
+// Your code starts here.

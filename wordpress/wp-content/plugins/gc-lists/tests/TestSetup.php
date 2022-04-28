28ac56148f6bd43eedf0ccbea519675416ce8f23
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2022-04-28 16:58:14 -0400

    Update to work with PHPest

--- wordpress/wp-content/plugins/gc-lists/tests/TestSetup.php
+++ wordpress/wp-content/plugins/gc-lists/tests/TestSetup.php
@@ -1,25 +1,11 @@
 <?php
-/**
- * Class TestSetup
- *
- * @package Gc_Lists
- */
 
-/**
- * Sample test case.
- */
-class TestSetup extends WP_UnitTestCase {
+test('Just test something with some wordpress functions', function () {
+	$this->assertTrue( true );
 
-	/**
-	 * A single example test.
-	 */
-	public function test_sample() {
-		$this->assertTrue( true );
+	$site_url = get_site_url();
+	$login_url = wp_login_url();
 
-		$site_url = get_site_url();
-		$login_url = wp_login_url();
-
-		$this->assertEquals($login_url, $site_url . '/wp-login.php');
-		$this->assertEquals($site_url, 'http://example.org');
-	}
-}
+	$this->assertEquals($login_url, $site_url . '/wp-login.php');
+	$this->assertEquals($site_url, 'http://example.org');
+});

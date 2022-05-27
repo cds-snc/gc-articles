35133bd6a23256374ee0afbcacc25e320bfce4b4
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2022-05-27 13:29:54 -0400

    Get versions of original

--- wordpress/wp-content/plugins/gc-lists/src/Database/Models/Message.php
+++ wordpress/wp-content/plugins/gc-lists/src/Database/Models/Message.php
@@ -96,7 +96,9 @@ class Message extends Model
      */
     public function versions(array $options = []): ?Collection
     {
-        return static::whereEquals(['original_message_id' => $this->getAttribute('id')], $options);
+        // @TODO: rewrite
+        $original = $this->original();
+        return static::whereEquals(['original_message_id' => $original->getAttribute('id')], $options);
     }
 
     /**

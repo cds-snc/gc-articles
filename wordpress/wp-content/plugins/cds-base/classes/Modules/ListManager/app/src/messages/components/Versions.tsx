fbc978d4a39ce81d893475a45dc4c4fad06d5b9d
Tim Arney <tim@line37.com>
2022-05-27 12:20:02 -0400

    endpoint

--- wordpress/wp-content/plugins/cds-base/classes/Modules/ListManager/app/src/messages/components/Versions.tsx
+++ wordpress/wp-content/plugins/cds-base/classes/Modules/ListManager/app/src/messages/components/Versions.tsx
@@ -1,4 +1,28 @@
+import * as React from 'react';
+import { useState, useEffect } from 'react';
+import { useService } from '../../util/useService';
+import useFetch from 'use-http';
+
 export const Versions = ({ }: {}) => {
+
+    const [loading, setLoading] = useState(false);
+    const [data, setData] = useState([]);
+    const { serviceId } = useService();
+    const { request, response } = useFetch({ data: [] })
+
+    useEffect(() => {
+        const getTemplateVersions = async () => {
+            setLoading(true);
+            await request.get(`/messages/sent}`);
+            if (response.ok) {
+                console.log(response)
+                setData(await response.json());
+                setLoading(false)
+            }
+        }
+        getTemplateVersions();
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, []);
 
     return (
         <div>

529102122705def15582f3f3995b2db5865def5c
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2022-05-31 10:50:34 -0400

    Add submenus and basic page templates

---
+++ wordpress/wp-content/plugins/gc-lists/resources/templates/subscribers.php
@@ -0,0 +1,3 @@
+<h2>Subscribers</h2>
+
+<?php echo $subscribers ?>
--- wordpress/wp-content/plugins/gc-lists/src/Menu.php
+++ wordpress/wp-content/plugins/gc-lists/src/Menu.php
@@ -10,7 +10,9 @@ use CDS\Modules\Notify\Notices;
 class Menu
 {
     protected static $instance;
-    protected string $admin_page = 'gc_lists';
+    protected string $messagesPageSlug = 'gc-lists_messages';
+    protected string $subscribersPageSlug = 'gc-lists_subscribers';
+    protected string $capability = 'list_manager_bulk_send';
 
     public static function getInstance(): Menu
     {
@@ -21,26 +23,92 @@ class Menu
     public function register()
     {
         add_action('admin_menu', [$this, 'addMenu']);
+        add_action('admin_menu', [$this, 'addMessagesSubmenuItem']);
+        add_action('admin_menu', [$this, 'addSubscriberListsSubmenuItem']);
     }
 
     public function addMenu()
     {
         add_menu_page(
-            __('Send Notify Template', "cds-snc"),
             __('GC Lists', "cds-snc"),
-            'list_manager_bulk_send',
-            $this->admin_page,
-            [$this, 'renderForm'],
+            __('GC Lists', "cds-snc"),
+            $this->capability,
+            $this->messagesPageSlug,
+            [$this, 'renderMessages'],
             'dashicons-email'
         );
     }
 
-    public function renderForm(): void
+    public function addMessagesSubmenuItem()
+    {
+        add_submenu_page(
+            $this->messagesPageSlug,
+            __('Messages', 'cds-snc'),
+            __('Messages', 'cds-snc'),
+            $this->capability,
+            $this->messagesPageSlug,
+            [$this, 'renderMessages'],
+        );
+    }
+
+    public function addSubscriberListsSubmenuItem()
+    {
+        add_submenu_page(
+            $this->messagesPageSlug,
+            __('Subscriber lists', 'cds-snc'),
+            __('Subscriber lists', 'cds-snc'),
+            $this->capability,
+            $this->subscribersPageSlug,
+            [$this, 'renderSubscribers'],
+        );
+    }
+
+    public function renderMessages(): void
     {
         if (!get_option('NOTIFY_API_KEY')) {
-            FormHelpers::noApiKey();
-        } else {
-            echo "Hello";
+            $this->noApiKey();
+            exit;
         }
+
+        $messages = "Hello";
+
+        $this->render('messages', [
+            'messages' => $messages
+        ]);
+    }
+
+    public function renderSubscribers(): void
+    {
+        if (!get_option('NOTIFY_API_KEY')) {
+            $this->noApiKey();
+            exit;
+        }
+
+        $subscribers = "Hello";
+
+        $this->render('subscribers', [
+            'subscribers' => $subscribers
+        ]);
+    }
+
+    public function noApiKey()
+    {
+        ?>
+        <h1><?php echo esc_html(get_admin_page_title()); ?></h1>
+        <p>
+            <?php
+            echo sprintf(
+                __('You must configure your <a href="%s">Notify API Key</a>', 'cds-snc'),
+                admin_url("options-general.php?page=notify-settings")
+            );
+            ?>
+        </p>
+        <?php
+    }
+
+    public function render($template, $args)
+    {
+        extract($args);
+        require_once(__DIR__ . "/../resources/templates/{$template}.php");
     }
 }

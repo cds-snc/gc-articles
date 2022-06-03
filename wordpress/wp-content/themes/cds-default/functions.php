04239100139e7268362f1ce161d576871516bd40
Tim Arney <timarney@users.noreply.github.com>
2022-06-03 12:48:34 -0400

    version bump 3.0.7 (#825)

--- wordpress/wp-content/themes/cds-default/functions.php
+++ wordpress/wp-content/themes/cds-default/functions.php
@@ -17,7 +17,7 @@ require_once __DIR__ . '/inc/template-filters.php';
 // phpcs:disable
 if (!defined('_S_VERSION')) {
     // Replace the version number of the theme on each release.
-    define('_S_VERSION', '3.0.6');
+    define('_S_VERSION', '3.0.7');
 }
 
 if (!defined('THEME_NAMESPACE')) {

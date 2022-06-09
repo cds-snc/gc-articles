7212cde9263781b9203e3471d72d4186070d91a7
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2022-06-09 11:25:08 -0400

    Add back to messages links

--- wordpress/wp-content/plugins/gc-lists/resources/js/src/messages/components/AllSendingHistory.tsx
+++ wordpress/wp-content/plugins/gc-lists/resources/js/src/messages/components/AllSendingHistory.tsx
@@ -1,8 +1,15 @@
 import { SendingHistory } from "./SendingHistory";
+import { __ } from '@wordpress/i18n';
+import { StyledLink } from './Table';
+import * as React from 'react';
+import { Back } from './icons/Back';
 
 export const AllSendingHistory = () => {
     return (
         <>
+            <StyledLink to={`/messages`}>
+                <Back /> <span>{__("Back to messages ", "gc-lists")}</span>
+            </StyledLink>
             <SendingHistory perPage={10} pageNav={true} allLink={false} />
         </>
 

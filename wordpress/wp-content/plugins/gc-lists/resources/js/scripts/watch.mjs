bb0c8f2a548985f8d2e02da98edb88b0d709ce99
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2022-06-09 17:32:46 -0400

    Improved version

--- wordpress/wp-content/plugins/gc-lists/resources/js/scripts/watch.mjs
+++ wordpress/wp-content/plugins/gc-lists/resources/js/scripts/watch.mjs
@@ -1,20 +1,6 @@
-import watch from "watch";
 import shell from 'shelljs';
 import notifier from 'node-notifier';
 
-
-watch.createMonitor('./src', function (monitor) {
-    monitor.files['*.ts, *.tsx'];
-
-    console.log("Watching...");
-    notifier.notify({ title: 'GC Articles', message: 'Watching...' });
-
-    monitor.on("changed", function (f, curr, prev) {
-        console.clear();
-        console.log("File changed: " + f);
-
-        const code = (shell.exec(`npm run build`)).code;
-
-        notifier.notify({ title: 'GC Articles', message: 'Build completed' });
-    })
-})
+notifier.notify({title: 'GC Articles', message: 'Change detected'});
+shell.exec(`npm run build`)
+notifier.notify({ title: 'GC Articles', message: 'Build completed' });

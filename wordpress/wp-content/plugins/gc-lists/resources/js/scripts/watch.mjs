c1e3a85418120ba2736f9d8bf2702d565ac214af
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2022-06-10 14:45:15 -0400

    Add config file to turn off notifications

--- wordpress/wp-content/plugins/gc-lists/resources/js/scripts/watch.mjs
+++ wordpress/wp-content/plugins/gc-lists/resources/js/scripts/watch.mjs
@@ -1,6 +1,32 @@
 import shell from 'shelljs';
 import notifier from 'node-notifier';
 
-notifier.notify({title: 'GC Articles', message: 'Change detected'});
-shell.exec(`npm run build`)
-notifier.notify({ title: 'GC Articles', message: 'Build completed' });
+import fs from 'fs';
+
+let settings;
+
+try {
+    settings = JSON.parse(fs.readFileSync('.watch.json', { encoding: 'utf8', flag: 'r' }));
+    console.log("Settings file found, notifications are " + (settings.notifications ? "on" : "off"));
+} catch (e) {
+    console.log("No settings file found, notifcations are on by default");
+    settings = JSON.parse('{ "notifications": true }');
+}
+
+const notifications = settings.notifications;
+
+if (notifications) {
+    notifier.notify({ title: 'GC Articles', message: 'Change detected' })
+}
+
+shell.exec('npm run build', function(code, stdout, stderr) {
+    if (code === 1) {
+        if (notifications) {
+            notifier.notify({ title: 'GC Articles', message: stdout });
+        }
+    } else {
+        if (notifications) {
+            notifier.notify({ title: 'GC Articles', message: stdout });
+        }
+    }
+})

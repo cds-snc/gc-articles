5bf7628d1550771cc7bb8510b57f40e763cc85e8
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2022-06-14 14:07:29 -0400

    Add tests around Utils class

--- wordpress/wp-content/plugins/gc-lists/tests/Unit/TestUtils.php
+++ wordpress/wp-content/plugins/gc-lists/tests/Unit/TestUtils.php
@@ -14,17 +14,38 @@ test('extractServiceIdFromApiKey', function() {
 });
 
 test('getServiceId', function() {
-    mock('get_option')
-        ->shouldReceive('NOTIFY_API_KEY')
-        ->andReturn('this-is-fake-a7902fc8-37f0-417c-54c8-9ab499ee54c8-8f43566b-e491-451h-99e7-4f604b5c8283');
-
+    add_option('NOTIFY_API_KEY', 'this-is-fake-a7902fc8-37f0-417c-54c8-9ab499ee54c8-8f43566b-e491-451h-99e7-4f604b5c8283');
     $serviceId = Utils::getServiceId();
-})->skip();
+    $this->assertEquals($serviceId, 'a7902fc8-37f0-417c-54c8-9ab499ee54c8');
+});
 
 test('getServices', function() {
-    // todo
-})->skip();
+    add_option('NOTIFY_API_KEY', 'this-is-fake-a7902fc8-37f0-417c-54c8-9ab499ee54c8-8f43566b-e491-451h-99e7-4f604b5c8283');
+    $services = Utils::getServices();
+    $this->assertEquals($services, [
+        'name' => 'Your Lists',
+        'service_id' => 'a7902fc8-37f0-417c-54c8-9ab499ee54c8'
+    ]);
+});
 
 test('getUserPermissions', function() {
-    // todo
-})->skip();
+    $user_id = $this->factory->user->create();
+    wp_set_current_user( $user_id );
+    $current_user = wp_get_current_user();
+
+    $permissions = Utils::getUserPermissions();
+
+    $none = new stdClass();
+    $none->hasEmail = false;
+    $none->hasPhone = false;
+    $this->assertEquals($none, $permissions);
+
+    $current_user->add_cap('list_manager_bulk_send', true);
+    $current_user->add_cap('list_manager_bulk_send_sms', true);
+    $permissions = Utils::getUserPermissions();
+
+    $hasBoth = new stdClass();
+    $hasBoth->hasEmail = true;
+    $hasBoth->hasPhone = true;
+    $this->assertEquals($hasBoth, $permissions);
+});

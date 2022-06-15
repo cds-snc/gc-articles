dc2f7eed3ece47beff7bf422c5f95a6406d70b9f
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2022-06-15 14:00:51 -0400

    Intercept HTTP request, fix test

--- wordpress/wp-content/plugins/gc-lists/tests/Unit/Api/TestSendMessage.php
+++ wordpress/wp-content/plugins/gc-lists/tests/Unit/Api/TestSendMessage.php
@@ -3,55 +3,29 @@
 use GCLists\Api\SendMessage;
 use function Pest\Faker\faker;
 
-beforeAll(function () {
-    WP_Mock::setUp();
-});
-
-afterAll(function () {
-    WP_Mock::tearDown();
-});
-
 test('Send a message', function() {
-    $response = array(
-        'body' => '{"status": "OK"}',
-        'response' => array(
-            'code' => 200,
-            'message' => 'OK',
-        ),
-        'cookies' => array(),
-        'filename' => null,
-    );
+    // intercept/mock the HTTP proxy request
+    add_filter( 'pre_http_request', function() {
+        return [
+            'headers'     => [],
+            'cookies'     => [],
+            'filename'    => null,
+            'response'    => 200,
+            'status_code' => 200,
+            'success'     => 1,
+            'body'        => '{"status": "OK"}',
+        ];
+    }, 10, 3 );
 
-    $url = getenv('LIST_MANAGER_ENDPOINT') . '/send';
     $listId = faker()->uuid();
     $subject = 'This is my subject';
     $body = 'This is my body';
+    $proxy_response = SendMessage::handle($listId, $subject, $body);
 
-    $args = [
-        'method' => 'POST',
-        'headers' => [
-            'Authorization' => getenv('DEFAULT_LIST_MANAGER_API_KEY'),
-            'Content-Type' => 'application/json'
-        ],
-        'body' => json_encode([
-            'job_name' => "gc-lists",
-            'list_id' => $listId,
-            'personalisation' => json_encode([
-                'subject' => $subject,
-                'message' => $body,
-            ]),
-            'template_id' => get_option('NOTIFY_GENERIC_TEMPLATE_ID'),
-            'template_type' => "email",
-        ]),
+    $expected = (object) [
+        'status' => 'OK'
     ];
 
-    \WP_Mock::userFunction( 'wp_remote_request', [
-        'times' => 1,
-        'args' => [$url, $args],
-        'return' => $response
-    ]);
-
-    $proxy_response = SendMessage::handle($listId, $subject, $body);
-    
-    $this->assertEquals( $proxy_response, $response );
-})->group('send');
+    $this->assertInstanceOf('WP_REST_Response', $proxy_response);
+    $this->assertEquals( $proxy_response->get_data(), $expected );
+});

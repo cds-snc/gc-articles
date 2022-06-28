a8d07a7578be850cec3943ead0dacc136d87a005
Tim Arney <tim@line37.com>
2022-06-28 14:25:12 -0400

    use wp element

--- wordpress/wp-content/plugins/cds-js-test/sample-react-app/src/Home.js
+++ wordpress/wp-content/plugins/cds-js-test/sample-react-app/src/Home.js
@@ -1,8 +1,6 @@
-import React from "react";
-
 const Home = () => {
   const { __ } = wp.i18n;
-  return <div>{__('REACT APP', 'cds-js-test' )} {__('js-html-react', 'cds-js-test')}!!</div>;
+  return <div>{__('REACT APP wp element', 'cds-js-test' )} - {__('js-html-react', 'cds-js-test')}!!</div>;
 };
 
 export default Home;
--- wordpress/wp-content/plugins/cds-js-test/sample-react-app/src/index.js
+++ wordpress/wp-content/plugins/cds-js-test/sample-react-app/src/index.js
@@ -1,8 +1,8 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+const { render } = wp.element;
+
 import Home from './Home';
 
-ReactDOM.render(
+render(
   <Home />,
   document.getElementById('app')
 );

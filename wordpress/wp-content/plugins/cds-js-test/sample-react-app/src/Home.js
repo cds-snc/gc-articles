9146d8ecb618138a90dae07194a4944e90ef5def
Tim Arney <tim@line37.com>
2022-06-28 13:42:13 -0400

    text

--- wordpress/wp-content/plugins/cds-js-test/sample-react-app/src/Home.js
+++ wordpress/wp-content/plugins/cds-js-test/sample-react-app/src/Home.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Home = () => {
   const { __ } = wp.i18n;
-  return <div>REACT APP {__('js-html-react', 'cds-js-test')}!!</div>;
+  return <div>{__('REACT APP', 'cds-js-test' )} {__('js-html-react', 'cds-js-test')}!!</div>;
 };
 
 export default Home;

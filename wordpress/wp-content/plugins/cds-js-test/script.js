b18123b9e8505dfedaa36e3b284119fb87107e39
Tim Arney <tim@line37.com>
2022-06-28 15:10:13 -0400

    ---

--- wordpress/wp-content/plugins/cds-js-test/script.js
+++ wordpress/wp-content/plugins/cds-js-test/script.js
@@ -10,6 +10,6 @@ div.innerHTML += __('js-html', 'cds-js-test');
 (() => {
     "use strict";
     const e = function () {
-        return wp.element.createElement("div", null, __("REACT APP wp element", "cds-js-test"), " - ", __("js-html-react", "cds-js-test"), "!!")
+        return wp.element.createElement("div", null, __("REACT APP wp element", "cds-js-test"), " - ", __("js-html-react", "cds-js-test"), "!!", __("more", "cds-js-test"))
     }; (0, wp.element.render)(wp.element.createElement(e, null), document.getElementById("app1"))
 })();

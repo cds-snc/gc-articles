44911aa962b9de6c45811f9f18336f4204a229ca
Tim Arney <tim@line37.com>
2022-06-28 09:38:44 -0400

    update domain

--- wordpress/wp-content/plugins/cds-js-test/script.js
+++ wordpress/wp-content/plugins/cds-js-test/script.js
@@ -1,8 +1,8 @@
 const { __ } = wp.i18n;
 
-alert(__('js-alert', 'test'));
+alert(__('js-alert', 'cds-js-test'));
 
-console.log(__('js-log', 'test'));
+console.log(__('js-log', 'cds-js-test'));
 
 div = document.getElementById('h1');
-div.innerHTML += __('js-html', 'test');
+div.innerHTML += __('js-html', 'cds-js-test');

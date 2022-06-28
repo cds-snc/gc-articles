b5ed1a6d87f69bcc4deb414014ebe1ff1af617e5
Tim Arney <tim@line37.com>
2022-06-28 13:13:37 -0400

    add lock file

--- wordpress/wp-content/plugins/cds-js-test/sample-react-app/webpack.config.js
+++ wordpress/wp-content/plugins/cds-js-test/sample-react-app/webpack.config.js
@@ -22,16 +22,7 @@ module.exports = {
         use: {
           loader: "babel-loader",
         },
-      },
-      {
-        test: /\.(sa|sc|c)ss$/, // styles files
-        use: ["style-loader", "css-loader", "sass-loader"],
-      },
-      {
-        test: /\.(png|woff|woff2|eot|ttf|svg)$/, // to import images and fonts
-        loader: "url-loader",
-        options: { limit: false },
-      },
+      }
     ],
   },
 };

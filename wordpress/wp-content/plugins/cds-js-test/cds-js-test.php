57ad8abc0f019002cdf22d2b15671f23cf7af096
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2022-06-28 09:29:30 -0400

    Create a test plugin scaffold

---
+++ wordpress/wp-content/plugins/cds-js-test/cds-js-test.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Plugin Name:     Cds Js Test
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     cds-js-test
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Cds_Js_Test
+ */
+
+// Your code starts here.

605e2bf30fcad10c1f2d63cf05a8f11b5bfa8ddb
Tim Arney <tim@line37.com>
2022-06-28 16:06:27 -0400

    update

--- wordpress/wp-content/plugins/cds-js-test/sample-react-app/src/Home.js
+++ wordpress/wp-content/plugins/cds-js-test/sample-react-app/src/Home.js
@@ -1,6 +1,6 @@
 const Home = () => {
   const { __ } = wp.i18n;
-  return <><div>{__('REACT APP wp element', 'cds-js-test' )} - {__('js-html-react', 'cds-js-test')}!!</div> {__('more', 'cds-js-test')}!!</>;
+  return <><div>{__('REACT APP wp element', 'cds-js-test' )} - {__('js-html-react', 'cds-js-test')}!!</div> {__('more', 'cds-js-test')}!!{__('and more', 'cds-js-test')}</>;
 };
 
 export default Home;
--- wordpress/wp-content/plugins/cds-js-test/sample-react-app/webpack.config.js
+++ wordpress/wp-content/plugins/cds-js-test/sample-react-app/webpack.config.js
@@ -1,11 +1,19 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
+const I18nLoaderWebpackPlugin = require("@automattic/i18n-loader-webpack-plugin");
+const DependencyExtractionWebpackPlugin = require( '@wordpress/dependency-extraction-webpack-plugin' );
 
 module.exports = {
   externals: {
-		"react": "React",
-		"react-dom": "ReactDOM"
-	},
+    "react": "React",
+    "react-dom": "ReactDOM",
+  },
+  optimization: {
+    minimize: false,
+    concatenateModules: false,
+    
+  },
   output: {
     path: path.join(__dirname, "/dist"), // the bundle output path
     filename: "bundle.js", // the name of the bundle
@@ -15,6 +23,10 @@ module.exports = {
     new HtmlWebpackPlugin({
       template: "src/index.html", // to import index.html file inside index.js
     }),
+    new I18nLoaderWebpackPlugin({
+      textdomain: 'cds-js-test',
+    }),
+    new DependencyExtractionWebpackPlugin() 
   ],
   devServer: {
     port: 3030, // you can change the port

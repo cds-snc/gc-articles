902b93dce5c00c22a6b2b97d9826740cad248074
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2022-06-29 14:37:50 -0400

    Start new webpack setup

--- wordpress/wp-content/plugins/gc-lists/resources/js/src/index.tsx
+++ wordpress/wp-content/plugins/gc-lists/resources/js/src/index.tsx
@@ -2,52 +2,49 @@
  * External dependencies
  */
 import * as React from 'react';
-import { createRoot } from 'react-dom/client';
+
+// @ts-ignore
+const { render } = wp.element;
 
 /**
  * Internal dependencies
  */
 import './lists/index.css';
 import App from './App';
-import { ServiceData, User, NotifyList } from "./types";
+import { ServiceData, User, NotifyList } from './types';
+
 declare global {
-  interface Window {
-    CDS_LIST_MANAGER: { endpoint: string },
-    renderListManager: (el: string) => void,
-    CDS_VARS: {
-      rest_url?: string;
-      rest_nonce?: string;
-      notify_list_ids?: NotifyList[];
+    interface Window {
+        CDS_LIST_MANAGER: { endpoint: string },
+        renderListManager: (el: string) => void,
+        CDS_VARS: {
+            rest_url?: string;
+            rest_nonce?: string;
+            notify_list_ids?: NotifyList[];
+        }
     }
-  }
 }
 
-const renderApp = () => {
-  if (document.getElementById("list-manager-app")) {
-
+if (document.getElementById('list-manager-app')) {
     const container = document.getElementById('list-manager-app')!;
-    const root = createRoot(container);
-
     if (container) {
-      let data = container.getAttribute("data-ids");
-      let user = container.getAttribute("data-user");
-      let baseUrl = container.getAttribute('data-base-url');
+        let data = container.getAttribute('data-ids');
+        let user = container.getAttribute('data-user');
+        let baseUrl = container.getAttribute('data-base-url');
 
-      if (data && user) {
-        data = JSON.parse(data);
-        const serviceData = data as unknown as ServiceData;
+        if (data && user) {
+            data = JSON.parse(data);
+            const serviceData = data as unknown as ServiceData;
 
-        user = JSON.parse(user);
-        const userData = user as unknown as User;
+            user = JSON.parse(user);
+            const userData = user as unknown as User;
 
-        root.render(
-          <React.StrictMode>
-            <App serviceData={serviceData} user={userData} baseUrl={baseUrl} />
-          </React.StrictMode>
-        );
-      }
+            render(
+                <React.StrictMode>
+                    <App serviceData={serviceData} user={userData} baseUrl={baseUrl} />
+                </React.StrictMode>
+                , container
+            );
+        }
     }
-  }
 }
-
-renderApp();
---
+++ wordpress/wp-content/plugins/gc-lists/resources/js/webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const defaultConfig = require("@wordpress/scripts/config/webpack.config");
+
+module.exports = {
+    ...defaultConfig,
+    externals: {
+        "react": "React",
+        "react-dom": "ReactDOM"
+    },
+    entry: "./src/index.tsx",
+    module: {
+        ...defaultConfig.module,
+        rules: [
+            ...defaultConfig.module.rules,
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+        ],
+    },
+
+    resolve: {
+        ...defaultConfig.resolve,
+        extensions: [".tsx", ".ts", "js", "jsx"],
+    },
+
+    output: {
+        ...defaultConfig.output,
+        filename: "index.js",
+        path: path.resolve(__dirname, "build"),
+    }
+};

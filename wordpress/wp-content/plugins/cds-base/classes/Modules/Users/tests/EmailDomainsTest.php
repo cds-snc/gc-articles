264136af0989506cb3335df8361611bd2ac4ad94
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2022-06-29 17:26:11 -0400

    Refactor to Pest style

--- wordpress/wp-content/plugins/cds-base/classes/Modules/Users/tests/EmailDomainsTest.php
+++ wordpress/wp-content/plugins/cds-base/classes/Modules/Users/tests/EmailDomainsTest.php
@@ -2,65 +2,41 @@
 
 use CDS\Modules\Users\EmailDomains;
 
-class TestEmailDomains extends \WP_Mock\Tools\TestCase
-{
-    private $users;
+beforeAll(function () {
+    WP_Mock::setUp();
 
-    public function setUp(): void
-    {
-        \WP_Mock::setUp();
-        \WP_Mock::userFunction('is_email', array(
-            'return' => true,
-        ));
-    }
+    WP_Mock::userFunction('is_email', array(
+        'return' => true,
+    ));
+});
 
-    public function tearDown(): void
-    {
-        \WP_Mock::tearDown();
-    }
+afterAll(function () {
+    WP_Mock::tearDown();
+});
 
-    public function badEmails(): array
-    {
-        return [
-            "bad domain" => ['justin.trudeau@gmail.com'],
-            "no TLD" => ['justin.trudeau@cds-snc'],
-            "no @" => ['justin.trudeaucds-snc.ca'],
-            "no username" => ['@cds-snc.ca'],
-            "onmicrosoft without domain" => ['justin.trudeau@onmicrosoft.com'],
-            'empty' => [''],
-            'similar domain cds-snc.ca' => ['justin.trudeau@badcds-snc.ca'],
-            'similar domain canada.ca' => ['justin.truedau@badcanada.ca']
-        ];
-    }
+test('badEmails', function($email) {
+    expect(EmailDomains::isValidDomain($email))->toEqual(false);
+})->with([
+    "bad domain" => 'justin.trudeau@gmail.com',
+    "no TLD" => 'justin.trudeau@cds-snc',
+    "no @" => 'justin.trudeaucds-snc.ca',
+    "no username" => '@cds-snc.ca',
+    "onmicrosoft without domain" => 'justin.trudeau@onmicrosoft.com',
+    'empty' => '',
+    'similar domain cds-snc.ca' => 'justin.trudeau@badcds-snc.ca',
+    'similar domain canada.ca' => 'justin.truedau@badcanada.ca'
+]);
 
-    /**
-     * @dataProvider badEmails
-     */
-    public function testBadEmails(string $email): void
-    {
-        expect(EmailDomains::isValidDomain($email))->toEqual(false);
-    }
-
-    public function goodEmails(): array
-    {
-        return [
-            "CDS domain" => ['justin.trudeau@cds-snc.ca'],
-            "TBS domain" => ['justin.trudeau@tbs-sct.gc.ca'],
-            "random GC domain" => ['justin.trudeau@my-very-cool.department.gc.ca'],
-            "only GC domain" => ['justin.trudeau@gc.ca'], // pretty sure this is *not* valid
-            'Canada domain' => ['justin.trudeau@canada.ca'],
-            'Service Canada domain' => ['justin.trudeau@servicecanada.ca'],
-            'random Canada domain' => ['justin.trudeau@hockey.night.in.canada.ca'],
-            '‘innovation’ government domain @ PSPC' => ['justin.trudeau@pspcinnovation.onmicrosoft.com'],
-            'random ‘innovation’ Canada domain' => ['justin.trudeau@esdc-innovation.onmicrosoft.com'],
-        ];
-    }
-
-    /**
-     * @dataProvider goodEmails
-     */
-    public function testGoodemails(string $email): void
-    {
-        expect(EmailDomains::isValidDomain($email))->toEqual(true);
-    }
-}
+test('Good emails', function($email) {
+    expect(EmailDomains::isValidDomain($email))->toEqual(true);
+})->with([
+    "CDS domain" => 'justin.trudeau@cds-snc.ca',
+    "TBS domain" => 'justin.trudeau@tbs-sct.gc.ca',
+    "random GC domain" => 'justin.trudeau@my-very-cool.department.gc.ca',
+    "only GC domain" => 'justin.trudeau@gc.ca', // pretty sure this is *not* valid
+    'Canada domain' => 'justin.trudeau@canada.ca',
+    'Service Canada domain' => 'justin.trudeau@servicecanada.ca',
+    'random Canada domain' => 'justin.trudeau@hockey.night.in.canada.ca',
+    'pspc innovation domain' => 'justin.trudeau@pspcinnovation.onmicrosoft.com',
+    'esdc ‘innovation’ domain' => 'justin.trudeau@esdc-innovation.onmicrosoft.com',
+]);

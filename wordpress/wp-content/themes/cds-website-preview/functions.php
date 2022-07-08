c5387c0ccb0c69c5c4ce1dee566421d2144125b7
Tim Arney <tim@line37.com>
2022-07-08 11:31:20 -0400

    test hook

--- wordpress/wp-content/themes/cds-website-preview/functions.php
+++ wordpress/wp-content/themes/cds-website-preview/functions.php
@@ -1,3 +1,50 @@
 <?php
 
 add_theme_support('post-thumbnails');
+
+# https://github.com/cds-snc/cds-website-cms/blob/main/api/webhooks/controllers/webhooks.js#L9-L39
+
+PATCH = $(curl - X POST - H "Accept: application/vnd.github+json" - H "Authorization: token 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" - d '{"event_type":"strapi_update","client_payload":{}' https://api.github.com/repos/cds-snc/cds-website-pr-bot/dispatches)
+
+
+
+function onSavePost($post_ID, $post)
+{
+    // note this will fire for saves and updates
+    if ($post->post_status === "publish") {
+        $hostname = "https://api.github.com";
+        $path = "repos/cds-snc/cds-website-pr-bot/dispatches";
+
+        //$path = "repos/cds-snc/cds-website-pr-bot/languages";
+
+        $url = $hostname . '/' . $path;
+        $token = "";
+
+        $args = [
+            'headers' => [
+                'Accept' => 'application/vnd.github+json',
+                'Authorization' => 'token ' . $token,
+            ],
+            'body'        => [
+                'event_type' => 'strapi_update',
+                "client_payload" => json_encode(["test" => "true"])
+            ]
+        ];
+
+        error_log(print_r($args, true));
+        /*
+        $response = wp_remote_post($url);
+
+        if ( is_wp_error( $response ) ) {
+            $error_message = $response->get_error_message();
+            error_log( "Something went wrong: $error_message");
+        } else {
+            error_log('Response:');
+            error_log($response['body']);
+            error_log(print_r( $response, true ));
+        }
+        */
+    }
+}
+
+add_action('save_post', 'onSavePost', 10, 2);

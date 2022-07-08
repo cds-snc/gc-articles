1500179686ce65d0dd5b9e909ee88a900c32ec46
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2022-07-08 16:01:05 -0400

    Add a very basic test

--- wordpress/wp-content/plugins/gc-lists/tests/Integration/ListManager/TestProxy.php
+++ wordpress/wp-content/plugins/gc-lists/tests/Integration/ListManager/TestProxy.php
@@ -1,2 +1,35 @@
 <?php
 
+use GCLists\ListManager\Proxy;
+
+test('ListManager Proxy request', function() {
+    define('LIST_MANAGER_ENDPOINT', 'https://list-manager.cdssandbox.xyz');
+    define('DEFAULT_LIST_MANAGER_API_KEY', 'abcxyz');
+
+    // intercept/mock the HTTP proxy request
+    add_filter( 'pre_http_request', function() {
+        return [
+            'headers'     => [],
+            'cookies'     => [],
+            'filename'    => null,
+            'response'    => [
+                "code" => 200
+            ],
+            'status_code' => 200,
+            'success'     => 1,
+            'body'        => '{"status": "OK"}',
+        ];
+    }, 10, 3 );
+
+    $proxy = Proxy::getInstance();
+
+    $request = new WP_REST_Request( 'GET', '/list-manager/lists' );
+
+    $response = $proxy->proxyRequest($request);
+
+    $expected = (object) [
+        'status' => 'OK'
+    ];
+
+    $this->assertEquals($response->get_data(), $expected);
+});

173672bd9728fcdf0b2a92e5236bd432b95ee27b
Tim Arney <tim@line37.com>
2022-07-12 11:22:44 -0400

    add on save

--- wordpress/wp-content/themes/cds-website-preview/functions.php
+++ wordpress/wp-content/themes/cds-website-preview/functions.php
@@ -50,3 +50,46 @@ function cds_web_register_post_type()
 }
 
 add_action('init', 'cds_web_register_post_type');
+
+//
+
+function onSavePost($post_ID, $post)
+{
+    // note this will fire for saves and updates
+    if ($post->post_status === "publish") {
+        $hostname = "https://api.github.com";
+        $path = "repos/cds-snc/cds-website-pr-bot/dispatches";
+
+        //$path = "repos/cds-snc/cds-website-pr-bot/languages";
+
+        $url = $hostname . '/' . $path;
+        $token = "";
+
+        $args = [
+            'headers' => [
+                'Accept' => 'application/vnd.github+json',
+                'Authorization' => 'token ' . $token,
+            ],
+            'body'        => [
+                'event_type' => 'strapi_update',
+                "client_payload" => json_encode(["test" => "true"])
+            ]
+        ];
+
+        error_log(print_r($args, true));
+        /*
+        $response = wp_remote_post($url);
+
+        if ( is_wp_error( $response ) ) {
+            $error_message = $response->get_error_message();
+            error_log( "Something went wrong: $error_message");
+        } else {
+            error_log('Response:');
+            error_log($response['body']);
+            error_log(print_r( $response, true ));
+        }
+        */
+    }
+}
+
+add_action('save_post', 'onSavePost', 10, 2);

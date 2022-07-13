f2b77ee284d9d2d8d030fadc472175f97c88e703
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2022-07-13 14:11:39 -0400

    bit of cleanup

--- wordpress/wp-content/themes/cds-website-preview/settings.php
+++ wordpress/wp-content/themes/cds-website-preview/settings.php
@@ -5,6 +5,12 @@ use CDS\Utils;
 
 require_once __DIR__ . '/../../../vendor/autoload.php';
 
+/**
+ * Setup EncryptedOption.
+ * There is some duplicated code here from cds-base/classes/Setup.php because that class
+ * does not use the singleton patter we use elsewhere. If we refactor that class, we could remove some of this
+ * duplicate code and retrieve the initialized EncryptedOption from there.
+ */
 function getEncryptionKey()
 {
     /**
@@ -19,6 +25,17 @@ function getEncryptionKey()
 }
 
 $encryptedOption = new EncryptedOption(getEncryptionKey());
+
+$encryptedOptions = [
+    'GITHUB_AUTH_TOKEN',
+];
+
+if (!\CDS\Utils::isWpEnv()) {
+    foreach ($encryptedOptions as $option) {
+        add_filter("pre_update_option_{$option}", [$encryptedOption, 'encryptString']);
+        add_filter("option_{$option}", [$encryptedOption, 'decryptString']);
+    }
+}
 
 function cds_web_settings_init()
 {
@@ -42,13 +59,13 @@ function cds_web_settings_init()
     );
 
     add_settings_field(
-        'GITHUB_AUTH_TOKEN', // id
+        'github_auth_token', // id
         __('Github Auth Token', 'cds-snc'), // title
         'cds_web_auth_token_callback', // callback
         'cds-web-settings-admin', // page
         'cds_web_settings_setting_section', // section
         [
-            'label_for' => 'GITHUB_AUTH_TOKEN'
+            'label_for' => 'github_auth_token'
         ]
     );
 }
@@ -104,7 +121,7 @@ function cds_web_options_page()
     add_options_page(
         __('CDS Website Settings', 'cds-snc'), // page_title
         __('CDS Website Settings', 'cds-snc'), // menu_title
-        'manage_notify', // capability
+        'manage_options', // capability
         'cds-web-settings', // menu_slug
         'cds_web_options_page_html' // function
     );
@@ -112,50 +129,18 @@ function cds_web_options_page()
 
 function cds_web_options_page_html()
 {
-
-    // check user capabilities
-    if (! current_user_can('manage_options')) {
-        return;
-    }
-
-    // add error/update messages
-
-    // check if the user have submitted the settings
-    // WordPress will add the "settings-updated" $_GET parameter to the url
-//    if ( isset( $_GET['settings-updated'] ) ) {
-//        // add settings saved message with the class of "updated"
-//        add_settings_error( 'wporg_messages', 'wporg_message', __( 'Settings Saved', 'wporg' ), 'updated' );
-//    }
-//
-//    // show error/update messages
-//    settings_errors( 'wporg_messages' );
     ?>
     <div class="wrap">
         <h1><?php echo esc_html(get_admin_page_title()); ?></h1>
-        <form action="options.php" method="post">
+        <form method="post" action="options.php" id="notify_settings_form" class="gc-form-wrapper">
             <?php
-            // output security fields for the registered setting "wporg"
             settings_fields('github_auth_token_option_group');
-            // output setting sections and their fields
-            // (sections are registered for "wporg", each field is registered to a specific section)
             do_settings_sections('cds-web-settings-admin');
-            // output save settings button
-            submit_button('Save Settings');
+            submit_button(__('Save Changes', 'cds-snc'));
             ?>
         </form>
     </div>
     <?php
-}
-
-$encryptedOptions = [
-    'GITHUB_AUTH_TOKEN',
-];
-
-if (!\CDS\Utils::isWpEnv()) {
-    foreach ($encryptedOptions as $option) {
-        add_filter("pre_update_option_{$option}", [$encryptedOption, 'encryptString']);
-        add_filter("option_{$option}", [$encryptedOption, 'decryptString']);
-    }
 }
 
 add_action('admin_menu', 'cds_web_options_page', 100);

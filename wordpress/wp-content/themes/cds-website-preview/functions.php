1bf67c69c51052fd12e4d86048bc6a9c4b136c45
Tim Arney <tim@line37.com>
2022-07-18 10:59:27 -0400

    theme updates

--- wordpress/wp-content/themes/cds-website-preview/functions.php
+++ wordpress/wp-content/themes/cds-website-preview/functions.php
@@ -4,6 +4,10 @@ include get_theme_file_path('/inc/webhook.php');
 include get_theme_file_path('/post-types/job.php');
 include get_theme_file_path('/post-types/project.php');
 
+function get_fonts_uri(){
+    return get_stylesheet_directory_uri()."/fonts/";
+}
+
 add_theme_support('post-thumbnails');
 
 require_once(__DIR__ . '/settings.php');

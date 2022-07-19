c8b08431b872e2f7dc1a062f9c02e62edcc8133b
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2022-07-19 16:06:14 -0400

    Initial setup

---
+++ wordpress/wp-content/plugins/cds-wpml-mods/cds-wpml-mods.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Plugin Name:     Cds Wpml Mods
+ * Plugin URI:      PLUGIN SITE HERE
+ * Description:     PLUGIN DESCRIPTION HERE
+ * Author:          YOUR NAME HERE
+ * Author URI:      YOUR SITE HERE
+ * Text Domain:     cds-wpml-mods
+ * Domain Path:     /languages
+ * Version:         0.1.0
+ *
+ * @package         Cds_Wpml_Mods
+ */
+
+// Your code starts here.
---
+++ wordpress/wp-content/plugins/cds-wpml-mods/tests/test-sample.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Class SampleTest
+ *
+ * @package Cds_Wpml_Mods
+ */
+
+/**
+ * Sample test case.
+ */
+class SampleTest extends WP_UnitTestCase {
+
+	/**
+	 * A single example test.
+	 */
+	public function test_sample() {
+		// Replace this with some actual testing code.
+		$this->assertTrue( true );
+	}
+}

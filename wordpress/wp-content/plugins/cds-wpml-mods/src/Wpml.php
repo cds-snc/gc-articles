f7f79ba9162353e7e267a0dc0a1cc0ccfe2c3723
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2022-07-19 16:29:18 -0400

    Add some basics

--- wordpress/wp-content/plugins/cds-wpml-mods/src/Wpml.php
+++ wordpress/wp-content/plugins/cds-wpml-mods/src/Wpml.php
@@ -2,14 +2,30 @@
 
 namespace CDS\Wpml;
 
+use CDS\Wpml\Api\Endpoints;
+
 class Wpml
 {
     protected static $instance;
+
+    protected Endpoints $endpoints;
 
     public static function getInstance(): Wpml
     {
         is_null(self::$instance) and self::$instance = new self();
 
         return self::$instance;
+    }
+
+    public function setup()
+    {
+        $this->endpoints = Endpoints::getInstance();
+
+        $this->addHooks();
+    }
+
+    public function addHooks()
+    {
+        //
     }
 }

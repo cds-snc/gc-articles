0ebb6c412cdc3db98743136c8ce79e6ce966d8b9
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2022-07-19 13:34:42 -0400

    Sanitize input and escape output of custom Site Settings options

--- wordpress/wp-content/plugins/cds-base/classes/Modules/Site/SiteSettings.php
+++ wordpress/wp-content/plugins/cds-base/classes/Modules/Site/SiteSettings.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace CDS\Modules\Site;
 
+use Carbon\Exceptions\ParseErrorException;
+
 class SiteSettings
 {
     public function __construct()
@@ -53,7 +55,7 @@ class SiteSettings
             __('Site Settings', "cds-snc"), // menu_title
             'manage_options', // capability
             'collection-settings', // menu_slug
-            array( $this, 'collectionSettingsCreateAdminPage' ) // function
+            array($this, 'collectionSettingsCreateAdminPage') // function
         );
     }
 
@@ -62,17 +64,17 @@ class SiteSettings
 
         ?>
 
-        <div class="wrap">
-            <h1><?php _e('Site Settings', 'cds-snc') ?></h1>
+      <div class="wrap">
+        <h1><?php _e('Site Settings', 'cds-snc') ?></h1>
 
-            <form method="post" action="options.php" id="collection_settings_form" class="gc-form-wrapper">
-                <?php
-                settings_fields('site_settings_group');
-                do_settings_sections('collection-settings-admin');
-                submit_button();
-                ?>
-            </form>
-        </div>
+        <form method="post" action="options.php" id="collection_settings_form" class="gc-form-wrapper">
+            <?php
+            settings_fields('site_settings_group');
+            do_settings_sections('collection-settings-admin');
+            submit_button();
+            ?>
+        </form>
+      </div>
     <?php }
 
     public function collectionSettingsPageInit()
@@ -89,7 +91,7 @@ class SiteSettings
         add_settings_section(
             'collection_settings_section_maintenance', // id
             __("Maintenance mode", 'cds-snc'), // title
-            array( $this, 'maintenanceDescriptionCallback'), // callback
+            array($this, 'maintenanceDescriptionCallback'), // callback
             'collection-settings-admin' // page
         );
 
@@ -103,70 +105,122 @@ class SiteSettings
 
         register_setting(
             'site_settings_group', // option_group
-            'collection_mode',
-        );
-
-        register_setting(
-            'site_settings_group', // option_group
             'collection_mode_maintenance_page',
+            function ($input) {
+                if (is_int($input)) {
+                    return $input;
+                }
+
+                return 0;
+            }
         );
 
         // reading options
         register_setting(
             'site_settings_group', // option_group
             'show_on_front',
+            function ($input) {
+                return 'page'; // this is a hardcoded hidden field
+            }
         );
 
         register_setting(
             'site_settings_group', // option_group
             'page_on_front',
+            function ($input) {
+                if (is_int($input)) {
+                    return $input;
+                }
+
+                return 0;
+            }
         );
 
         register_setting(
             'site_settings_group', // option_group
             'collection_mode',
+            function ($input) {
+                if (in_array($input, ['maintenance', 'live'])) {
+                    return $input;
+                }
+
+                return 'live';
+            }
         );
 
         register_setting(
             'site_settings_group', // option_group
             'blogname',
+            function ($input) {
+                return sanitize_text_field($input);
+            }
         );
 
         register_setting(
             'site_settings_group', // option_group
             'blogdescription',
+            function ($input) {
+                return sanitize_text_field($input);
+            }
         );
 
         register_setting(
             'site_settings_group', // option_group
             'show_wet_menu',
+            function ($input) {
+                if (in_array($input, ['on', 'off'])) {
+                    return $input;
+                }
+
+                return 'off';
+            }
         );
 
         register_setting(
             'site_settings_group', // option_group
             'show_search',
+            function ($input) {
+                if (in_array($input, ['on', 'off'])) {
+                    return $input;
+                }
+
+                return 'off';
+            }
         );
 
         register_setting(
             'site_settings_group', // option_group
             'show_breadcrumbs',
+            function ($input) {
+                if (in_array($input, ['on', 'off'])) {
+                    return $input;
+                }
+
+                return 'off';
+            }
         );
 
         register_setting(
             'site_settings_group', // option_group
             'fip_href',
+            function ($input) {
+                return esc_url_raw($input);
+            }
         );
 
         register_setting(
             'site_settings_group', // option_group
             'analytics_id',
+            function ($input) {
+                return sanitize_text_field($input);
+            }
         );
 
         // add fields GENERAL
         add_settings_field(
             'blogname', // id
             __('Site Name', 'cds-snc'), // title
-            array( $this, 'blogNameCallback'), // callback
+            array($this, 'blogNameCallback'), // callback
             'collection-settings-admin', // page
             'collection_settings_section_general', // section
             [
@@ -177,7 +231,7 @@ class SiteSettings
         add_settings_field(
             'blogdescription', // id
             __('Site Description', 'cds-snc'), // title
-            array( $this, 'blogDescriptionCallback'), // callback
+            array($this, 'blogDescriptionCallback'), // callback
             'collection-settings-admin', // page
             'collection_settings_section_general', // section
             [
@@ -188,7 +242,7 @@ class SiteSettings
         add_settings_field(
             'page_on_front', // id
             __('Home Page', 'cds-snc'), // title
-            array( $this, 'readingSettingsCallback'), // callback
+            array($this, 'readingSettingsCallback'), // callback
             'collection-settings-admin', // page
             'collection_settings_section_general', // section
             [
@@ -200,7 +254,7 @@ class SiteSettings
         add_settings_field(
             'collection_mode', // id
             __('Activate maintenance mode', 'cds-snc'), // title
-            array( $this, 'collectionModeCallback'), // callback
+            array($this, 'collectionModeCallback'), // callback
             'collection-settings-admin', // page
             'collection_settings_section_maintenance', // section
             [
@@ -211,7 +265,7 @@ class SiteSettings
         add_settings_field(
             'collection_mode_maintenance_page', // id
             __('Maintenance Page', 'cds-snc'), // title
-            array( $this, 'collectionMaintenancePageCallback'), // callback
+            array($this, 'collectionMaintenancePageCallback'), // callback
             'collection-settings-admin', // page
             'collection_settings_section_maintenance', // section
             [
@@ -222,7 +276,7 @@ class SiteSettings
         add_settings_field(
             'blog_public', // id
             __('Search engine visibility', 'cds-snc'), // title
-            array( $this, 'indexSiteCallback'), // callback
+            array($this, 'indexSiteCallback'), // callback
             'collection-settings-admin', // page
             'collection_settings_section_general', // section
             [
@@ -234,7 +288,7 @@ class SiteSettings
         add_settings_field(
             'show_wet_menu', // id
             __('Canada.ca top menu', 'cds-snc'), // title
-            array( $this, 'wetMenuCallback'), // callback
+            array($this, 'wetMenuCallback'), // callback
             'collection-settings-admin', // page
             'collection_settings_section_config', // section
             [
@@ -245,7 +299,7 @@ class SiteSettings
         add_settings_field(
             'show_search', // id
             __('Search bar', 'cds-snc'), // title
-            array( $this, 'showSearchCallback'), // callback
+            array($this, 'showSearchCallback'), // callback
             'collection-settings-admin', // page
             'collection_settings_section_config', // section
             [
@@ -262,7 +316,7 @@ class SiteSettings
         add_settings_field(
             'show_breadcrumbs', // id
             __('Breadcrumbs', 'cds-snc'), // title
-            array( $this, 'breadcrumbsCallback'), // callback
+            array($this, 'breadcrumbsCallback'), // callback
             'collection-settings-admin', // page
             'collection_settings_section_config', // section
             [
@@ -273,7 +327,7 @@ class SiteSettings
         add_settings_field(
             'fip_href', // id
             __('Where should the Canada.ca header link to?', 'cds-snc'), // title
-            array( $this, 'fipHrefCallback'), // callback
+            array($this, 'fipHrefCallback'), // callback
             'collection-settings-admin', // page
             'collection_settings_section_config', // section
             [
@@ -292,7 +346,7 @@ class SiteSettings
         add_settings_field(
             'analytics_id', // id
             __('Analytics id', 'cds-snc'), // title
-            array( $this, 'analyticsCallback'), // callback
+            array($this, 'analyticsCallback'), // callback
             'collection-settings-admin', // page
             'collection_settings_section_analytics', // section
             [
@@ -305,42 +359,74 @@ class SiteSettings
     {
         $collection_mode = get_option('collection_mode');
 
-        printf('<input type="radio" name="collection_mode" id="collection_maintenance" value="maintenance" %s /> <label for="collection_maintenance">%s</label><br />', checked('maintenance', $collection_mode, false), __('Turn on', "cds-snc"));
-        printf('<input type="radio" name="collection_mode" id="collection_live" value="live" %s /> <label for="collection_live">%s</label><br />', checked('live', $collection_mode, false), __('Turn off', "cds-snc"));
+        printf(
+            '<input type="radio" name="collection_mode" id="collection_maintenance" value="maintenance" %s /> <label for="collection_maintenance">%s</label><br />',
+            checked('maintenance', $collection_mode, false),
+            __('Turn on', "cds-snc")
+        );
+        printf(
+            '<input type="radio" name="collection_mode" id="collection_live" value="live" %s /> <label for="collection_live">%s</label><br />',
+            checked('live', $collection_mode, false),
+            __('Turn off', "cds-snc")
+        );
     }
 
     public function wetMenuCallback()
     {
         $show_wet_menu = get_option('show_wet_menu');
 
-        printf('<input type="radio" name="show_wet_menu" id="show_wet_menu_on" value="on" %s /> <label for="show_wet_menu_on">%s</label><br />', checked("on", $show_wet_menu, false), __('Show Canada.ca menu', "cds-snc"));
-        printf('<input type="radio" name="show_wet_menu" id="show_wet_menu_off" value="off" %s /> <label for="show_wet_menu_off">%s</label><br />', checked("off", $show_wet_menu, false), __('Hide Canada.ca menu', "cds-snc"));
+        printf(
+            '<input type="radio" name="show_wet_menu" id="show_wet_menu_on" value="on" %s /> <label for="show_wet_menu_on">%s</label><br />',
+            checked("on", $show_wet_menu, false),
+            __('Show Canada.ca menu', "cds-snc")
+        );
+        printf(
+            '<input type="radio" name="show_wet_menu" id="show_wet_menu_off" value="off" %s /> <label for="show_wet_menu_off">%s</label><br />',
+            checked("off", $show_wet_menu, false),
+            __('Hide Canada.ca menu', "cds-snc")
+        );
     }
 
     public function showSearchCallback()
     {
         $show_search = get_option('show_search');
 
-        printf('<input type="radio" name="show_search" id="show_search_on" value="on" %s /> <label for="show_search_on">%s</label><br />', checked('on', $show_search, false), __('Show the search bar', "cds-snc"));
-        printf('<input type="radio" name="show_search" id="show_search_off" value="off" %s /> <label for="show_search_off">%s</label><br />', checked('off', $show_search, false), __('Hide the search bar', "cds-snc"));
+        printf(
+            '<input type="radio" name="show_search" id="show_search_on" value="on" %s /> <label for="show_search_on">%s</label><br />',
+            checked('on', $show_search, false),
+            __('Show the search bar', "cds-snc")
+        );
+        printf(
+            '<input type="radio" name="show_search" id="show_search_off" value="off" %s /> <label for="show_search_off">%s</label><br />',
+            checked('off', $show_search, false),
+            __('Hide the search bar', "cds-snc")
+        );
     }
 
     public function breadcrumbsCallback()
     {
         $show_breadcrumbs = get_option('show_breadcrumbs');
 
-        printf('<input type="radio" name="show_breadcrumbs" id="show_breadcrumbs_on" value="on" %s /> <label for="show_breadcrumbs_on">%s</label><br />', checked("on", $show_breadcrumbs, false), __('Show breadcrumbs', "cds-snc"));
-        printf('<input type="radio" name="show_breadcrumbs" id="show_breadcrumbs_off" value="off" %s /> <label for="show_breadcrumbs_off">%s</label><br />', checked("off", $show_breadcrumbs, false), __('Hide breadcrumbs', "cds-snc"));
+        printf(
+            '<input type="radio" name="show_breadcrumbs" id="show_breadcrumbs_on" value="on" %s /> <label for="show_breadcrumbs_on">%s</label><br />',
+            checked("on", $show_breadcrumbs, false),
+            __('Show breadcrumbs', "cds-snc")
+        );
+        printf(
+            '<input type="radio" name="show_breadcrumbs" id="show_breadcrumbs_off" value="off" %s /> <label for="show_breadcrumbs_off">%s</label><br />',
+            checked("off", $show_breadcrumbs, false),
+            __('Hide breadcrumbs', "cds-snc")
+        );
     }
 
     public function fipHrefCallback()
     {
         $fipUrl = get_option("fip_href", "");
-        $value = $fipUrl ? esc_url($fipUrl) : home_url();
-        $value = str_replace('http://', 'https://', $value);
+        $value  = $fipUrl ? esc_url($fipUrl) : home_url();
+        $value  = str_replace('http://', 'https://', $value);
 
         ?>
-        <input name="fip_href" type="text" id="fip_href" class="regular-text" value="<?php echo $value; ?>">
+      <input name="fip_href" type="text" id="fip_href" class="regular-text" value="<?php echo $value; ?>">
         <?php
     }
 
@@ -348,7 +434,8 @@ class SiteSettings
     {
         $analyticsId = get_option("analytics_id", "");
         ?>
-        <input name="analytics_id" type="text" id="analytics_id" class="regular-text" value="<?php echo $analyticsId; ?>">
+      <input name="analytics_id" type="text" id="analytics_id" class="regular-text"
+             value="<?php echo esc_attr($analyticsId); ?>">
         <?php
     }
 
@@ -369,46 +456,57 @@ class SiteSettings
     public function readingSettingsCallback()
     {
 
-           echo '<input name="show_on_front" type="hidden" value="page">';
+        echo '<input name="show_on_front" type="hidden" value="page">';
 
-            wp_dropdown_pages(
-                array(
-                    'name' => 'page_on_front',
-                    'echo' => 1,
-                    'show_option_none' => __('&mdash; Select &mdash;'),
-                    'option_none_value' => '0',
-                    'selected' => get_option('page_on_front'),
-                )
-            );
+        wp_dropdown_pages(
+            array(
+                'name'              => 'page_on_front',
+                'echo'              => 1,
+                'show_option_none'  => __('&mdash; Select &mdash;'),
+                'option_none_value' => '0',
+                'selected'          => get_option('page_on_front'),
+            )
+        );
     }
 
     public function blogNameCallback()
     {
         ?>
-        <input name="blogname" type="text" id="blogname" class="regular-text" value="<?php echo get_option("blogname");?>">
+      <input name="blogname" type="text" id="blogname" class="regular-text"
+             value="<?php echo esc_attr(get_option("blogname")); ?>">
         <?php
     }
 
     public function blogDescriptionCallback()
     {
         ?>
-        <input name="blogdescription" type="text" id="blogdescription" class="regular-text" value="<?php echo get_option("blogdescription");?>">
+      <input name="blogdescription" type="text" id="blogdescription" class="regular-text"
+             value="<?php echo esc_attr(get_option("blogdescription")); ?>">
         <?php
     }
 
     public function indexSiteCallback()
     {
         ?>
-        <input name="blog_public" type="checkbox" id="blog_public" value="0" <?php checked('0', get_option('blog_public')); ?> />
+      <input name="blog_public" type="checkbox" id="blog_public" value="0" <?php checked(
+          '0',
+          get_option('blog_public')
+                                                                           ); ?> />
         <?php _e('Discourage search engines from indexing this site'); ?>
-        <p class="description"><?php _e('It is up to search engines to honor this request.'); ?></p>
+      <p class="description"><?php _e('It is up to search engines to honor this request.'); ?></p>
         <?php
     }
 
     public function maintenanceDescriptionCallback()
     {
-        echo __('In maintenance mode, pages and articles you publish will <strong>not</strong> be publicly visible.', 'cds-snc');
+        echo __(
+            'In maintenance mode, pages and articles you publish will <strong>not</strong> be publicly visible.',
+            'cds-snc'
+        );
         echo '<br />';
-        echo __('Logged-in users will be able to create and view content, but all other visitors will be redirected to the maintenance page.', 'cds-snc');
+        echo __(
+            'Logged-in users will be able to create and view content, but all other visitors will be redirected to the maintenance page.',
+            'cds-snc'
+        );
     }
 }

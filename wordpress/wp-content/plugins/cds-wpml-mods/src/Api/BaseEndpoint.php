1f502a6d8661a9d88a0ecdee5a2b5ebbcbdbc36e
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2022-07-19 20:15:57 +0000

    Refactor to CDS namespace

---
+++ wordpress/wp-content/plugins/cds-wpml-mods/src/Api/BaseEndpoint.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+namespace CDS\Wpml\Api;
+
+class BaseEndpoint
+{
+    protected string $namespace;
+
+    public function __construct()
+    {
+        $this->namespace = "gc-lists";
+    }
+}

e41d758525a311d0a335f075e13a37243def1c33
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2022-07-19 17:08:09 -0400

    Stub out some endpoints

--- wordpress/wp-content/plugins/cds-wpml-mods/src/Api/Endpoints.php
+++ wordpress/wp-content/plugins/cds-wpml-mods/src/Api/Endpoints.php
@@ -17,25 +17,87 @@ class Endpoints extends BaseEndpoint
         return self::$instance;
     }
 
-    public function hasPermission(): bool
+    public function hasPermission($pageId = null): bool
     {
+        // if an optional page_id is provided, check that the user has permissions to it (ie same site)
         return true; // current_user_can('delete_posts');
     }
 
     public function registerRestRoutes()
     {
         // Get available pages by language
-        register_rest_route($this->namespace, '/pages/' . '(?P<language>en|fr)', [
+        register_rest_route($this->namespace, '/(?P<type>pages|posts)/(?P<language>en|fr)', [
             'methods'             => 'GET',
-            'callback'            => [$this, 'all'],
+            'callback'            => [$this, 'getAvailablePages'],
+            'permission_callback' => function () {
+                return $this->hasPermission();
+            }
+        ]);
+
+        // Associate a page as a translation to a given page
+        register_rest_route($this->namespace, '/(?P<type>pages|posts)/(?P<id>[\d]+)', [
+            'methods' => 'POST',
+            'callback' => [$this, 'saveTranslation'],
+            'permission_callback' => function () {
+                return $this->hasPermission();
+            }
+        ]);
+
+        // Get a page's associated translation
+        register_rest_route($this->namespace, '/(?P<type>pages|posts)/(?P<id>[\d]+)', [
+            'methods'             => 'GET',
+            'callback'            => [$this, 'getTranslation'],
             'permission_callback' => function () {
                 return $this->hasPermission();
             }
         ]);
     }
 
-    public function all(WP_REST_Request $request)
+    /**
+     * Retrieve untranslated pages of the specified language.
+     *
+     * @param  WP_REST_Request  $request
+     *
+     * @return mixed
+     */
+    public function getAvailablePages(WP_REST_Request $request)
     {
-        return $request['language'];
+        return [
+            $request['language'],
+            $request['type'],
+        ];
+    }
+
+    /**
+     * Associate a page as a translation of a given page.
+     *
+     * @param  WP_REST_Request  $request
+     *
+     * @return array
+     */
+    public function saveTranslation(WP_REST_Request $request)
+    {
+        // disassociate any existing translations
+        // create new entry in translations table
+
+        return [
+            $request['language'],
+            $request['type'],
+        ];
+    }
+
+    /**
+     * Get the existing translation for a given page.
+     *
+     * @param  WP_REST_Request  $request
+     *
+     * @return array
+     */
+    public function getTranslation(WP_REST_Request $request)
+    {
+        return [
+            $request['language'],
+            $request['type'],
+        ];
     }
 }

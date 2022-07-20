dec848d2376ab6f82faf75e4fb5a91d1850d8ac9
Tim Arney <tim@line37.com>
2022-07-20 12:39:28 -0400

    add to document sidebar

--- wordpress/wp-content/plugins/cds-wpml-mods/resources/js/sidebar.js
+++ wordpress/wp-content/plugins/cds-wpml-mods/resources/js/sidebar.js
@@ -2,9 +2,9 @@
 
 // https://rudrastyh.com/gutenberg/plugin-sidebars.html
 const { registerPlugin } = wp.plugins;
-const { PluginSidebar, PluginSidebarMoreMenuItem } = wp.editPost;
+const { PluginSidebar, PluginSidebarMoreMenuItem, PluginDocumentSettingPanel } = wp.editPost;
 const { Fragment } = wp.element;
-const { PanelBody } = wp.components;
+const { PanelBody, PanelRow } = wp.components;
 
 import { PageSelect } from "./components/PageSelect";
 const { __ } = wp.i18n;
@@ -16,6 +16,14 @@ registerPlugin('cds-wpml-mods', {
 				<PluginSidebarMoreMenuItem target="cds-wpml-mods" icon="admin-links">
 					{__("Translations", "cds-wpml-mods")}
 				</PluginSidebarMoreMenuItem>
+
+
+				<PluginDocumentSettingPanel title={__('Translations', 'cds-wpml-mods')} initialOpen="true">
+					<PanelRow>
+						Hello there.
+					</PanelRow>
+				</PluginDocumentSettingPanel>
+
 				<PluginSidebar name="cds-wpml-mods" title={__("Translations", "cds-wpml-mods")} icon="admin-links">
 					<PanelBody title={__("Linked page", "cds-wpml-mods")} initialOpen={true}>
 						<PageSelect />

9e8ee6170635859d05a76a019a5f766480cdeb77
Dave Samojlenko <dave.samojlenko@cds-snc.ca>
2022-07-29 12:24:54 -0400

    Change the enqueue

--- wordpress/wp-content/plugins/cds-wpml-mods/src/Wpml.php
+++ wordpress/wp-content/plugins/cds-wpml-mods/src/Wpml.php
@@ -28,19 +28,27 @@ class Wpml
     {
         add_action('rest_api_init', [$this->endpoints, 'registerRestRoutes']);
 
-        add_action('enqueue_block_editor_assets', function () {
-            wp_enqueue_script(
-                'cds-wpml-mods',
-                plugin_dir_url(__FILE__) . '../resources/js/build/sidebar.js',
-                array( 'wp-edit-post', 'wp-element', 'wp-components', 'wp-plugins', 'wp-data' ),
-                filemtime(plugin_dir_path(__FILE__) . '../resources/js/build/sidebar.js')
-            );
+        // Enqueue scripts
+        add_action('enqueue_block_editor_assets', [$this, 'enqueue']);
+    }
 
-            wp_localize_script('cds-wpml-mods', 'CDS_VARS', [
-                'rest_url' => esc_url_raw(rest_url()),
-            ]);
+    public function enqueue()
+    {
+        wp_enqueue_script(
+            'cds-wpml-mods',
+            plugin_dir_url(__FILE__) . '../resources/js/build/sidebar.js',
+            array('wp-edit-post', 'wp-element', 'wp-components', 'wp-plugins', 'wp-data'),
+            filemtime(plugin_dir_path(__FILE__) . '../resources/js/build/sidebar.js')
+        );
 
-            wp_set_script_translations('cds-wpml-mods', 'cds-wpml-mods', CDS_WPML_PLUGIN_BASE_PATH . '/resources/languages/');
-        });
+        wp_localize_script('cds-wpml-mods', 'CDS_VARS', [
+            'rest_url' => esc_url_raw(rest_url()),
+        ]);
+
+        wp_set_script_translations(
+            'cds-wpml-mods',
+            'cds-wpml-mods',
+            CDS_WPML_PLUGIN_BASE_PATH . '/resources/languages/'
+        );
     }
 }

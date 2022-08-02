c7e1c9c6546531676e2935f995e210c2350678e5
Paul Craig <paul@pcraig3.ca>
2022-08-02 14:10:13 -0400

    Remove WPML Language panel with JS and not CSS (#1011)

--- wordpress/wp-content/plugins/cds-wpml-mods/resources/js/sidebar.js
+++ wordpress/wp-content/plugins/cds-wpml-mods/resources/js/sidebar.js
@@ -5,9 +5,13 @@ const { registerPlugin } = wp.plugins;
 const { PluginDocumentSettingPanel } = wp.editPost;
 const { Fragment } = wp.element;
 const { PanelRow } = wp.components;
+const { dispatch } = wp.data;
 
 import { PageSelect } from "./components/PageSelect";
 const { __ } = wp.i18n;
+
+/* Remove the WPML "Language" panel */
+dispatch('core/edit-post').removeEditorPanel('meta-box-icl_div');
 
 registerPlugin('cds-wpml-mods', {
 	icon: 'translation',

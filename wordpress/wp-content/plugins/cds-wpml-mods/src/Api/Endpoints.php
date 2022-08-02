399a18c5c4bfbf159cc91408ad0aa3aba3c7dd8c
Paul Craig <paul@pcraig3.ca>
2022-08-02 13:31:00 -0400

    Add DELETE route that unlinks a translation
    This route resets the TRID for a post, as well as its translation.
    
    Also refactored the code around sending the post type to WPML since
    it was a little confusing.

--- wordpress/wp-content/plugins/cds-wpml-mods/src/Api/Endpoints.php
+++ wordpress/wp-content/plugins/cds-wpml-mods/src/Api/Endpoints.php
@@ -103,6 +103,15 @@ class Endpoints extends BaseEndpoint
                 return $this->hasPermission();
             }
         ]);
+
+        // Unset a page's associated translation
+        register_rest_route($this->namespace, '/posts/(?P<id>[\d]+)/translation', [
+            'methods'             => 'DELETE',
+            'callback'            => [$this, 'unsetTranslation'],
+            'permission_callback' => function () {
+                return $this->hasPermission();
+            }
+        ]);
     }
 
     /**
@@ -114,11 +123,11 @@ class Endpoints extends BaseEndpoint
      */
     public function getAvailablePages(WP_REST_Request $request): WP_REST_Response
     {
-        $post_type = $request['type'] === 'pages' ? 'page' : 'post';
+        $postType = $request['type'] === 'pages' ? 'page' : 'post';
 
         $language_code = $request['language'];
 
-        $posts = $this->post->getAvailable($post_type, $language_code);
+        $posts = $this->post->getAvailable($postType, $language_code);
 
         $response = new WP_REST_Response($posts);
 
@@ -143,23 +152,24 @@ class Endpoints extends BaseEndpoint
 
         $originalPostLanguage = $this->post->getLanguageCodeOfPostObject($originalPost);
         $targetPostLanguage = $this->post->getLanguageCodeOfPostObject($targetPost);
+        $postType = 'post_' . $originalPost->post_type;
 
         // Unset existing translation if exists
         if ($translatedPostID = $this->post->getTranslatedPostID($originalPost)) {
-            $this->post->setTranslationForPost($translatedPostID, $originalPost->post_type, $targetPostLanguage);
+            $this->post->setTranslationForPost($translatedPostID, $postType, $targetPostLanguage);
         }
 
-        // Unset existing translation if exists
+        // Unset existing translation for the target post if exists
         if ($otherTranslatedPostID = $this->post->getTranslatedPostID($targetPost)) {
-            $this->post->setTranslationForPost($otherTranslatedPostID, $targetPost->post_type, $originalPostLanguage);
+            $this->post->setTranslationForPost($otherTranslatedPostID, $postType, $originalPostLanguage);
         }
 
         // Note that post_trid is a string (normal ids are integers)
-        $postTrid = $sitepress->get_element_trid($originalPost->ID, 'post_' . $originalPost->post_type);
+        $postTrid = $sitepress->get_element_trid($originalPost->ID, $postType);
 
         // Set translations for each post
-        $this->post->setTranslationForPost($originalPost->ID, $originalPost->post_type, $originalPostLanguage, $postTrid);
-        $this->post->setTranslationForPost($targetPost->ID, $targetPost->post_type, $targetPostLanguage, $postTrid, $originalPostLanguage);
+        $this->post->setTranslationForPost($originalPost->ID, $postType, $originalPostLanguage, $postTrid);
+        $this->post->setTranslationForPost($targetPost->ID, $postType, $targetPostLanguage, $postTrid, $originalPostLanguage);
 
         // Return the details
         $response = new WP_REST_Response($this->post->buildResponseObject(post: $originalPost, withTranslations: true));
@@ -184,6 +194,40 @@ class Endpoints extends BaseEndpoint
         if (is_null($post)) {
             return new WP_Error('post_not_found', __('The post you are looking for does not exist', 'cds-wp-mods'), array( 'status' => 404 ));
         }
+
+        $response = new WP_REST_Response($this->post->buildResponseObject(post: $post, withTranslations: true));
+
+        $response->set_status(200);
+
+        return rest_ensure_response($response);
+    }
+
+    /**
+     * Get the existing translation for a given page.
+     * Returns a WP_Error if no post is found for the id given.
+     *
+     * @param  WP_REST_Request  $request
+     *
+     * @return array | WP_Error
+     */
+    public function unsetTranslation(WP_REST_Request $request)// : WP_REST_Response|WP_Error
+    {
+        $post = get_post($request['id']);
+
+        if (is_null($post)) {
+            return new WP_Error('post_not_found', __('The post you are looking for does not exist', 'cds-wp-mods'), array( 'status' => 404 ));
+        }
+
+        $postType = 'post_' . $post->post_type;
+        $language_code = $this->post->getLanguageCodeOfPostObject($post);
+
+        // Unset existing translation if exists
+        if ($translatedPostID = $this->post->getTranslatedPostID($post)) {
+            $altLanguage = $language_code === 'en' ? 'fr' : 'en';
+            $this->post->setTranslationForPost($translatedPostID, $postType, $altLanguage);
+        }
+
+        $this->post->setTranslationForPost($post->ID, $postType, $language_code);
 
         $response = new WP_REST_Response($this->post->buildResponseObject(post: $post, withTranslations: true));
 
--- wordpress/wp-content/plugins/cds-wpml-mods/src/Post.php
+++ wordpress/wp-content/plugins/cds-wpml-mods/src/Post.php
@@ -63,19 +63,24 @@ class Post
     }
 
     /**
-     * @param $postId
-     * @param $postType
-     * @param $languageCode
-     * @param  false  $trid
-     * @param  null  $sourceLanguageCode
+     * @param int $postId  Use term_taxonomy_id for taxonomies, post_id for posts
+     * @param string $postType The type of an element. Can be a post type: post_post, post_page, post_attachment, post_nav_menu_item, post_{custom post key} or taxonomy: tax_category, tax_post_tag, tax_nav_menu, tax_{custom taxonomy key}. Defaults to post_post if not set.
+     * @param string $languageCode  The language code for the element
+     * @param int  $trid   The trid to which the element is to be assigned to. If set to FALSE it will create a new trid for the element causing any potential translation relations to/from it to disappear.
+     * @param null|string  $sourceLanguageCode  The source language code for the element. NULL is reserved for original elements i.e. elements that are not a translation of another. Defaults to NULL when not set.
+     *
+     * This function uses the sitepress method directly, but the args from the hook are all passed in.
+     * Reference: https://wpml.org/wpml-hook/wpml_set_element_language_details/
      */
     public function setTranslationForPost($postId, $postType, $languageCode, $trid = false, $sourceLanguageCode = null)
     {
         global $sitepress;
 
+        $elementType = str_starts_with($postType, 'post_') ? $postType : 'post_' . $postType;
+
         $sitepress->set_element_language_details_action([
             'element_id'    => $postId,
-            'element_type'  => 'post_' . $postType,
+            'element_type'  => $elementType,
             'trid'          => $trid,
             'language_code' => $languageCode,
             'source_language_code' => $sourceLanguageCode

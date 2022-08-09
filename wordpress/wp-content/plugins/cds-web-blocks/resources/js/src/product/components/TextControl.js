edf38f4f28683fa906daf14e6f02895e1deccf8b
Tim Arney <tim@line37.com>
2022-08-09 04:28:21 -0400

    update products

--- wordpress/wp-content/plugins/cds-web-blocks/resources/js/src/product/components/TextControl.js
+++ wordpress/wp-content/plugins/cds-web-blocks/resources/js/src/product/components/TextControl.js
@@ -5,24 +5,51 @@ import { compose } from '@wordpress/compose';
 export const CDSTextControl = compose(
     withDispatch((dispatch, props) => {
         return {
-            setMetaValue: (value) => {
-                dispatch('core/editor').editPost({ meta: { [props.metaKey]: value } });
+            setMetaValue: (value, content) => {
+                let metaKey = props.metaKey.split(":");
+                let key = "";
+                let prop = "";
+                if (metaKey.length === 2) {
+                    key = metaKey[0];
+                    prop = metaKey[1];
+                }
+
+                try {
+                    let data = {};
+                    data = JSON.parse(content);
+                    data[prop] = value;
+                    value = JSON.stringify(data)
+                } catch (err) {
+                    console.log(err.message);
+                }
+
+                dispatch('core/editor').editPost({ meta: { [key]: value } });
             }
         }
     }),
     withSelect((select, props) => {
+        let key = props.metaKey.split(":");
+        if (key.length === 2) {
+            key = key[0];
+        }
         return {
-            metaValue: select('core/editor').getEditedPostAttribute('meta')[props.metaKey],
+            metaValue: select('core/editor').getEditedPostAttribute('meta')[key],
         }
     })
 )((props) => {
+    const key = props.metaKey.split(":");
+    let value = props.metaValue;
+    if (value && key.length === 2) {
+        value = JSON.parse(props.metaValue)[key[1]];
+    }
+
     return (
         <TextControl
-            style={{padding: '10px'}}
+            style={{ padding: '10px' }}
             type="text"
             label={props.label}
-            value={props.metaValue || ""}
-            onChange={(content) => { props.setMetaValue(content) }}
+            value={value || ""}
+            onChange={(content) => { props.setMetaValue(content, props.metaValue) }}
         />
     );
 });
--- wordpress/wp-content/plugins/cds-web-blocks/resources/js/src/product/edit.js
+++ wordpress/wp-content/plugins/cds-web-blocks/resources/js/src/product/edit.js
@@ -1,7 +1,8 @@
-import { InnerBlocks, useBlockProps } from '@wordpress/block-editor';
+import { useBlockProps } from '@wordpress/block-editor';
 import TextControl from './components/TextControl';
-import HiddenControl from './components/HiddenControl';
 import { __ } from '@wordpress/i18n';
+
+// http://localhost/cds/wp-json/wp/v2/product?_embed
 
 const Edit = ({ attributes, setAttributes }) => {
 
@@ -9,19 +10,8 @@ const Edit = ({ attributes, setAttributes }) => {
     return (
         <div {...blockProps}>
             <p>{__('Product', 'cds-web')}</p>
-            <TextControl label={__('Name', 'cds-web')} metaKey="cds_product" />
-            <HiddenControl label={__('Hidden', 'cds-web')} metaKey="cds_product" />
-            <InnerBlocks template={[
-                ['core/button', { id: "test-c", text: 'My button and text!!', url: 'http://github1.com', linkTarget: '_blank', rel: 'noreferrer noopener', className: 'buttonaction' }],
-            ]}
-                allowedBlocks={[
-                    'core/column',
-                    'core/button',
-                    'core/columns',
-                    'core/heading',
-                    'core/paragraph',
-                ]} />
-
+            <TextControl label={__('Name', 'cds-web')} metaKey="cds_product:name" />
+            <TextControl label={__('Description', 'cds-web')} metaKey="cds_product:description" />
         </div>
     );
 };

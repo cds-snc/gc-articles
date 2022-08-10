c278214158fb60fae11664e145c3229595743906
Tim Arney <tim@line37.com>
2022-08-10 13:29:10 -0400

    remove slot for teams

--- wordpress/wp-content/plugins/cds-web-blocks/resources/js/src/team/edit.js
+++ wordpress/wp-content/plugins/cds-web-blocks/resources/js/src/team/edit.js
@@ -2,14 +2,11 @@ import { useBlockProps } from '@wordpress/block-editor';
 import TextControl from '../components/TextControl';
 import { __ } from '@wordpress/i18n';
 
-import GCPostMetaSlotFill from "../../../../../gc-post-meta/resources/js/slot";
-
 const Edit = ({ attributes, setAttributes }) => {
 	const blockProps = useBlockProps();
 	return (
 		<div {...blockProps}>
 			<p>{__('Team Member', 'cds-web')}</p>
-			<GCPostMetaSlotFill>This is a test Team !!</GCPostMetaSlotFill>
 			<TextControl label={__('Name', 'cds-web')} metaKey="cds_web_team_member_name" />
 			<TextControl label={__('Title English', 'cds-web')} metaKey="cds_web_team_member_title_en" />
 			<TextControl label={__('Title French', 'cds-web')} metaKey="cds_web_team_member_title_fr" />

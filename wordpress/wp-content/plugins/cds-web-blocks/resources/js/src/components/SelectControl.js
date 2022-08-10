12d774a1483e397db9e54ca48087849e8f32b7da
Tim Arney <tim@line37.com>
2022-08-10 13:21:08 -0400

    add select control

---
+++ wordpress/wp-content/plugins/cds-web-blocks/resources/js/src/components/SelectControl.js
@@ -0,0 +1,36 @@
+import { SelectControl } from '@wordpress/components';
+
+import { parseMetaKey, updateValue, getValue } from "../utils";
+
+const { withSelect, withDispatch } = wp.data;
+const { compose } = wp.compose;
+
+export const CDSToggleControl = compose(
+    withDispatch((dispatch, props) => {
+        return {
+            setMetaValue: (value, content) => {
+                const { key } = parseMetaKey(props.metaKey);
+                const newValue = updateValue(props.metaKey, value, content);
+                dispatch('core/editor').editPost({ meta: { [key]: newValue } });
+            }
+        }
+    }),
+    withSelect((select, props) => {
+        const { key } = parseMetaKey(props.metaKey);
+        return {
+            metaValue: select('core/editor').getEditedPostAttribute('meta')[key],
+        }
+    })
+)((props) => {
+    const value = getValue(props.metaKey, props.metaValue);
+    return (
+        <SelectControl
+            label={props.label}
+            value={value}
+            options={props.options}
+            onChange={(content) => { props.setMetaValue(content, props.metaValue) }}
+        />
+    );
+});
+
+export default CDSToggleControl;
--- wordpress/wp-content/plugins/cds-web-blocks/resources/js/src/product/components/ProductFields.js
+++ wordpress/wp-content/plugins/cds-web-blocks/resources/js/src/product/components/ProductFields.js
@@ -1,12 +1,23 @@
 const { PanelBody } = wp.components;
 const { __ } = wp.i18n;
 import ToggleControl from "../../components/ToggleControl";
+import SelectControl from "../../components/SelectControl";
 
 export const ProductFields = () => {
     return (
         <div>
             <PanelBody title={__("Status", "cds-web")} initialOpen={true}>
                 <ToggleControl label={__("On Homepage", "cds-web")} metaKey="cds_product:on_home_page" />
+                <SelectControl
+                    options={[
+                        { label: 'Discovery', value: 'Discovery' },
+                        { label: 'Alpha', value: 'alpha' },
+                        { label: 'Beta', value: 'beta' },
+                        { label: 'Live', value: 'live' },
+                    ]}
+                    label={__("Phase", "cds-web")}
+                    metaKey="cds_product:phase"
+                />
             </PanelBody>
         </div>
     )

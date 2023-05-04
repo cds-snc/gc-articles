c45e34eec92a4b2c67fdab13f83bd85538d0f0a4
Pat Heard <patrick.heard@cds-snc.ca>
2023-05-04 14:18:12 -0400

    chore: upgrade to Cypress 12

---
+++ cypress.config.js
@@ -0,0 +1,17 @@
+import { defineConfig } from 'cypress'
+
+export default defineConfig({
+  video: false,
+  screenshotOnRunFailure: false,
+  retries: {
+    runMode: 2,
+    openMode: 0,
+  },
+  projectId: 'rv8iqi',
+  chromeWebSecurity: false,
+  e2e: {
+    setupNodeEvents(on, config) {},
+    baseUrl: 'http://localhost:8889',
+    specPattern: 'cypress/e2e/**/*.{js,jsx,ts,tsx}',
+  },
+})

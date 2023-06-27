f7f2b555f062cc0ea191719d34d3f1881e7b870c
Pat Heard <patrick.heard@cds-snc.ca>
2023-06-27 09:13:51 -0400

    fix: invalidate entire subsite cache (#1285)
    Update the `CDS base` plugin `c3-cloudfront-clear-cache` 
    filter so that cache invalidation requests cause the entire 
    subsite's CloudFront cache to be cleared.
    
    This is to work around a bug in the plugin that is preventing 
    any cache invalidations from running.  There are several open 
    issues and PRs on the plugin to fix the issue but they haven't 
    been merged or triaged.

--- wordpress/wp-content/plugins/cds-base/classes/Modules/Cache/Cache.php
+++ wordpress/wp-content/plugins/cds-base/classes/Modules/Cache/Cache.php
@@ -17,15 +17,12 @@ class Cache
 
     protected function registerInvalidationPaths()
     {
+        // Invalidate the entire site's cache on change.  This is to deal with a bug
+        // in the plugin that is failing to provide the correct paths for some
+        // page update types.
         add_filter('c3_invalidation_items', function ($items, $post) {
-
             $sitePrefix = $this->getSitePrefixForPost();
-            $localePrefix = $this->getLocalePrefixForPost($post);
-
-            return array_merge($items, [
-                "{$sitePrefix}{$localePrefix}wp-json/wp/v2/{$post->post_type}s/?slug={$post->post_name}",
-                "{$sitePrefix}menus/v1/menus/*",
-            ]);
+            return array("{$sitePrefix}*");
         }, 10, 2);
     }
 
@@ -35,18 +32,5 @@ class Cache
         $site = get_blog_details(array('blog_id' => $blog_id));
 
         return $site->path;
-    }
-
-    protected function getLocalePrefixForPost($post): string
-    {
-        if ($language_information = apply_filters('wpml_post_language_details', null, $post->ID)) {
-            $locale = $language_information['locale'];
-
-            if ($locale === self::LOCALE_FR) {
-                return 'fr/';
-            }
-        }
-
-        return '';
     }
 }
--- wordpress/wp-content/themes/cds-default/functions.php
+++ wordpress/wp-content/themes/cds-default/functions.php
@@ -363,7 +363,7 @@ function cc_mime_types($mimes)
     $mimes['svg'] = 'image/svg+xml';
     return $mimes;
 }
-  add_filter('upload_mimes', 'cc_mime_types');
+add_filter('upload_mimes', 'cc_mime_types');
 
 function fix_svg()
 {

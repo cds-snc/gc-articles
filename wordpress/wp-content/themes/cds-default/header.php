3bb91f6bd84a60883d3e6dbf4a2be71d7a9f4572
Pat Heard <patrick.heard@cds-snc.ca>
2023-09-25 15:20:13 -0400

    feat: add tob bar menu location
    Add a new menu location for links that will appear
    to the left of the language swap and sign in links
    in the `#wb-bnr` element of the site.

--- wordpress/wp-content/themes/cds-default/header.php
+++ wordpress/wp-content/themes/cds-default/header.php
@@ -59,15 +59,25 @@ declare(strict_types=1);
       <section id="wb-lng" class="col-xs-3 col-sm-12 pull-right text-right">
         <h2 class="wb-inv"><?php _e('Language selection'); ?></h2>
         <ul class="list-inline mrgn-bttm-0">
-          <li class="mrgn-rght-md">
-              <?php if (is_main_site()) { ?>
-                    <?php if (is_user_logged_in()) { ?>
-                    <a href="<?php echo wp_logout_url(); ?>"><?php _e('Sign out', 'cds-snc')?></a>
-                    <?php } else { ?>
-                    <a href="<?php echo wp_login_url();  ?>"><?php _e('Sign in', 'cds-snc')?></a>
-                    <?php } ?>
-              <?php } ?>
-          </li>
+          <?php
+            if (has_nav_menu('topbar')) {
+                wp_nav_menu(array(
+                    'container' => false,
+                    'depth' => 1,
+                    'items_wrap' => '%3$s',
+                    'theme_location' => 'topbar'
+                ));
+            }
+            ?>
+          <?php if (is_main_site()) { ?>
+            <li class="mrgn-rght-md">
+                <?php if (is_user_logged_in()) { ?>
+                <a href="<?php echo wp_logout_url(); ?>"><?php _e('Sign out', 'cds-snc')?></a>
+                <?php } else { ?>
+                <a href="<?php echo wp_login_url();  ?>"><?php _e('Sign in', 'cds-snc')?></a>
+                <?php } ?>
+            </li>
+          <?php } ?>
           <li>
               <?php echo language_switcher(); ?>
           </li>

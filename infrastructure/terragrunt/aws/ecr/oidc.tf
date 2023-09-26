1c8a9000817d7a275a285cc8b6f04422b20860e6
Pat Heard <patrick.heard@cds-snc.ca>
2023-09-26 09:03:07 -0400

    feat: add OIDC role to manage ECR image pushes (#1442)
    Add OIDC role that can perform ECR image pushes on
    GitHub tag references.

---
+++ infrastructure/terragrunt/aws/ecr/oidc.tf
@@ -0,0 +1,53 @@
+locals {
+  ecr_tag_release = "gc-articles-ecr-tag-release"
+}
+
+module "ecr_tag_release" {
+  source            = "github.com/cds-snc/terraform-modules//gh_oidc_role?ref=v7.0.2"
+  billing_tag_value = var.billing_tag_value
+  roles = [
+    {
+      name      = local.ecr_tag_release
+      repo_name = "gc-articles"
+      claim     = "ref:refs/tags/v*"
+    }
+  ]
+}
+
+resource "aws_iam_role_policy_attachment" "ecr_tag_release" {
+  role       = local.ecr_tag_release
+  policy_arn = aws_iam_policy.ecr_push.arn
+  depends_on = [
+    module.ecr_tag_release
+  ]
+}
+
+resource "aws_iam_policy" "ecr_push" {
+  name   = "wordpress-ecr-push"
+  path   = "/"
+  policy = data.aws_iam_policy_document.ecr_push.json
+}
+
+data "aws_iam_policy_document" "ecr_push" {
+  statement {
+    effect = "Allow"
+    actions = [
+      "ecr:CompleteLayerUpload",
+      "ecr:UploadLayerPart",
+      "ecr:InitiateLayerUpload",
+      "ecr:BatchCheckLayerAvailability",
+      "ecr:PutImage"
+    ]
+    resources = [
+      aws_ecr_repository.wordpress.arn
+    ]
+  }
+
+  statement {
+    effect = "Allow"
+    actions = [
+      "ecr:GetAuthorizationToken"
+    ]
+    resources = ["*"]
+  }
+}

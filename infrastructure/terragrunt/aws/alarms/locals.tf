9d367752dfe1c294008d5e4e9a18ce851b0760a4
Pat Heard <patrick.heard@cds-snc.ca>
2023-11-16 14:24:03 -0500

    fix: suppression of translation alarms (#1532)
    Update how translation file warnings are suppressed
    by CloudWatch.  The previous pattern was not
    generic enough.

--- infrastructure/terragrunt/aws/alarms/locals.tf
+++ infrastructure/terragrunt/aws/alarms/locals.tf
@@ -20,7 +20,7 @@ locals {
     "HTTP/1.1\\\" 403",
     "HTTP/1.1\\\" 404",
     "Undefined constant",
-    "fr_FR.po",
+    "/usr/src/wordpress/wp-content/languages",
   ]
   wordpress_database_errors = [
     "database error",
@@ -31,7 +31,7 @@ locals {
   ]
   wordpress_warnings_skip = [
     "Undefined array key*c3-cloudfront-clear-cache",
-    "fr_FR.po",
+    "/usr/src/wordpress/wp-content/languages",
     "chmod()",
   ]
   wordpress_error_metric_pattern          = "[(w1=\"*${join("*\" || w1=\"*", local.wordpress_errors)}*\") && w1!=\"*${join("*\" && w1!=\"*", local.wordpress_errors_skip)}*\"]"

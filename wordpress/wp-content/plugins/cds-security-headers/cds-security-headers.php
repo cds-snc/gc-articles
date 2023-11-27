5d7e863193427d056fe1d184b03e31a5b002c4e7
Pat Heard <patrick.heard@cds-snc.ca>
2023-11-27 13:40:20 -0500

    test: add Staging Google Tag Manager CSP
    Add a CSP hash to test GTM in Staging.

--- wordpress/wp-content/plugins/cds-security-headers/cds-security-headers.php
+++ wordpress/wp-content/plugins/cds-security-headers/cds-security-headers.php
@@ -96,6 +96,7 @@ function cds_security_headers($headers)
             "'sha256-oZcz0w2RWHkYSMQ8YVYNBR9cguVgzGE9AQFBA8cl0Ic='",
             "'sha256-Bs0uWww5CxWnf/7or8We/tRBQtfACJzPsj9WFg4UEPM='",
             "'sha256-8E/Gjs/wB5HLNdxu7gY99fxxjYvurvtM/9yNcrs9q5c='",
+            "'sha256-SrS284pYQwHK1L2p/XCO/y3J0svDBuhdRlHt/lfxkfE='",
             "https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.js",
             "https://www.canada.ca/etc/designs/canada/wet-boew/js/wet-boew.min.js",
             "https://www.canada.ca/etc/designs/canada/wet-boew/js/theme.min.js",

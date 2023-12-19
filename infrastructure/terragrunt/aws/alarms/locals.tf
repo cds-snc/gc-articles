328a209fb22732cc651d3e2d91064ca0b005bc24
Pat Heard <patrick.heard@cds-snc.ca>
2023-12-19 13:52:37 -0500

    chore: suppress HTTP 400 bad request errors (#1568)
    Do not trigger a CloudWatch error alarm for HTTP 400 response codes.
    
    We have been seeing a few of these over the past weeks caused by
    people scanning for WordPress vulnerabilities.

--- infrastructure/terragrunt/aws/alarms/locals.tf
+++ infrastructure/terragrunt/aws/alarms/locals.tf
@@ -17,6 +17,7 @@ locals {
     "action=lostpassword&error",
     "database error",
     "GET /notification-gc-notify/wp-json/wp/v2/pages",
+    "HTTP/1.1\\\" 400",
     "HTTP/1.1\\\" 403",
     "HTTP/1.1\\\" 404",
     "Undefined constant",

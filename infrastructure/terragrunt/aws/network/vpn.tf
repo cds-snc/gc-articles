c06480ff0c7c6f4b2ca468380678fbbb83355c1b
Pat Heard <patrick.heard@cds-snc.ca>
2024-03-18 16:34:54 -0400

    feat: add client VPN to access database (#1658)
    Add a client VPN to Articles that will allow access to the database
    using our AWS SSO with IAM Identity Center group membership.
    
    By default no subnets will be associated with the VPN so there will
    be no additional cost.  Subnets will be associated/disassociated on
    an as-needed basis.

---
+++ infrastructure/terragrunt/aws/network/vpn.tf
@@ -0,0 +1,59 @@
+module "vpn" {
+  source = "github.com/cds-snc/terraform-modules//client_vpn?ref=v9.2.5"
+
+  endpoint_name       = "private_subnets"
+  access_group_id     = var.client_vpn_access_group_id
+  self_service_portal = "disabled"
+
+  vpc_id              = module.wordpress_vpc.vpc_id
+  vpc_cidr_block      = module.wordpress_vpc.cidr_block
+  subnet_cidr_blocks  = module.wordpress_vpc.private_subnet_cidr_blocks
+  subnet_ids          = []
+  acm_certificate_arn = aws_acm_certificate.client_vpn.arn
+
+  client_vpn_saml_metadata_document = var.client_vpn_saml_metadata
+
+  billing_tag_value = var.billing_tag_value
+}
+
+#
+# Certificate used for VPN communication
+#
+resource "tls_private_key" "client_vpn" {
+  algorithm = "RSA"
+  rsa_bits  = 2048
+}
+
+resource "tls_self_signed_cert" "client_vpn" {
+  private_key_pem       = tls_private_key.client_vpn.private_key_pem
+  validity_period_hours = 43800 # 5 years
+  early_renewal_hours   = 672   # Generate new cert if Terraform is run within 4 weeks of expiry
+
+  subject {
+    common_name = "vpn.${var.env}.articles.alpha.canada.ca"
+  }
+
+  allowed_uses = [
+    "key_encipherment",
+    "digital_signature",
+    "server_auth",
+    "ipsec_end_system",
+    "ipsec_tunnel",
+    "any_extended",
+    "cert_signing",
+  ]
+}
+
+resource "aws_acm_certificate" "client_vpn" {
+  private_key      = tls_private_key.client_vpn.private_key_pem
+  certificate_body = tls_self_signed_cert.client_vpn.cert_pem
+
+  tags = {
+    Terraform             = true
+    (var.billing_tag_key) = var.billing_tag_value
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
--- infrastructure/terragrunt/env/prod/database/terragrunt.hcl
+++ infrastructure/terragrunt/env/prod/database/terragrunt.hcl
@@ -12,8 +12,9 @@ dependency "network" {
   mock_outputs_allowed_terraform_commands = ["init", "fmt", "validate", "plan", "show"]
   mock_outputs_merge_with_state           = true
   mock_outputs = {
-    private_subnet_ids = [""]
-    vpc_id             = ""
+    client_vpn_security_group_id = "sg-0123456789101212"
+    private_subnet_ids           = [""]
+    vpc_id                       = ""
   }
 }
 
@@ -21,6 +22,7 @@ inputs = {
   database_instances_count              = 3
   database_instance_class               = "db.r4.large"
   database_performance_insights_enabled = true
+  client_vpn_security_group_id          = dependency.network.outputs.client_vpn_security_group_id
   private_subnet_ids                    = dependency.network.outputs.private_subnet_ids
   vpc_id                                = dependency.network.outputs.vpc_id
 }

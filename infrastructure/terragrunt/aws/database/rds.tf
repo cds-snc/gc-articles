06d0f5764eb3ef038d3f61e46b8209ed2db494d6
Pat Heard <patrick.heard@cds-snc.ca>
2024-05-03 08:51:21 -0400

    chore: update WordPress to use RDS cluster endpoint (#1725)
    Update the database host secret to contain the cluster's endpoint
    rather than the proxy.
    
    This is being done in preparation for the blue/green deployment which
    requires the RDS proxy to be temporarily removed.

--- infrastructure/terragrunt/aws/database/rds.tf
+++ infrastructure/terragrunt/aws/database/rds.tf
@@ -2,7 +2,7 @@
 # RDS MySQL cluster across 3 subnets
 #
 module "rds_cluster" {
-  source = "github.com/cds-snc/terraform-modules//rds?ref=v9.3.9"
+  source = "github.com/cds-snc/terraform-modules//rds?ref=v9.4.3"
   name   = "wordpress"
 
   database_name  = var.database_name

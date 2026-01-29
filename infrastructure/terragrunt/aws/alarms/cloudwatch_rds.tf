b6cc804553929223a9e54691f0d3fa5048205875
Pat Heard <patrick.heard@cds-snc.ca>
2026-01-29 13:59:31 -0500

    feat: shutdown staging overnight (#2427)
    Schedule the shutdown of the staging ECS service and RDS cluster between 10pm and 10am UTC.
    
    This change also removes the CloudWatch alarms on the RDS reader instances
    as they are no longer needed.

--- infrastructure/terragrunt/aws/alarms/cloudwatch_rds.tf
+++ infrastructure/terragrunt/aws/alarms/cloudwatch_rds.tf
@@ -21,46 +21,6 @@ resource "aws_cloudwatch_metric_alarm" "rds_cpu_utilization_writer" {
   }
 }
 
-resource "aws_cloudwatch_metric_alarm" "rds_cpu_utilization_reader" {
-  alarm_name          = "RDSCpuUtilizationReader"
-  comparison_operator = "GreaterThanThreshold"
-  evaluation_periods  = "1"
-  metric_name         = "CPUUtilization"
-  namespace           = "AWS/RDS"
-  period              = "300"
-  statistic           = "Maximum"
-  threshold           = var.rds_cpu_maxiumum
-
-  alarm_description = "CPU utilization for RDS cluster reader in a 5 minute period"
-  alarm_actions     = [aws_sns_topic.alert_warning.arn]
-  ok_actions        = [aws_sns_topic.alert_warning.arn]
-
-  dimensions = {
-    DBClusterIdentifier = var.rds_cluster_id
-    Role                = "READER"
-  }
-}
-
-resource "aws_cloudwatch_metric_alarm" "rds_aurora_replica_lag" {
-  alarm_name          = "RdsAuroraReplicaLag"
-  comparison_operator = "GreaterThanThreshold"
-  evaluation_periods  = "1"
-  metric_name         = "AuroraReplicaLag"
-  namespace           = "AWS/RDS"
-  period              = "300"
-  statistic           = "Maximum"
-  threshold           = var.rds_aurora_replica_lag_maximum
-
-  alarm_description = "Replica lag (milliseconds) for RDS cluster reader in a 5 minute period"
-  alarm_actions     = [aws_sns_topic.alert_warning.arn]
-  ok_actions        = [aws_sns_topic.alert_warning.arn]
-
-  dimensions = {
-    DBClusterIdentifier = var.rds_cluster_id
-    Role                = "READER"
-  }
-}
-
 resource "aws_cloudwatch_metric_alarm" "rds_freeable_memory_writer" {
   alarm_name          = "RdsFreeableMemoryWriter"
   comparison_operator = "LessThanThreshold"
@@ -78,25 +38,5 @@ resource "aws_cloudwatch_metric_alarm" "rds_freeable_memory_writer" {
   dimensions = {
     DBClusterIdentifier = var.rds_cluster_id
     Role                = "WRITER"
-  }
-}
-
-resource "aws_cloudwatch_metric_alarm" "rds_freeable_memory_reader" {
-  alarm_name          = "RdsFreeableMemoryReader"
-  comparison_operator = "LessThanThreshold"
-  evaluation_periods  = "1"
-  metric_name         = "FreeableMemory"
-  namespace           = "AWS/RDS"
-  period              = "300"
-  statistic           = "Minimum"
-  threshold           = var.rds_freeable_memory_minimum
-
-  alarm_description = "Freeable memory (Bytes) for RDS cluster reader in a 5 minute period"
-  alarm_actions     = [aws_sns_topic.alert_warning.arn]
-  ok_actions        = [aws_sns_topic.alert_warning.arn]
-
-  dimensions = {
-    DBClusterIdentifier = var.rds_cluster_id
-    Role                = "READER"
   }
 }
---
+++ infrastructure/terragrunt/aws/alarms/shutdown.tf
@@ -0,0 +1,21 @@
+module "schedule_shutdown" {
+  count = var.env == "staging" ? 1 : 0
+
+  source = "github.com/cds-snc/terraform-modules//schedule_shutdown?ref=v10.10.2"
+
+  cloudwatch_alarm_arns = [
+    aws_cloudwatch_metric_alarm.alb_target_unhealthy_host.arn,
+    aws_cloudwatch_metric_alarm.alb_target_response_time_average.arn,
+  ]
+  ecs_service_arns = [
+    "arn:aws:ecs:${var.region}:${var.account_id}:service/${var.ecs_cluster_name}/${var.ecs_service_name}",
+  ]
+  rds_cluster_arns = [
+    "arn:aws:rds:${var.region}:${var.account_id}:cluster:${var.rds_cluster_id}",
+  ]
+
+  schedule_shutdown = "cron(0 22 * * ? *)"       # 10pm UTC, every day
+  schedule_startup  = "cron(0 10 ? * MON-FRI *)" # 10am UTC, Monday-Friday
+
+  billing_tag_value = var.billing_tag_value
+}

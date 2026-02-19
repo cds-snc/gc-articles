b8a8c5126ae27446a0b479767a127e8a855f3753
Pat Heard <patrick.heard@cds-snc.ca>
2026-02-19 16:19:52 -0500

    chore: remove cds-web-blocks plugin

--- wordpress/wp-content/plugins/cds-web-blocks/resources/js/src/team/index.js
+++
@@ -1,9 +0,0 @@
-import { registerBlockType } from '@wordpress/blocks';
-import './style.scss';
-import Edit from './edit';
-registerBlockType('cds/team', {
-	edit: Edit,
-	save: ({ attributes, className }) => null,
-});
-
-

d0a122978ffe3a124aed14067ae2d336ef2ad0b1
Pat Heard <patrick.heard@cds-snc.ca>
2026-03-21 10:58:39 -0400

    chore: remove ListManager REST API endpoints
    Remove the ListManager REST API endoints and associated WordPress
    custom blocks and secrets. These features are no longer being used
    and were fuzzed by security researchers, causing errors in our logs.

--- wordpress/wp-content/plugins/cds-base/classes/Modules/BlocksPHP/BlocksPHP.php
+++ wordpress/wp-content/plugins/cds-base/classes/Modules/BlocksPHP/BlocksPHP.php
@@ -5,7 +5,6 @@ namespace CDS\Modules\BlocksPHP;
 use CDS\Utils;
 use CDS\Modules\Forms\Contact\ContactForm;
 use CDS\Modules\Forms\RequestSite\RequestSiteForm;
-use CDS\Modules\Forms\Subscribe\SubscriptionForm;
 
 class BlocksPHP
 {
@@ -21,31 +20,6 @@ class BlocksPHP
 
     public function registerBlocks()
     {
-        register_block_type(__DIR__ . '/build/subscribe/', [
-            'render_callback' => function ($attributes, $content, $block): string {
-                $form = new SubscriptionForm();
-                return $form->render($attributes);
-            },
-            'attributes' => [
-                'listId' => [
-                    'type' => 'string',
-                    "default" => ""
-                ],
-                'emailLabel' => [
-                    'type' => 'string',
-                    "default" => __("Enter your email:", "cds-snc")
-                ],
-                'subscribeLabel' => [
-                    'type' => 'string',
-                    "default" =>  __("Subscribe:", "cds-snc")
-                ],
-                'privacyLink' => [
-                    'type' => 'string',
-                    "default" => ''
-                ],
-            ]
-        ]);
-
         // return early if viewing the admin interface but not a superadmin
         if (is_admin() && !is_super_admin() && !Utils::isWpEnv()) {
             return;

6a7ea8f83e8c412b347e1815834201aa59416c92
Omar Tehsin <omartehsin@gmail.com>
2026-04-15 12:28:56 -0400

    wip

--- wordpress/wp-content/themes/cds-default/functions.php
+++ wordpress/wp-content/themes/cds-default/functions.php
@@ -6,6 +6,7 @@ require_once __DIR__ . '/../../../vendor/autoload.php';
 require_once __DIR__ . '/inc/template-functions.php';
 require_once __DIR__ . '/inc/template-filters.php';
 require_once __DIR__ . '/inc/webhook.php';
+require_once __DIR__ . '/inc/settings.php';
 
 /**
  * cds-default functions and definitions
--- wordpress/wp-content/themes/cds-default/inc/webhook.php
+++ wordpress/wp-content/themes/cds-default/inc/webhook.php
@@ -4,20 +4,29 @@ function onSavePost($post_ID, $post)
 {
     if ($post->post_status === "publish") {
         try {
-            $url = "https://api.github.com/repos/cds-snc/cds-website-pr-bot/dispatches";
+            // Get configurable repository URL
+            $repo_url = get_option('GITHUB_REPOSITORY_URL', 'cds-snc/cds-website-pr-bot');
+            $url = "https://api.github.com/repos/{$repo_url}/dispatches";
+            
             $token = get_option('GITHUB_AUTH_TOKEN');
 
             if (empty($token)) {
+                error_log("GitHub :: No auth token configured");
+                return;
+            }
+
+            if (empty($repo_url)) {
+                error_log("GitHub :: No repository URL configured");
                 return;
             }
 
             $args = [
                 'headers' => [
-                'Accept' => 'application/vnd.github+json',
-                'Authorization' => 'token ' . $token,
-                'Content-Type' => 'application/json'
+                    'Accept' => 'application/vnd.github+json',
+                    'Authorization' => 'token ' . $token,
+                    'Content-Type' => 'application/json'
                 ],
-                'body'        => json_encode(['event_type' => 'strapi_update'])
+                'body' => json_encode(['event_type' => 'strapi_update'])
             ];
 
             $response = wp_remote_post($url, $args);
@@ -26,7 +35,7 @@ function onSavePost($post_ID, $post)
                 $error_message = $response->get_error_message();
                 error_log("Github :: Something went wrong: $error_message");
             } else {
-                error_log('Github :: response:');
+                error_log("Github :: Success - triggered workflow for: $repo_url");
                 error_log(print_r($response['response'], true));
             }
         } catch (Exception $e) {

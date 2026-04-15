310a8eae22d0435edbe4280c55a535110cc0e690
Pat Heard <patrick.heard@cds-snc.ca>
2026-04-15 14:57:17 -0400

    version bump 4.9.0 (#2562)
    Updates version files with bug fix and CDS default theme GitHub dispatch setting.

--- wordpress/wp-content/themes/cds-default/functions.php
+++ wordpress/wp-content/themes/cds-default/functions.php
@@ -19,7 +19,7 @@ require_once __DIR__ . '/settings.php';
 // phpcs:disable
 if (!defined('_S_VERSION')) {
     // Replace the version number of the theme on each release.
-    define('_S_VERSION', '4.8.1');
+    define('_S_VERSION', '4.9.0');
 }
 
 if (!defined('THEME_NAMESPACE')) {

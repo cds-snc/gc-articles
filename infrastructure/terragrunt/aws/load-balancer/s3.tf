51880fe7903a22e6b54df263da0be92d324aa8fd
Omar Tehsin <omartehsin@gmail.com>
2026-04-22 13:58:43 -0400

    revert s3 update commit

--- infrastructure/terragrunt/aws/load-balancer/s3.tf
+++ infrastructure/terragrunt/aws/load-balancer/s3.tf
@@ -1,8 +1,6 @@
 #
 # CloudFront logs
 # TODO: switch to the cds-snc S3 module
-data "aws_canonical_user_id" "current" {}
-
 resource "aws_s3_bucket" "cloudfront_logs" {
   # checkov:skip=CKV_AWS_18:access logging not required for ephemeral data
   # checkov:skip=CKV_AWS_21:verioning not needed for ephemeral data
@@ -17,23 +15,25 @@ resource "aws_s3_bucket" "cloudfront_logs" {
       }
     }
   }
+  lifecycle_rule {
+    enabled = true
 
-}
-resource "aws_s3_bucket_acl" "cloudfront_logs" {
-  bucket = aws_s3_bucket.cloudfront_logs.id
-
-  access_control_policy {
-    grant {
-      grantee {
-        id   = "c4c1ede66af53448b93c283ce9448c4ba468c9432aa01d700d3878632f77d2d0"
-        type = "CanonicalUser"
-      }
-      permission = "FULL_CONTROL"
+    expiration {
+      days                         = 30
+      expired_object_delete_marker = false
     }
 
-    owner {
-      id = data.aws_canonical_user_id.current.id
+    noncurrent_version_expiration {
+      days = 30
     }
+  }
+
+  # awslogsdelivery account needs full control for cloudfront logging
+  # https://docs.aws.amazon.com/AmazonCloudFront/latest/DeveloperGuide/AccessLogs.html
+  grant {
+    id          = "c4c1ede66af53448b93c283ce9448c4ba468c9432aa01d700d3878632f77d2d0"
+    type        = "CanonicalUser"
+    permissions = ["FULL_CONTROL"]
   }
 }
 
@@ -44,21 +44,4 @@ resource "aws_s3_bucket_public_access_block" "cloudfront_logs" {
   block_public_policy     = true
   ignore_public_acls      = true
   restrict_public_buckets = true
-}
-
-resource "aws_s3_bucket_lifecycle_configuration" "cloudfront_logs" {
-  bucket = aws_s3_bucket.cloudfront_logs.id
-
-  rule {
-    id     = "expire-objects"
-    status = "Enabled"
-
-    expiration {
-      days = 30
-    }
-
-    noncurrent_version_expiration {
-      noncurrent_days = 30
-    }
-  }
 }

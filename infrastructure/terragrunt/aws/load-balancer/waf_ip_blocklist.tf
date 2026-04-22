fbf54549072b57ac9cac1dc09e9bfd097b800d5a
Omar Tehsin <omartehsin@gmail.com>
2026-04-22 09:12:12 -0400

    updated source for waf ip blocklist

--- infrastructure/terragrunt/aws/load-balancer/waf_ip_blocklist.tf
+++ infrastructure/terragrunt/aws/load-balancer/waf_ip_blocklist.tf
@@ -1,5 +1,5 @@
 module "waf_ip_blocklist" {
-    source = "https://github.com/cds-snc/terraform-modules/pull/832/changes/14bc5c8e2493911e3088ea9c86fd556dca8b8f5d"
+    source = "github.com/cds-snc/terraform-modules//waf_ip_blocklist?ref=14bc5c8e2493911e3088ea9c86fd556dca8b8f5d"
     service_name = "gc-articles-waf-ip-blocklist-test"
     athena_query_results_bucket = module.athena_bucket.s3_bucket_id
     athena_query_source_bucket = var.cbs_satellite_bucket_name

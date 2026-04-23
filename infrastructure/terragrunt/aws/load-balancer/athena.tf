c45694068785abc6f9b286b32f54cd0478a4888c
Omar Tehsin <33768337+omartehsin1@users.noreply.github.com>
2026-04-23 08:46:14 -0400

    test: updated athena access logs, added waf ip module (#2585)
    * updated athena access logs, added waf ip module
    
    * updated source for waf ip blocklist
    
    * formatting
    
    * updated deprecated lifecycle rule
    
    * updated s3
    
    * updated athena source
    
    * revert s3 update commit
    
    * update athena SHA

--- infrastructure/terragrunt/aws/load-balancer/athena.tf
+++ infrastructure/terragrunt/aws/load-balancer/athena.tf
@@ -2,7 +2,7 @@
 # Create Athena queries to view the WAF and load balancer access logs
 #
 module "athena_access_logs" {
-  source = "github.com/cds-snc/terraform-modules//athena_access_logs?ref=v10.11.3"
+  source = "github.com/cds-snc/terraform-modules//athena_access_logs?ref=90caf6e7f57620d8278a71c8f6d95d202b91cb24"
 
   athena_bucket_name = module.athena_bucket.s3_bucket_id
 
